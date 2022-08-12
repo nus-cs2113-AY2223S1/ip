265befa511df76f4fa25d17f9330b1f514e22be3
LokQiJun <e0544063@u.nus.edu>
2022-08-12 17:10:45 +0800

    wk2 level 0 greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greetings();
+        exit();
+    }
+
+    public static void exit(){
+        System.out.println(" Bye! Hope to see you again soon :)");
+    }
+
+    public static void greetings(){
+        System.out.println(" Hello! I'm Duke\n" + " What can I do for you?");
     }
 }
