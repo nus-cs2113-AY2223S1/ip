a5d52ac65a3e7ba8731b12a33ef34c4dc25b3b51
jingwei55 <e0725540@u.nus.edu>
2022-08-13 16:12:39 +0800

    jackson

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 public class Duke {
     public static void level_0 () {
         String intro = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                + "Hello! I'm Duke, your personal chatbot! :)\n"
+                + "Hello! I'm Jackson, your personal chatbot! :)\n"
                 + "What service are you looking for?\n"
                 + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 + "Leaving already? :( Come back soon!\n"
@@ -9,12 +9,18 @@ public class Duke {
         System.out.println(intro);
     }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String jackson = "       _            _                    \n"
+                + "      | |          | |                   \n"
+                + "      | | __ _  ___| | _____  ___  _ __  \n"
+                + "  _   | |/ _` |/ __| |/ / __|/ _ \\| '_ \\ \n"
+                + " | |__| | (_| | (__|   <\\__ \\ (_) | | | |\n"
+                + "  \\____/ \\__,_|\\___|_|\\_\\___/\\___/|_| |_|\n";
+        System.out.println("Hello from\n" + jackson);
         level_0();
     }
 }
