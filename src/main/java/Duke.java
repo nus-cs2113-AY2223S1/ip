5f4ff68f94a3610f84dacd12906ce6e2fe178c41
Wilson <wilsonngja@gmail.com>
2022-08-14 14:22:38 +0800

    Edit greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   _           _       __         \n"
+                +     "  | |   ___   | |  |_||  |        \n"
+                +     "  | |  /   \\  | |  __ |  |       \n"
+                +     "  | | / __  \\ | | |  ||  |        \n"
+                +     "  | |/ /   \\ \\| | |  ||  |        \n"
+                +     "  |___/     \\___| |__||__|         \n";
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Wilson");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
     }
 }
