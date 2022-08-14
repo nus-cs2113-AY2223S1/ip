38a5b2066b6fb251edd2ae1787fab46aafcc2aa3
dhanish265 <dhanish265@gmail.com>
2022-08-14 21:26:30 +0800

    add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printLine();
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLine(){
+        for(int i = 0; i < 60; i++){
+            System.out.print("\u2500");
+        }
+        System.out.println();
     }
 }
