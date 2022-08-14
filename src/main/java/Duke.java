eda891a1c8fb480cadd83a75883bf3e47e529f7f
alfred-leong <alfredleongweilun@gmail.com>
2022-08-14 17:10:44 +0800

    Completed Level 0 (Greet)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
 public class Duke {
+    public static void drawLine() {    //print underscore symbol 50 times
+        System.out.println("__________________________________________________");
+    }
+
+    public static void welcomeUser() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void byeUser() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        drawLine();
+        welcomeUser();
+        drawLine();
+        byeUser();
     }
 }
