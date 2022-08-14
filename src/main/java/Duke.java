73ae6e03f3e6cdc50788020ccce79bdf8c132d74
Devesh Logendran <deveshl@users.noreply.github.com>
2022-08-14 22:54:44 +0800

    Update logo, add greet and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "$$$$$$$\\                  $$\\           \n" +
+                "$$  __$$\\                 $$ |          \n" +
+                "$$ |  $$ |$$\\   $$\\  $$$$$$$ | $$$$$$\\  \n" +
+                "$$ |  $$ |$$ |  $$ |$$  __$$ |$$  __$$\\ \n" +
+                "$$ |  $$ |$$ |  $$ |$$ /  $$ |$$$$$$$$ |\n" +
+                "$$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|\n" +
+                "$$$$$$$  |\\$$$$$$  |\\$$$$$$$ |\\$$$$$$$\\ \n" +
+                "\\_______/  \\______/  \\_______| \\_______|";
+        String greet = "I am Dude.\n" +
+                "Type something in, dude.";
+        String bye = "Catch you later, dude.";
+        System.out.println(logo);
+        System.out.println();
+        System.out.println(greet);
+        System.out.println();
+        System.out.println(bye);
     }
 }
