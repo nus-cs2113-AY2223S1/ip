fa64b3ad93fb879d639e82e4b0e5e88f546bbdb2
Malcolm Ang <malcolmangsc@gmail.com>
2022-08-15 20:37:19 +0800

    Add Increment: Level-0
    Added Level-0 functionality with the "Dude" chatbot.

---
+++ src/main/java/Dude.java
@@ -0,0 +1,12 @@
+public class Dude {
+    public static void main(String[] args) {
+        String line = "=================================";
+
+        System.out.println(line);
+        System.out.println("Hey dude");
+        System.out.println("What's up?");
+        System.out.println(line);
+        System.out.println("See ya dude");
+        System.out.println(line);
+    }
+}
