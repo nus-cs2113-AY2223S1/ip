58f4229f4c0a866847ae3cba53872d681c0f5c26
joshuan98 <dzhei98@gmail.com>
2022-08-15 22:42:13 +0800

    Add greet functionality
    Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____ _ \n"
+        // + "| _ \\ _ _| | _____ \n"
+        // + "| | | | | | | |/ / _ \\\n"
+        // + "| |_| | |_| | < __/\n"
+        // + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+        String horizontal_line = "______________________________";
+        String introduction = "Hihi, my name is Jay!\nWhat can I do for you today?";
+        String exit = "Goodbye! Hope to see you again!";
+
+        System.out.println(horizontal_line);
+        System.out.println(introduction);
+        System.out.println(horizontal_line);
+        System.out.println(exit);
+        System.out.println(horizontal_line);
     }
 }
