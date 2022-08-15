577867f3d04337e2b7036660133d805a5ba6c191
wcwy <thinhong99@gmail.com>
2022-08-15 20:32:06 +0800

    Gitignore file: ignore .class files Duke class: Add greeting messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    public static void greet(){
+        String entryBanner = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?";
+        System.out.println(entryBanner);
+    }
+
+    public static void quit(){
+        String exitBanner = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(exitBanner);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        quit();
     }
 }
