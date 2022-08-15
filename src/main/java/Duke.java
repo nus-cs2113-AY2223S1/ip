ffee13557b6e2ad699b49edfb14768e50e472583
markusteimann <markusteimann13@gmail.com>
2022-08-15 10:39:56 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                Hello, my name is Duke!\s
+                 What can I do for you?\s
+                 Bye. Hope to see you again soon!""");
+
     }
 }
