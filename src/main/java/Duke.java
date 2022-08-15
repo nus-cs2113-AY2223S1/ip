a9427a0b505e5910b6c639437070d06654d4909b
zhoukaiwen <e0774651@u.nus.edu>
2022-08-15 21:32:00 +0800

    increment level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
 public class Duke {
 
     //Zhou Zhou
+    public static void line() {
+        System.out.println("------------------------------");
+    }
+
+    public static void greet() {
+        line();
+        System.out.println("Hi! I'm Slave Kai, Zhou Zhou's 256IQ bot\nHow can I help you?");
+        line();
+    }
+
+    public static void bye() {
+        System.out.println("Have a great day!");
+        line();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,5 +22,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+
+
+        bye();
     }
 }
