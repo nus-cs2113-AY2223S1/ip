48da91ed8879e597dc3149328b355712ccde7dca
xzynos <lim_fang_jie@hotmail.com>
2022-08-16 22:52:22 +0800

    Add greeting and goodbye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        String greetingMessage = " Hello! I'm Duke\n"
+                + " What can I do for you?";
+        System.out.println(greetingMessage);
+
+        System.out.println("____________________________________________________________");
+        String goodbyeMessage = " Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+
+        System.out.println("____________________________________________________________");
     }
 }
