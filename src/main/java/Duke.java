f5748234188a6443f8c4af35079cdf0310405cce
ivanthengwr <ivanthengwr@gmail.com>
2022-08-17 22:52:10 +0800

    Add initial skeletal version of the Flash

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "___________.__                ___.     \n"
+                + "\\_   _____/|  | _____    _____|  |__  \n"
+                + " |    __)  |  | \\__  \\  /  ___/  |  \\ \n"
+                + " |     \\   |  |__/ __ \\_\\___ \\|   Y  \\\n"
+                + " \\___  /   |____(____  /____  >___|  /\n"
+                + "     \\/              \\/     \\/     \\/ \n";
         System.out.println("Hello from\n" + logo);
+        String lineDivider = "____________________________________________________________\n";
+        System.out.println(lineDivider
+                + "Hello! I'm Flash\n"
+                + "What can I do for you?\n"
+                + lineDivider
+                + "Bye. Hope to see you again soon!\n"
+                + lineDivider);
     }
 }
