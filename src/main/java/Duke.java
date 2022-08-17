0f251c3713211608fd09005df67853b0f4b30d68
P0tatoChips <69447339+P0tatoChips@users.noreply.github.com>
2022-08-17 15:28:27 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    public static void printLines(int n) {
+        // to print those lines
+        for (int i = 0; i < n; i ++) {
+            System.out.print("_");
+        }
+        System.out.println("");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLines(50);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLines(50);
+        System.out.println("Bye. Hope to see you again soon!");
+        printLines(50);
     }
 }
