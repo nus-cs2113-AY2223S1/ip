9d736d13f8375dae07a9f35badc8401d751bcdc6
nshian <ianng800@gmail.com>
2022-08-18 13:30:53 +0800

    Edit greetUser()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,6 @@ public class Duke {
         System.out.println(greeting);
     }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|";
-        System.out.println(logo);
         greetUser();
     }
 }
