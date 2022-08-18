4010cf2f6e0cce4786ca36ba1d2abc1ba4401174
nshian <ianng800@gmail.com>
2022-08-18 13:24:19 +0800

    Add greetUser() for Level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+
+    public static void greetUser(){
+        String greeting = "____________________________________________________________" + System.lineSeparator()
+        + "Hello! My name is Axel. :-)" + System.lineSeparator()
+        + "How may I help you today?" + System.lineSeparator()
+        + "____________________________________________________________" + System.lineSeparator()
+        + "Goodbye. Hope to see you again soon!" + System.lineSeparator()
+        + "____________________________________________________________";
+        System.out.println(greeting);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|";
+        System.out.println(logo);
+        greetUser();
     }
 }
