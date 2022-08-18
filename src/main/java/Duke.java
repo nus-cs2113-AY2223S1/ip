608ca45d6a87a8e6c3e7522c3ce4fc99d5286530
nshian <ianng800@gmail.com>
2022-08-18 19:40:44 +0800

    Edit greetUser(), add echoToUser() and sayByeToUser() for Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
 public class Duke {
 
     public static void greetUser(){
         String greeting = "____________________________________________________________" + System.lineSeparator()
         + "Hello! My name is Axel. :-)" + System.lineSeparator()
         + "How may I help you today?" + System.lineSeparator()
-        + "____________________________________________________________" + System.lineSeparator()
-        + "Goodbye. Hope to see you again soon!" + System.lineSeparator()
-        + "____________________________________________________________";
+        + "____________________________________________________________" + System.lineSeparator();
         System.out.println(greeting);
+    }
+
+    public static void echoToUser(String curr){
+        String greeting = "____________________________________________________________" + System.lineSeparator()
+                + curr + System.lineSeparator()
+                + "____________________________________________________________" + System.lineSeparator();
+        System.out.println(greeting);
+    }
+    public static void sayByeToUser(){
+        String message = "____________________________________________________________" + System.lineSeparator()
+                + "Goodbye. Hope to see you again soon!" + System.lineSeparator()
+                + "____________________________________________________________";
+        System.out.println(message);
     }
     public static void main(String[] args) {
         String logo = "   _____                .__   \n" +
@@ -17,5 +29,17 @@ public class Duke {
                 "\\____|__  /__/\\_ \\\\___  >____/\n";
         System.out.println(logo);
         greetUser();
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit){
+            String curr = input.nextLine();
+            if(curr.equals("bye")){
+                exit = true;
+                sayByeToUser();
+            }
+            else{
+                echoToUser(curr);
+            }
+        }
     }
 }
