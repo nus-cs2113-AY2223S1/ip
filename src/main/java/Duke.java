a138977baf27ffe45b76f263a83c78cf570d809b
Nay <naychi.w_naing@u.yale-nus.edu.sg>
2022-08-18 22:02:29 +0800

    Level-0:Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____                 _     \n" +
+                " |  _ \\ ___  __ _  ___| |__  \n" +
+                " | |_) / _ \\/ _` |/ __| '_ \\ \n" +
+                " |  __/  __/ (_| | (__| | | |\n" +
+                " |_|   \\___|\\__,_|\\___|_| |_|\n" +
+                "                             ";
+
+        System.out.println(logo);
+
+        String intro = "__________________________________________________ \n"
+                + "  Hello! I'm Peach  \n"
+                + "  What can I do for you? \n"
+                + "__________________________________________________ \n"
+                + "  Bye. Hope to see you again soon! \n"
+                + "__________________________________________________ \n";
+
+        System.out.println(intro);
+
     }
 }
