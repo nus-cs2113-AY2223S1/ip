441ccc737a5915bec3327d728adc7e2e84fa5f84
helpdesk1234 <ongzhihong2000@gmail.com>
2022-08-18 11:51:48 +0800

    Add Increment Level-0 (Greet)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        newLine();
+        welcomeGreeting();
+        newLine();
+        goodbyeGreeting();
+        newLine();
+    }
+
+    private static void newLine(){
+        for (int i = 0; i < 60; i++) {
+            System.out.print('_');
+        }
+        System.out.print('\n');
+    }
+
+    private static void welcomeGreeting(){
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+    }
+
+    private static void goodbyeGreeting(){
+        System.out.println(" Bye. Hope to see you again soon!");
     }
 }
