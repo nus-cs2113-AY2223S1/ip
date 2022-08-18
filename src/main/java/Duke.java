5701ddc97f05f7313996f2d28d13695fe09af13d
John <johntohjiajun@gmail.com>
2022-08-19 01:59:56 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\\\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(logo);
+        String welcomeText = "Hello! I am not Duke.\n"
+                + "I can echo you now.\n";
+        System.out.println(welcomeText);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.toLowerCase().trim().equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        System.out.println("Byebye!!!");
     }
 }
