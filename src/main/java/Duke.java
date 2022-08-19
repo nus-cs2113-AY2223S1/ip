1a24cbc79478a3047545db4715e8d070fd3378ca
ngdeqi <deqi.angel@gmail.com>
2022-08-19 22:41:53 +0800

    Add Level 1 features: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greet =
-                "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
+        String input, output;
+        output =
+                "    ____________________________________________________________\n"
+                + "    Hello! I'm DuckyMoMo\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(output);
 
-        System.out.println(greet);
+
+        input = sc.next();
+        while (!input.equals("bye")) {
+            output = "    ____________________________________________________________\n"
+                    + "    " + input + "\n"
+                    + "    ____________________________________________________________\n";
+            System.out.println(output);
+            input = sc.next();
+        }
+
+        output = "    ____________________________________________________________\n"
+                + "    Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(output);
     }
 }
