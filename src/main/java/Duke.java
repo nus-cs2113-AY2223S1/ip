1b6d7a95ab15b72d0ca70fce8b7febf66a3b809e
bdthanh <bdthanh200403@gmail.com>
2022-08-19 11:31:24 +0800

    Add Increment: Level-0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+        String divider = "____________________________________________________________\n";
+        String greet = divider
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + divider
+                + "Bye. Hope to see you again soon!\n"
+                + divider;
+        System.out.println(greet);
     }
 }
