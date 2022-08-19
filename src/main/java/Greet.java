3219077db0a1cf75abb9a181aa151b992413f9e4
Chiew Yi Xiang <yixiangchiew@gmail.com>
2022-08-19 15:01:38 +0800

    Level-0: Greet

---
+++ src/main/java/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+    private String hello = "Hello! I'm Duke";
+    private String greeting = "What can I do for you?";
+    private String goodbye = "Bye. Hope to see you again soon!";
+    public void printGreet() {
+        System.out.println(hello);
+        System.out.println(greeting);
+        System.out.println(goodbye);
+    }
+}
