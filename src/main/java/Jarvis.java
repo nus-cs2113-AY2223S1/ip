66dfc8b75a1b1ccd7c2b8e9337321e645a2729d2
kohnh <kohnh88@gmail.com>
2022-08-19 11:59:35 +0800

    Revert "Revert "Added Level-0""
    This reverts commit 02a839308dec4b643321ee0c9e7867cb606e2ae3.

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,10 +1,21 @@
 public class Jarvis {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____.  _____ ______________   ____.___  _________\n"
+                + "    |    | /  _  \\\\______   \\   \\ /   /|   |/   _____/\n"
+                + "    |    |/  /_\\  \\|       _/\\   Y   / |   |\\_____  \\ \n"
+                + "/\\__|    /    |    \\    |   \\ \\     /  |   |/        \\\n"
+                + "\\________\\____|__  /____|_  /  \\___/   |___/_______  /\n"
+                + "                 \\/       \\/                       \\/ \n";
+
+        String greeting = "------------------------------------------------\n"
+                + "Hello! I'm\n"
+                + logo
+                + "What can I do for you?\n"
+                + "------------------------------------------------\n";
+        String parting = "Bye. Hope to see you again soon!\n"
+                + "------------------------------------------------\n";
+
+        System.out.println(greeting);
+        System.out.println(parting);
     }
 }
