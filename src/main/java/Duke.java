ffe68a56d727ca29de92d46383b67152b0b8d591
ngdeqi <deqi.angel@gmail.com>
2022-08-19 23:44:10 +0800

    Fix Level-1 bug

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,13 @@ public class Duke {
         System.out.println(output);
 
 
-        input = sc.next();
+        input = sc.nextLine();
         while (!input.equals("bye")) {
             output = "    ____________________________________________________________\n"
                     + "    " + input + "\n"
                     + "    ____________________________________________________________\n";
             System.out.println(output);
-            input = sc.next();
+            input = sc.nextLine();
         }
 
         output = "    ____________________________________________________________\n"
