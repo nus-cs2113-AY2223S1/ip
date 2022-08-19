c1c9ceb6ea567ab8dc758fc57d28f979f160e90e
Ria Vora <riavora11@gmail.com>
2022-08-19 21:30:34 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,58 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+
+        //Initialize necessary variables
+        init();
+
+        //Print introduction to Duke
+        introduction();
+
+        //Re-iterate what the user types
+
+        while (!respondToUser()) {
+        }
+
+        goodbye();
+
+    }
+
+    private static void init() {
+        scanner = new Scanner(System.in);
+    }
+
+    private static void introduction() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        introduction();
+        String introText = "\n    Hello! I'm Duke\n    What can I do for you?";
+        String introduction = logo + hLine + introText + hLine + "\n";
+        System.out.println(introduction);
     }
 
-    private static void introduction() {
-        String hLine = "\n-----------------------------------";
-        String introduction = "\nHello! I'm Duke\nWhat can I do for you?";
-        String goodbye = "\nBye. Hope to see you again soon!";
-        String result = hLine + introduction + hLine + goodbye + hLine;
-        System.out.println(result);
+
+    private static boolean respondToUser() {
+        String input = scanner.next();
+        boolean exit = input.toLowerCase().equals("bye");
+        if (!exit) {
+            System.out.println(hLine + indent + input + hLine + "\n");
+        }
+        return exit;
     }
+
+    private static void goodbye() {
+        String goodbyeText = "\n    Bye. Hope to see you again soon!";
+        String goodbye = hLine + goodbyeText + hLine;
+        System.out.println(goodbye);
+    }
+
+    private static Scanner scanner;
+    private static String indent = "\n    ";
+    private static String hLine = indent + "-----------------------------------";
 }
