e5b21a0b9967e20ab3d1828796f14bff07b45fd3
Ria Vora <riavora11@gmail.com>
2022-08-19 16:27:15 +0800

    added level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        introduction();
+    }
+
+    private static void introduction() {
+        String hLine = "\n-----------------------------------";
+        String introduction = "\nHello! I'm Duke\nWhat can I do for you?";
+        String goodbye = "\nBye. Hope to see you again soon!";
+        String result = hLine + introduction + hLine + goodbye + hLine;
+        System.out.println(result);
     }
 }
