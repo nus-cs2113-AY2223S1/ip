c059384a81bbf1c7f1c117bb27c7143652a41fff
owenl131 <owenl131@gmail.com>
2022-08-19 23:17:32 +0800

    Print status in list command

--- src/main/java/ListManager.java
+++ src/main/java/ListManager.java
@@ -2,20 +2,24 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ListManager {
-    List<String> items;
+    List<ListItem> items;
 
     public ListManager() {
         items = new ArrayList<>();
     }
 
     public void addItem(String item) {
-        items.add(item);
+        items.add(new ListItem(item));
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Here are the tasks in your list:\n");
         for (int i = 0; i < items.size(); i++) {
-            sb.append(String.format("%d. %s\n", i + 1, items.get(i)));
+            sb.append(String.format("%d.[%s] %s\n",
+                    i + 1,
+                    items.get(i).isDone() ? "X" : " ",
+                    items.get(i).getName()));
         }
         return sb.toString();
     }
