c7428f82bb008a89435bec66bfcea6a45bb11a63
bdthanh <bdthanh200403@gmail.com>
2022-08-19 12:39:15 +0800

    Add Increment Level-0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,31 @@
 public class Duke {
-    public static void main(String[] args) {
+
+    private void reply (String message) {
+        System.out.print(message);
+        System.out.println("____________________________________________________________");
+    }
+    private void greet() {
+        String greetMessage = "Hello! I'm Banana\n"
+                + "What can I do for you?\n";
+        reply(greetMessage);
+    }
+
+    private void bye() {
+        String byeMessage = "Bye. Hope to see you again soon!\n";
+        reply(byeMessage);
+    }
+
+    private void logo() {
         String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-        String divider = "____________________________________________________________\n";
-        String greet = divider
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + divider
-                + "Bye. Hope to see you again soon!\n"
-                + divider;
-        System.out.println(greet);
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        reply(logo);
+    }
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.greet();
+        duke.bye();
     }
 }
