f50aae479dac34800ca88c15c7537ed4e4a2bdf0
ngdeqi <deqi.angel@gmail.com>
2022-08-19 23:51:27 +0800

    Add Level 2 features: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,62 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static class List {
+        private ArrayList<String> tasks = new ArrayList<>();
+
+        public void addTask(String task) {
+            tasks.add(task);
+            printWithFormat("added: " + task);
+        }
+
+        public void printTasks() {
+            int counter = 1;
+            String output = "";
+            for (String task : tasks) {
+                output = output.concat(counter++ + ". " + task + "\n");
+            }
+            printWithFormat(output);
+        }
+    }
+
+    public static void printWithFormat(String input) {
+        String output = "";
+        String[] split = input.split("\n");
+        String line = "    ____________________________________________________________";
+        System.out.println(line);
+        for (String string: split) {
+            output = output.concat( "     " + string + "\n");
+        }
+        System.out.print(output);
+        System.out.println(line);
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input, output;
-        output =
-                "    ____________________________________________________________\n"
-                + "    Hello! I'm DuckyMoMo\n"
-                + "    What can I do for you?\n"
-                + "    ____________________________________________________________\n";
-        System.out.println(output);
+        List list = new List();
+        output = "Hello! I'm DuckyMoMo\n"
+                + "What can I do for you?";
+        printWithFormat(output);
 
 
         input = sc.nextLine();
-        while (!input.equals("bye")) {
-            output = "    ____________________________________________________________\n"
-                    + "    " + input + "\n"
-                    + "    ____________________________________________________________\n";
-            System.out.println(output);
+        while (true) {
+            switch (input) {
+            case "bye" :
+                printWithFormat("Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+                return;
+            case "list":
+                list.printTasks();
+                break;
+            default:
+                list.addTask(input);
+            }
+
             input = sc.nextLine();
         }
 
-        output = "    ____________________________________________________________\n"
-                + "    Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n"
-                + "    ____________________________________________________________\n";
-        System.out.println(output);
     }
 }
