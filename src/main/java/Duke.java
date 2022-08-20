f8bfe34b7ece30fe2637106527c7be78dd46627e
chydarren <chydarren@gmail.com>
2022-08-20 08:05:48 +0800

    Add ability to echo commands entered by the user and exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void printGreeting() {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣴⣶⣶⣶⣶⣶⠶⣶⣤⣤⣀⠀⠀⠀⠀⠀⠀ \n"
                 + "⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⠁⠀⢀⠈⢿⢀⣀⠀⠹⣿⣿⣿⣦⣄⠀\n"
                 + "⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⠿⠀⠀⣟⡇⢘⣾⣽⠀⠀⡏⠉⠙⢛⣿⣷⡖⠀\n"
                 + "⠀⠀⠀⠀⠀⣾⣿⣿⡿⠿⠷⠶⠤⠙⠒⠀⠒⢻⣿⣿⡷⠋⠀⠴⠞⠋⠁⢙⣿⣄ \n"
                 + "⠀⠀⠀⠀⢸⣿⣿⣯⣤⣤⣤⣤⣤⡄⠀⠀⠀⠀⠉⢹⡄⠀⠀⠀⠛⠛⠋⠉⠹⡇ \n"
                 + "⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣼⣇⣀⣀⣀⣛⣛⣒⣲⢾⡷ \n"
-                + "⢀⠤⠒⠒⢼⣿⣿⠶⠞⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⣼⠃\n"
+                + "⢀⠤⠒⠒⢼⣿⣿⠶⠞⢻⣿⣿;i⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⣼⠃\n"
                 + "⢮⠀⠀⠀⠀⣿⣿⣆⠀⠀⠻⣿⡿⠛⠉⠉⠁⠀⠉⠉⠛⠿⣿⣿⠟⠁⠀⣼⠃\n"
                 + "⠈⠓⠶⣶⣾⣿⣿⣿⣧⡀⠀⠈⠒⢤⣀⣀⡀⠀⠀⣀⣀⡠⠚⠁⠀⢀⡼⠃\n"
                 + "⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣤⣭⣭⣭⣭⣭⣥⣤⣤⣤⣴⣟⠁\n";
 
-        System.out.println("____________________________________________________________");
-        System.out.println(logo + "\nKon'nichiwa! Doraemon desu ≧◉◡◉≦\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println(logo + "\n    Kon'nichiwa! Doraemon desu ≧◉◡◉≦\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printGreeting();
+        Scanner in = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = in.nextLine();
+            if (!command.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + command);
+                System.out.println("    ____________________________________________________________");
+            }
+            else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Sayonara. Hope to see you again soon! ✪");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+        }
     }
 }
