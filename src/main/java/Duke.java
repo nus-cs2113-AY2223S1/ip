7701c4fafdf562d375be8c46c1a8b5da0ca256b4
xzynos <lim_fang_jie@hotmail.com>
2022-08-20 11:04:24 +0800

    Add functions to print logo, greeting and goodbye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,39 @@ import java.util.Scanner;
 public class Duke {
     public static final String HORIZONTAL_LINE = "____________________________________________________________";
 
-    public static void main(String[] args) {
+    /**
+     * Prints logo to standard out.
+     */
+    public static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
 
-        System.out.println(HORIZONTAL_LINE);
+    /**
+     * Prints greeting message to standard out.
+     */
+    public static void printGreetingMessage() {
         String greetingMessage = "Hello! I'm Duke\n"
                 + "What can I do for you?";
         System.out.println(greetingMessage);
+    }
+
+    /**
+     * Prints goodbye message to standard out.
+     */
+    public static void printGoodbyeMessage() {
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+    }
+
+    public static void main(String[] args) {
+        printLogo();
+        System.out.println(HORIZONTAL_LINE);
+        printGreetingMessage();
         System.out.println(HORIZONTAL_LINE);
 
         Scanner scanner = new Scanner(System.in);
@@ -30,8 +51,7 @@ public class Duke {
         }
 
         System.out.println(HORIZONTAL_LINE);
-        String goodbyeMessage = "Bye. Hope to see you again soon!";
-        System.out.println(goodbyeMessage);
+        printGoodbyeMessage();
         System.out.println(HORIZONTAL_LINE);
     }
 }
