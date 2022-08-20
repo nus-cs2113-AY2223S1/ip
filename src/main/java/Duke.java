2fd31abb6abf4df37c7445ae7ee6a84b4113ab35
dhanish265 <dhanish265@gmail.com>
2022-08-20 19:53:37 +0800

    add Level-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,17 +9,41 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        printLine();
-        System.out.println("Hello! I'm Duke!");
-        System.out.println("What can I do for you?");
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        greet();
+        echo();
     }
 
     public static void printLine(){
+        System.out.print("\t");
         for(int i = 0; i < 60; i++){
             System.out.print("\u2500");
         }
         System.out.println();
+    }
+
+    public static void greet(){
+        printLine();
+        System.out.println("\tHello! I'm Duke!");
+        System.out.println("\tWhat can I do for you?");
+        printLine();
+    }
+
+    public static void echo(){
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        input = scanner.nextLine();
+        while(!input.equals("bye")){
+            printLine();
+            System.out.println("\t" + input);
+            printLine();
+            input = scanner.nextLine();
+        }
+        exit();
+    }
+
+    public static void exit(){
+        printLine();
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        printLine();
     }
 }
