0878a05509ab6a7ec1031c5e1c6867328f91fd82
Brominne <e0773671@u.nus.edu>
2022-08-21 00:23:00 +0800

    Add greet and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
+
+    public static void separateLine() {
+        String str = "";
+        for(int i = 0; i < 60; i++) {
+            str += '_';
+        }
+        System.out.println(str);
+    }
+
+    public static void greet() {
+        separateLine();
+        System.out.println("Hello! I'm Rae.");
+        System.out.println("How does everything progree now?");
+    }
+
+    public static void bye() {
+        separateLine();
+        System.out.println("Bye! Always there waiting for you!");
+        separateLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        bye();
     }
 }
