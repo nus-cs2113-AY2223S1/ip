70286ec5a666bb37932b3d28b9aba15cd8541c41
nazrul0 <nazrulsyahmi00@gmail.com>
2022-08-20 18:51:40 +0800

    added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void handleInput() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        if (!line.equals("bye")) {
+            System.out.println("        ____________________________________________");
+            System.out.println("        " + line);
+            System.out.println("        ____________________________________________");
+            handleInput();
+        }
+
+        return;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,6 +26,9 @@ public class Duke {
         System.out.println("____________________________________________");
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________");
+        handleInput();
+
         System.out.println("____________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________");
