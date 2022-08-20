14fc657d5f1aebaeba2ec76291966c27724bbb4b
xzynos <lim_fang_jie@hotmail.com>
2022-08-20 09:58:50 +0800

    Add code to read and echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,15 +11,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________");
-        String greetingMessage = " Hello! I'm Duke\n"
-                + " What can I do for you?";
+        System.out.println(HORIZONTAL_LINE);
+        String greetingMessage = "Hello! I'm Duke\n"
+                + "What can I do for you?";
         System.out.println(greetingMessage);
+        System.out.println(HORIZONTAL_LINE);
 
-        System.out.println("____________________________________________________________");
-        String goodbyeMessage = " Bye. Hope to see you again soon!";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println();
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(input);
+            System.out.println(HORIZONTAL_LINE);
+        }
+
+        System.out.println(HORIZONTAL_LINE);
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
         System.out.println(goodbyeMessage);
-
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
