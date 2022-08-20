133d3c73f6de0dd7e21de28b5829a61422d40720
wangtingjia <wangtingjia49@gmail.com>
2022-08-20 16:19:28 +0800

    Add mark and unmark features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,38 @@ import java.util.Scanner;
 
 public class Duke {
     static String[] tasks = new String[100];
+    static boolean[] isDone = new boolean[100];
     static int taskCount = 0;
     static void print() {
         for (int i = 0; i < taskCount; i++) {
             System.out.print(i);
-            System.out.println(" " + tasks[i]);
+            System.out.print(" " + tasks[i] + " [");
+            if (isDone[i]) {
+                System.out.println("X]");
+            } else {
+                System.out.println(" ]");
+            }
         }
     }
     static void add(String input) {
         tasks[taskCount] = input;
+        isDone[taskCount] = false;
         taskCount++;
-        System.out.println("Echo: Added " + input);
+        System.out.print("Echo: Added " + input);
+    }
+    static void mark(String input) {
+        int taskNum = Integer.parseInt(input.substring(input.length() - 1));
+        if (taskNum < taskCount) {
+            isDone[taskNum] = true;
+            System.out.println("Marked as done: " + tasks[taskNum]);
+        }
+    }
+    static void unmark(String input) {
+        int taskNum = Integer.parseInt(input.substring(input.length() - 1));
+        if (taskNum < taskCount && isDone[taskNum]) {
+            isDone[taskNum] = false;
+            System.out.println("Removed marking: " + tasks[taskNum]);
+        }
     }
     public static void main(String[] args) {
         String greeting = "Hello! I'm Timmy\n What can I do for you?\n \n";
@@ -28,6 +49,10 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 print();
+            } else if (input.startsWith("mark")){
+                mark(input);
+            } else if (input.startsWith("unmark")) {
+                unmark(input);
             } else {
                 add(input);
             }
