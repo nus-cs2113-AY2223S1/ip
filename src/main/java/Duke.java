cd0efd44e5e147a3cc1dee66b561d387f0692e47
alfred-leong <alfredleongweilun@gmail.com>
2022-08-21 00:23:48 +0800

    Completed Level 1(Greet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,33 @@
+import java.util.*;
+
 public class Duke {
     public static void drawLine() {    //print underscore symbol 50 times
         System.out.println("__________________________________________________");
     }
 
     public static void welcomeUser() {
-        System.out.println("Hello! I'm Duke");
+        drawLine();
+        System.out.println("Hello! I'm Alpiit");
         System.out.println("What can I do for you?");
+        drawLine();
     }
 
-    public static void byeUser() {
-        System.out.println("Bye. Hope to see you again soon!");
+    public static void echoUser() {
+        Scanner in = new Scanner(System.in);
+        String command = "";
+        do {
+            command = in.nextLine();
+            drawLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+            drawLine();
+        } while (command != "bye");
+        System.out.println("Bye. Hope to see you again!");
+        drawLine();
     }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,9 +35,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        drawLine();
         welcomeUser();
-        drawLine();
-        byeUser();
+        echoUser();
     }
 }
