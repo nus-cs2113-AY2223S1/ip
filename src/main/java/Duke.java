3fe26b2eb95e1a8e1b45dacb9aafa94cbfcf06c8
wangtingjia <wangtingjia49@gmail.com>
2022-08-20 15:29:33 +0800

    Add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting =
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n"
-                + "\n"
-                + "Bye. Hope to see you again soon\n";
+        String greeting = "Hello! I'm Timmy\n What can I do for you?\n \n";
+
         System.out.println(greeting);
+
+        //Echo
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("Echo: " + input);
+            System.out.println();
+            input = in.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon\n");
     }
 }
+
