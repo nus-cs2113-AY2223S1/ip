f95f996df1aa81e739623728488b43741d1440ed
ngdeqi <deqi.angel@gmail.com>
2022-08-20 11:25:59 +0800

    Add Level-3 features: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,6 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static class List {
-        private ArrayList<String> tasks = new ArrayList<>();
-
-        public void addTask(String task) {
-            tasks.add(task);
-            printWithFormat("added: " + task);
-        }
-
-        public void printTasks() {
-            int counter = 1;
-            String output = "";
-            for (String task : tasks) {
-                output = output.concat(counter++ + ". " + task + "\n");
-            }
-            printWithFormat(output);
-        }
-    }
 
     public static void printWithFormat(String input) {
         String output = "";
@@ -34,26 +16,35 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String input, output;
-        List list = new List();
+        TaskManager taskManager = new TaskManager();
+        Scanner sc = new Scanner(System.in);
+
         output = "Hello! I'm DuckyMoMo\n"
                 + "What can I do for you?";
         printWithFormat(output);
 
-
         input = sc.nextLine();
         while (true) {
-            switch (input) {
+            String[] arguments = input.split("[ \t]+");
+
+            switch (arguments[0]) {
             case "bye" :
                 printWithFormat("Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
                 return;
             case "list":
-                list.printTasks();
+                output = taskManager.getTasks();
+                break;
+            case "mark":
+                output = taskManager.markTask(Integer.parseInt(arguments[1]));
+                break;
+            case "unmark":
+                output = taskManager.unmarkTask(Integer.parseInt(arguments[1]));
                 break;
             default:
-                list.addTask(input);
+                output = taskManager.addTask(input);
             }
+            printWithFormat(output);
 
             input = sc.nextLine();
         }
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class TaskManager {
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    public TaskManager() {
+    }
+
+    public String addTask(String task) {
+        tasks.add(new Task(task));
+        return "added: " + task;
+    }
+
+    public String markTask(int taskNum) {
+        Task task =  tasks.get(taskNum-1);
+        task.markAsDone();
+        return "Nice! I've marked this task as done:\n"
+                + "  [X] "
+                + task.getDescription() + "\n";
+    }
+
+    public String unmarkTask(int taskNum) {
+        Task task =  tasks.get(taskNum-1);
+        task.markAsNotDone();
+        return "OK, I've marked this task as not done yet:\n"
+                + "  [ ] "
+                + task.getDescription() + "\n";
+    }
+
+    public String getTasks() {
+        int counter = 1;
+        String output = "Here are the tasks in your list:\n";
+        for (Task task: tasks) {
+            output = output.concat(counter++ + "."
+                    + "[" + task.getStatusIcon() + "] "
+                    + task.getDescription()
+                    + "\n");
+        }
+        return output;
+    }
+
+}
