878c778197b2508242ae4467258f0596e34e2dcd
joshuan98 <dzhei98@gmail.com>
2022-08-21 20:05:32 +0800

    Add functionality to store input
    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String HORIZONTAL_LINE = "______________________________";
+    private static String[] list;
+    private static int numTasks;
 
     private static void printIntroduction() {
         final String INTRODUCTION =
@@ -22,26 +24,54 @@ public class Duke {
         System.out.println(HORIZONTAL_LINE);
     }
 
-    private static void echoInput() {
-        final String EXIT_PHRASE = "bye";
-
+    private static void handleInput() {
         Scanner in = new Scanner(System.in);
         String input;
 
-        input = in.nextLine();
+        final String EXIT_PHRASE = "bye";
+        final String LIST_PHRASE = "list";
 
-        while (!input.equals(EXIT_PHRASE)) {
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println(input);
-            System.out.println(HORIZONTAL_LINE);
-
+        while (true) {
             input = in.nextLine();
+
+            if (input.equals(EXIT_PHRASE)) {
+                break;
+            } else if (input.equals(LIST_PHRASE)) {
+                printList();
+            } else {
+                addToList(input);
+            }
         }
     }
 
+    private static void addToList(String text) {
+        final String ADD_PHRASE = "added: ";
+
+        list[numTasks] = text;
+        numTasks += 1;
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(ADD_PHRASE + text);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static void printList() {
+        System.out.println(HORIZONTAL_LINE);
+
+        for (int i = 0; i < numTasks; i += 1) {
+            System.out.println(Integer.toString(i + 1) + ". " + list[i]);
+        }
+
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
+        final int MAX_NUM_OF_TASKS = 100;
+        list = new String[MAX_NUM_OF_TASKS];
+        numTasks = 0;
+
         printIntroduction();
-        echoInput();
+        handleInput();
         printExit();
     }
 }
