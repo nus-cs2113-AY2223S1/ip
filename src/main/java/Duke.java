be4812617691f415477e69035b09b4442911fc9c
eehongchan <eehongchan@gmail.com>
2022-08-22 00:31:42 +0800

    Add functionality to echo user input and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-
+        String line;
+        Scanner in = new Scanner(System.in);
+        // Greet user
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+
+        // Wait for input
+        do {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        line +
+                        "\n____________________________________________________________\n");
+            }
+        } while (!line.equals("bye"));
+        System.out.println("____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________");
+
     }
 }
