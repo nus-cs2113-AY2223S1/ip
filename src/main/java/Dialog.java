6a787de91e3e40a6b492c9a29730ae53a0df43ed
Malcolm Ang <malcolmangsc@gmail.com>
2022-08-21 09:53:54 +0800

    Fix minor bugs relating to dialog box enforcement
    The dialog box should be more robust and able to take in words longer than the max line space.

--- src/main/java/Dialog.java
+++ src/main/java/Dialog.java
@@ -13,29 +13,24 @@ public class Dialog {
     public Dialog(String text, int consoleWidth, int boxHorizontalPosition) {
         this.maxRowLength = consoleWidth - boxHorizontalPosition - this.HORIZONTAL_PADDING*2 - 2;
         for (String row : text.split("\n")) {
-            boolean isFirstLine = true;
             if (row.length() > maxRowLength) {
                 String bufferString = "";
                 String indent = "";
                 int indentLength = 0;
                 for (String word : row.split(" ")){
                     if (word.length() > maxRowLength){
-                        word = bufferString.trim() + " " + word;
-                        bufferString = generateIndent(INDENT_SIZE);
+                        word = indent + (bufferString.isEmpty() ? bufferString : bufferString.trim() + " ") + word;
+                        bufferString = generateIndent(indentLength);
                         for (int i = 0; i < word.length(); i += maxRowLength) {
-                            if (isFirstLine){
-                                textRows.add(word.substring(i, i + maxRowLength));
-                                isFirstLine = false;
-                            }
-                            else {
-                                textRows.add(generateIndent(INDENT_SIZE) + word.substring(i, Math.min(word.length(), i + maxRowLength - INDENT_SIZE)));
-                            }
+                            textRows.add(indent + word.substring(i, Math.min(word.length(), i + maxRowLength - indentLength)));
+                            indentLength = INDENT_SIZE;
+                            indent = generateIndent(INDENT_SIZE);
+                            bufferString = indent;
                         }
                     }
                     else {
                         if (bufferString.length() + word.length() <= maxRowLength - indentLength) {
                             bufferString += word + " ";
-                            isFirstLine = false;
                         } else {
                             indentLength = INDENT_SIZE;
                             indent = generateIndent(indentLength);
@@ -44,7 +39,7 @@ public class Dialog {
                         }
                     }
                 }
-                if (!bufferString.isEmpty() || !bufferString.equals(generateIndent(INDENT_SIZE))){
+                if (!bufferString.isEmpty() && !bufferString.equals(generateIndent(INDENT_SIZE))){
                     textRows.add(bufferString.substring(0, bufferString.length()-1));
                 }
             }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 public class Duke {
     public static Display window = new Display();
     public static void main(String[] args) {
-        window.print("Hey, welcome!\nWhat would you like to do today?");
-        while(!echo().toLowerCase().equals("bye")){
+        window.print("Good morning!\nWhat would you like to do today?");
+        while(!echo().equalsIgnoreCase("bye")){
 
         }
         window.print("Alright, see you around then!");
