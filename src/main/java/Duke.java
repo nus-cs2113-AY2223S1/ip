277e7e63bcf5b91d0970d81118694e43a46cf284
joshuan98 <dzhei98@gmail.com>
2022-08-21 18:13:49 +0800

    Update coding style for constants
    Update horizontal_line, introduction, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,14 @@ public class Duke {
         // + "| |_| | |_| | < __/\n"
         // + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
-        String horizontal_line = "______________________________";
-        String introduction = "Hihi, my name is Jay!\nWhat can I do for you today?";
-        String exit = "Goodbye! Hope to see you again!";
+        String HORIZONTAL_LINE = "______________________________";
+        String INTRODUCTION = "Hihi, my name is Jay!\nWhat can I do for you today?";
+        String EXIT = "Goodbye! Hope to see you again!";
 
-        System.out.println(horizontal_line);
-        System.out.println(introduction);
-        System.out.println(horizontal_line);
-        System.out.println(exit);
-        System.out.println(horizontal_line);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INTRODUCTION);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(EXIT);
+        System.out.println(HORIZONTAL_LINE);
     }
 }
