01ed5c4fb52b460886305aa919d1dc4ca87db364
LokQiJun <e0544063@u.nus.edu>
2022-08-21 18:26:45 +0800

    Added level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,24 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputs;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         greetings();
+        inputs = scanner.nextLine();
+        while(!inputs.equals("bye") ) {
+            System.out.println(inputs + '\n');
+            inputs = scanner.nextLine();
+        }
         exit();
     }
 
-    private static void exit(){
+    private static void exit() {
         System.out.println(" Bye! Hope to see you again soon :)");
     }
 
