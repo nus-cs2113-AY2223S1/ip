0199be48cbcf99034071821fbafe4ff56f7475ba
LokQiJun <e0544063@u.nus.edu>
2022-08-21 19:10:35 +0800

    Added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,58 @@
 import java.util.*;
 public class Duke {
+    private static ArrayList<Task> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputs;
+        String[] splitInputs;
+        doGreetings();
+        inputs = scanner.nextLine();
+        while(!inputs.equals("bye") ) {
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            splitInputs = inputs.split(" ");
+            if(inputs.equals("list")){
+                doPrintlist();
+            }
+            else {
+                System.out.println("\t Added: " + inputs);
+                list.add(new Task(inputs));
+            }
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            inputs = scanner.nextLine();
+        }
+        doExit();
+    }
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+    }
+
+    public static void doPrintlist() {
+        for (int i = 0; i < list.size();i++) {
+            System.out.println(i+1 + ": " + " [" + list.get(i).getStatusIcon() + "] " + list.get(i).description );
+        }
+    }
+
+    public static void doExit() {
+        System.out.println(" Bye! Hope to see you again soon :)");
+    }
+
+    public static void doGreetings(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        greetings();
-        inputs = scanner.nextLine();
-        while(!inputs.equals("bye") ) {
-            System.out.println(inputs + '\n');
-            inputs = scanner.nextLine();
-        }
-        exit();
-    }
-
-    private static void exit() {
-        System.out.println(" Bye! Hope to see you again soon :)");
-    }
-
-    private static void greetings(){
-        System.out.println(" Hello! I'm Duke\n" + " What can I do for you?");
+        System.out.println("Hello from\n" + logo + "\n \n Hello! I'm Duke\n What can I do for you?");
     }
 }
