0b5a21295d9ddbdf33f45cbe3a2c9782920d85a0
LAPTOP-33NS3HEM\Yuxin <e0772744@u.nus.edu>
2022-08-21 19:46:00 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,31 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void response (String line) {
+    public static void listing (String[] listOfThings, int count) {
+        for (int i = 0; i < count; i += 1) {
+            int index = i + 1;
+            System.out.println("\t" + index + ". " + listOfThings[i]);
+        }
+    }
+
+    public static int response (String line, String[] listOfThings, int count) {
         System.out.println("  ____________________________________________________________");
-        System.out.println("\t" + line);
+        if (line.equals("list")) {
+            listing(listOfThings, count);
+        } else {
+            listOfThings[count] = line;
+            System.out.println("\tAdded: " + line);
+            count += 1;
+        }
         System.out.println("  ____________________________________________________________");
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String line;
+        String[] listOfThings = new String[100];
+        int count = 0;
         System.out.println("  ____________________________________________________________");
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
@@ -23,7 +39,7 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("  ____________________________________________________________");
             } else {
-                response(line);
+                count = response(line, listOfThings, count);
             }
         } while (!line.equals("bye"));
     }
