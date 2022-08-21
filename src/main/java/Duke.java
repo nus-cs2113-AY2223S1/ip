8ed2a83864ed996b44c9215182c1d37b93ab85bf
bdthanh <bdthanh200403@gmail.com>
2022-08-21 18:12:05 +0800

    Finish Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class Duke {
     private boolean isActive = true;
+    private String[] tasks = new String[100];
+    private int taskCount = 0;
     private void greet() {
         String greetMessage = "Hello! I'm Banana\n"
                 + "How can I help you?";
@@ -23,25 +26,44 @@ public class Duke {
         }
         System.out.println("*************************************************************");
     }
+    private void confirmAdd(String task) {
+        this.reply("added: " + task);
+    }
+    private void printList(String[] tasks) {
+        int i = 0;
+        if (tasks[0] == null) {
+            this.reply("There is nothing in the list right now");
+        } else {
+            System.out.println("*************************************************************");
+            for (; i < 100 && tasks[i] != null; i++) {
+                System.out.println("      " + String.valueOf(i + 1) + ". " + tasks[i]);
+            }
+            System.out.println("*************************************************************");
+        }
+    }
     private void parseMessage(String message) {
         switch(message) {
         case "bye":
             this.bye();
             this.isActive = false;
             break;
+        case "list":
+            this.printList(tasks);
+            break;
         default:
-            reply(message);
+            tasks[taskCount++] = message;
+            this.confirmAdd(message);
         }
     }
     public static void main(String[] args) {
-        Duke banana = new Duke();
-        banana.greet();
+        Duke duke = new Duke();
+        duke.greet();
 
         Scanner commands = new Scanner(System.in);
         String command;
-        while (banana.isActive) {
+        while (duke.isActive) {
             command = commands.nextLine();
-            banana.parseMessage(command);
+            duke.parseMessage(command);
         }
     }
 }
