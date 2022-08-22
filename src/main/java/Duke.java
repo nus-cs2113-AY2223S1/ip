ec00982c3e19b4e2af0054c4bef58f61e0f3a46e
Devesh Logendran <deveshl@users.noreply.github.com>
2022-08-22 20:39:12 +0800

    Add command processor, user input routine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    static final String LOGO = "$$$$$$$\\                  $$\\           \n" +
+            "$$  __$$\\                 $$ |          \n" +
+            "$$ |  $$ |$$\\   $$\\  $$$$$$$ | $$$$$$\\  \n" +
+            "$$ |  $$ |$$ |  $$ |$$  __$$ |$$  __$$\\ \n" +
+            "$$ |  $$ |$$ |  $$ |$$ /  $$ |$$$$$$$$ |\n" +
+            "$$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|\n" +
+            "$$$$$$$  |\\$$$$$$  |\\$$$$$$$ |\\$$$$$$$\\ \n" +
+            "\\_______/  \\______/  \\_______| \\_______|";
+    static final String GREET = "I am Dude.\n" +
+            "Type something in, dude.";
+    static final String BYE = "Catch you later, dude.";
+
+    public static void runCommand(String command) {
+        if (command.startsWith("bye")) {
+            System.out.println(BYE);
+            System.exit(0);
+        } else {
+            System.out.println(command);
+        }
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
-        String logo = "$$$$$$$\\                  $$\\           \n" +
-                "$$  __$$\\                 $$ |          \n" +
-                "$$ |  $$ |$$\\   $$\\  $$$$$$$ | $$$$$$\\  \n" +
-                "$$ |  $$ |$$ |  $$ |$$  __$$ |$$  __$$\\ \n" +
-                "$$ |  $$ |$$ |  $$ |$$ /  $$ |$$$$$$$$ |\n" +
-                "$$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|\n" +
-                "$$$$$$$  |\\$$$$$$  |\\$$$$$$$ |\\$$$$$$$\\ \n" +
-                "\\_______/  \\______/  \\_______| \\_______|";
-        String greet = "I am Dude.\n" +
-                "Type something in, dude.";
-        String bye = "Catch you later, dude.";
-        System.out.println(logo);
+
+        System.out.println(LOGO);
         System.out.println();
-        System.out.println(greet);
+        System.out.println(GREET);
         System.out.println();
-        System.out.println(bye);
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            runCommand(input.nextLine());
+        }
+
     }
 }
