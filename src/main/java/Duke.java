9d059b6874f15b668c643f16a46994654cd8477b
chinhan99 <braydenyong@yahoo.com.sg>
2022-08-22 18:46:16 +0800

    Added list incrementing and listing feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,40 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[101];
+        int taskIndex = 1;
         String inData = "0";
         Scanner scan = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        while (!inData.equals("bye")) {
-
+        while (true) {
+            //Continuously receive user input
             inData = scan.nextLine();
-            System.out.println("____________________________________________________________");
-            System.out.println(inData);
-            if (!inData.equals("bye")) {
+            if (inData.equals("list")) {
+                //Print entire list if input is equal to "list"
+                for (int i = 1; i < taskIndex; i += 1) {
+                    System.out.println(i + ". " + tasks[i]);
+                }
                 System.out.println("____________________________________________________________");
+            } else {
+                if (inData.equals("bye")) {
+                    //If input equals to "bye", exit the while-loop
+                    break;
+                } else {
+                    if (taskIndex < 101) {
+                        //If the number of elements within task[] is within 100, continue to add items to task[].
+                        tasks[taskIndex] = inData;
+                        taskIndex += 1;
+                        System.out.println("____________________________________________________________");
+                        System.out.println("added: " + inData);
+                        System.out.println("____________________________________________________________");
+                    }
+                }
             }
         }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
