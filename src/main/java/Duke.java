b0eb4b7661ecef6a8ac0b838acd1c01c1bd830f1
gaoyunfan <gaoyunfan8@gmail.com>
2022-08-22 19:21:02 +0800

    Add text storage

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,13 +5,23 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         String input;
+        String[] texts = new String[100];
+        int textCount = 0;
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
-        if (input.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                for (int i = 0; i < textCount; i++) {
+                    System.out.println(i + 1 + ". " + texts[i]);
+                }
+            }
+            else {
+                texts[textCount] = input;
+                textCount++;
+                System.out.println("added: " + input);
+            }
+            input = in.nextLine();
         }
-        else {
-            System.out.println(input);
-        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
