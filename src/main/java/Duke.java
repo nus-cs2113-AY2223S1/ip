42f285f89965aa993e83c767e31a5e4656d3e9a8
chinhan99 <braydenyong@yahoo.com.sg>
2022-08-22 17:27:32 +0800

    Removed unnecessary comment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-//        String str = "Hello! I'm  Duke;
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("____________________________________________________________");
