c20f4fc1687ee7682c643a29788822f4990e797a
gaoyunfan <gaoyunfan8@gmail.com>
2022-08-22 13:43:14 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(input);
+        }
     }
 }
