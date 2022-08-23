b381fb04648951a939a8dbd2da525ec7d88410ae
wcwy <thinhong99@gmail.com>
2022-08-24 00:46:28 +0800

    Modify echo method to add method and add list method in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,33 +8,48 @@ public class Duke {
                 + "____________________________________________________________";
         System.out.println(entryBanner);
     }
-
-    public static void echo(String userInput){
+    public static void add(String userInput){
         String echoReply = "____________________________________________________________\n"
-                + userInput + "\n"
+                + "added: " + userInput + "\n"
                 + "____________________________________________________________";
         System.out.println(echoReply);
     }
-
+    public static void list(String[] inputList, int count){
+        String listing = "____________________________________________________________\n";
+        for(int i=0; i<count; ++i){
+            listing += String.format("%d. %s\n", i+1, inputList[i]);
+        }
+        listing += "____________________________________________________________";
+        System.out.println(listing);
+    }
     public static void quit(){
         String exitBanner = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
         System.out.println(exitBanner);
     }
-
     public static void main(String[] args) {
         greet();
         String userInput = "";
         Scanner in = new Scanner(System.in);
-        while(true){
+        String[] listings = new String[100];
+        int count = 0;
+
+        while(!userInput.equals("bye")) {
             userInput = in.nextLine();
-            if(userInput.equals("bye")){
+            switch (userInput) {
+            case "list":
+                list(listings, count);
                 break;
-            }else{
-                echo(userInput);
+            case "bye":
+                quit();
+                break;
+            default:
+                listings[count] = userInput;
+                add(userInput);
+                count++;
+                break;
             }
         }
-        quit();
     }
 }
