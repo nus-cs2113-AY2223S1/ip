1d33524984321cbd7f344e3d9f1516b83c0326c4
Paul <iampaullow@gmail.com>
2022-08-24 01:48:56 +0800

    Add Level 1 Greet, Echo Exit
    Program terminates automatically after 5 echos

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\nBye. Hope to see you again soon!\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        int count = 0;
+        while (count < 5 && !line.equals("bye")) {
+            System.out.println(line + "\n");
+            line = in.nextLine();
+            count++;
+        }
+
+
+        System.out.println("Bye good friend! Hope to see you again soon!\n" + logo);
     }
 }
