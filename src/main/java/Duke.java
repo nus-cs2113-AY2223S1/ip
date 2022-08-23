86d8247b1bf391b374d31c3a9622cdf3491e607e
redders7 <e0726220@u.nus.edu>
2022-08-23 14:34:26 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        System.out.println(line+ " Hello! I'm Duke\n" + " What can I do for you?\n" + line + " Bye. Hope to see you again soon!\n" + line);
+        Scanner cmd = new Scanner(System.in);
+
+        System.out.println(line + " Hello! I'm Tommy\n" + " What can I do for you?\n" + line);
+
+        String foo = cmd.nextLine();
+
+        switch (foo) {
+        case "list":
+            System.out.println(line + " list\n" + line);
+            foo = cmd.nextLine();
+        case "blah":
+            System.out.println(line + " blah\n" + line);
+            foo = cmd.nextLine();
+        case "bye":
+            System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
+            break;
+        default:
+            break;
+        }
     }
 }
