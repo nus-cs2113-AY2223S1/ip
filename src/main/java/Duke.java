145436cc202292a78a3656b8a4f61c75b1f9795d
wcwy <thinhong99@gmail.com>
2022-08-24 00:23:57 +0800

    Add echo feature in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void greet(){
         String entryBanner = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
-                + "What can I do for you?";
+                + "What can I do for you?\n"
+                + "____________________________________________________________";
         System.out.println(entryBanner);
+    }
+
+    public static void echo(String userInput){
+        String echoReply = "____________________________________________________________\n"
+                + userInput + "\n"
+                + "____________________________________________________________";
+        System.out.println(echoReply);
     }
 
     public static void quit(){
@@ -15,6 +25,16 @@ public class Duke {
 
     public static void main(String[] args) {
         greet();
+        String userInput = "";
+        Scanner in = new Scanner(System.in);
+        while(true){
+            userInput = in.nextLine();
+            if(userInput.equals("bye")){
+                break;
+            }else{
+                echo(userInput);
+            }
+        }
         quit();
     }
 }
