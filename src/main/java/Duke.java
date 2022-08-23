1e15ac9b8a64d74bb23b7ab53277660924a96bb7
redders7 <e0726220@u.nus.edu>
2022-08-23 18:00:33 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         Scanner cmd = new Scanner(System.in);
-        String[] words = new String[100];
+        Task[] list = new Task[100];
         int cnt = 0;
         Boolean last = false;
 
@@ -12,27 +12,48 @@ public class Duke {
 
         do {
             String foo = cmd.nextLine();
-            switch (foo) {
-            case "list":
+
+            if (foo.contains("unmark")) {
+                String a = foo.substring(7);
+                int index = Integer.parseInt(a) - 1;
+                list[index].setUnDone();
+                System.out.println(line + " OK, I've marked this task as not done yet:");
+                System.out.println("  [" +list[index].getStatusIcon() + "]" + list[index].description);
                 System.out.println(line);
-                for (int i = 0; i < cnt; i++) {
-                    System.out.println(i + 1 + ". " + words[i]);
-                }
-                System.out.println(line);
-                break;
-            case "blah":
-                System.out.println(line + " blah\n" + line);
-                break;
-            case "bye":
-                last = true;
-                break;
-            default:
-                words[cnt] = foo;
-                cnt++;
-                System.out.println(line + "added: " + foo + "\n" + line);
-                break;
             }
-        } while (last==false);
+            else if (foo.contains("mark")) {
+                String a = foo.substring(5);
+                int index = Integer.parseInt(a) - 1;
+                list[index].setDone();
+                System.out.println(line + "Nice! I've marked this task as done:");
+                System.out.println("  [" + list[index].getStatusIcon() + "] " + list[index].description);
+                System.out.println(line);
+            }
+            else {
+                switch (foo) {
+                case "list":
+                    System.out.println(line);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < cnt; i++) {
+                        System.out.println((i + 1) + ".[" + list[i].getStatusIcon() + "]" + list[i].description);
+                    }
+                    System.out.println(line);
+                    break;
+                case "blah":
+                    System.out.println(line + " blah\n" + line);
+                    break;
+                case "bye":
+                    last = true;
+                    break;
+                default:
+                    list[cnt] = new Task(foo);
+                    cnt++;
+                    System.out.println(line + "added: " + foo + "\n" + line);
+                    break;
+                }
+            }
+        } while (!last);
+
         System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
     }
 }
