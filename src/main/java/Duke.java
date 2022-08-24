e78b7585d72c0a93d410bb45186d3f74da1417cb
deenliong <deenliong@gmail.com>
2022-08-25 01:14:18 +0800

    Completed L2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,20 +9,30 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-//        line = in.nextLine();
 
-        do{
+//        String[] toDoList = new String[100];
+
+        List<String> toDoList = new ArrayList<String>();
+        do {
             line = in.nextLine();
-            if(line.equals("bye")){
+            if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             }
-            else{
+
+            if (line.equals("list")) {
                 System.out.println("\t_____________________");
-                System.out.println("\t" + line);
+                for (int i = 1; i <= toDoList.size(); i += 1) {
+                    System.out.println("\t" + i + ": " + toDoList.get(i - 1));
+                }
+                System.out.println("\t_____________________\n");
+            } else {
+                toDoList.add(line);
+                System.out.println("\t_____________________");
+                System.out.println("\t" + "added: " + line);
                 System.out.println("\t_____________________\n");
             }
 
-        }while(!line.equals("bye"));
+        } while (!line.equals("bye"));
     }
 }
