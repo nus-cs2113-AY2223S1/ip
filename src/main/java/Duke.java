19526e6676ba5be624af96d79fc4a33f15c96d34
LAPTOP-S1RS66RO\marcu <marcusang1999@live.com>
2022-08-24 21:32:28 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String logo = "    ,---,                        ,-.            \n"
+                + "  .'  .' `\\                  ,--/ /|            \n"
+                + ",---.'     \\          ,--, ,--. :/ |            \n"
+                + "|   |  .`\\  |       ,'_ /| :  : ' /             \n"
+                + ":   : |  '  |  .--. |  | : |  '  /      ,---.   \n"
+                + "|   ' '  ;  :,'_ /| :  . | '  |  :     /     \\  \n"
+                + "'   | ;  .  ||  ' | |  . . |  |   \\   /    /  | \n"
+                + "|   | :  |  '|  | ' |  | | '  : |. \\ .    ' / | \n"
+                + "'   : | /  ; :  | : ;  ; | |  | ' \\ \\'   ;   /| \n"
+                + "|   | '` ,/  '  :  `--'   \\'  : |--' '   |  / | \n"
+                + ";   :  .'    :  ,      .-./;  |,'    |   :    | \n"
+                + "|   ,.'       `--`----'    '--'       \\   \\  /  \n"
+                + "'---'                                  `----'   \n";
+
+        //Greet
         System.out.println("____________________________________________________________");
         System.out.println(" Hello I'm Duke\n" + logo);
         System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        String command;                         //variable to store line (input)
+        Scanner in = new Scanner(System.in);    //create object that reads input
+        command = in.nextLine();                //read input
+
+        //Echo
+        while(!command.equals("bye")){
+            System.out.println(command);
+            command = in.nextLine();
+        }
+
+        //Exit
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
