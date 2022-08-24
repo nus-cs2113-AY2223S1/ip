d80acc7be7aa6e083d03418ec96238e90afb08bd
ivanthengwr <ivanthengwr@gmail.com>
2022-08-25 00:11:31 +0800

    Add ability to store and list user input text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,36 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
+
+    /**
+     * Returns a String array of saveData cutting off
+     * the NULL elements that have not been filled
+     *
+     * @param dataToStore A String array that stores the data
+     * @param userInput The input of the user
+     * @param indexData To index the element of the String array dataToStore
+     * @return A String array of saveData
+     */
+    public static String[] saveData(String[] dataToStore, String userInput, int indexData){
+        dataToStore[indexData] = userInput;
+        System.out.println("\t added: " + userInput);
+        return Arrays.copyOf(dataToStore, indexData + 1);
+    }
+
+    /**
+     * Prints out a list of save data from the user inputs
+     *
+     * @param dataOfInputs A String array of the saved data
+     */
+    public static void printList(String[] dataOfInputs){
+        int numberOrder = 1;
+        for(String data: dataOfInputs){
+            System.out.println("\t " + numberOrder + ". " + data);
+            numberOrder++;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = "___________.__                ___.     \n"
                 + "\\_   _____/|  | _____    _____|  |__  \n"
@@ -17,18 +47,31 @@ public class Duke {
 
         String userInput;
         Scanner in = new Scanner(System.in);
+        String[] dataToStore = new String[100];
+        String[] dataList = new String[]{};
+        int indexData = 0;
+
         do{
+            //Enable user to enter text
             userInput = in.nextLine();
             System.out.print("\t" + lineDivider);
+
             if(!userInput.equals("bye")) {
-                System.out.println("\t " + userInput);
+                if(!userInput.equals("list")) {
+                    dataList = saveData(dataToStore, userInput, indexData);
+                    indexData++;
+                }
+                else{
+                    printList(dataList);
+                }
             }
+
             else{
                 System.out.println("\t Bye. Hope to see you again soon!");
             }
+
             System.out.println("\t" + lineDivider);
         }
         while(!userInput.equals("bye"));
-
     }
 }
