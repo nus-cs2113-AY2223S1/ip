721bfe65ad5e962e7cf35038ff43a7d9418ea1a3
amitrahman1026 <amit.amit.rahman@gmail.com>
2022-08-25 00:27:39 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        System.out.println("Hello from\n" + logo);
+
+        String greetingMessage = "______________________________________\n" +
+                "Hello! I'm\n" +
+                logo +
+                "What can I do for you?\n" +
+                "______________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "______________________________________\n";
+        System.out.println(greetingMessage);
     }
 }
