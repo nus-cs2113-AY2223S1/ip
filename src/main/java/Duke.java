922529665458d4f13a4d52e6cf3874a6902d90ec
Marvin Pranajaya <marvinpofficial@gmail.com>
2022-08-24 15:23:48 +0800

    Completed Level-1. Greet, Echo, Exit feature implemented.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String dukeLogo =  " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String convStart = dukeLogo + "Hello! I'm Duke\n" + "What can I do for you?";
+        String convEnd= "Bye. Hope to see you again soon!";
+        printOutput(convStart);
 
-        String greet = "____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(greet);
+        String lineInput;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            lineInput = in.nextLine();
+            if (lineInput.equals("bye")) {
+                break;
+            }
+            printOutput(lineInput);
+        }
+
+        printOutput(convEnd);
+
+    }
+
+    private static void printOutput(String message) {
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.println(message);
+        System.out.println(separator);
+        System.out.println("");
     }
 }
