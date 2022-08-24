9d663e78328dd46395b061bcfa6d215bfcda7fed
markusteimann <markusteimann13@gmail.com>
2022-08-24 13:23:19 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("""
-                Hello, my name is Duke!\s
-                 What can I do for you?\s
-                 Bye. Hope to see you again soon!""");
-
+        System.out.println("Hello, my name is Duke! \n What can I do for you?");
+        Scanner myObj = new Scanner(System.in);
+        String userInput;
+        while (true){
+            userInput = myObj.nextLine();
+            if (userInput.toLowerCase().replaceAll("\\s+","").equals("bye")){
+                break;
+            }
+            System.out.println(userInput);
+        }
+        System.out.println("Bye. Hope to see you soon!");
     }
 }
