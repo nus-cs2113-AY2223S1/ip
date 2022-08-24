5b0039e605001ef259e81d156f99c43ecf21815c
Paul <iampaullow@gmail.com>
2022-08-24 15:51:20 +0800

    Add Level-2: Add, List
    Store text and display to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,14 +11,26 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
-        String line;
+        List<String> Storage = new ArrayList<>();
+        String line = " ";
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
         int count = 0;
-        while (count < 5 && !line.equals("bye")) {
-            System.out.println(line + "\n");
+
+
+        while (count < 100 && !line.equals("bye")) {
             line = in.nextLine();
-            count++;
+
+            if (line.equals("list")) {
+                for (int i = 0; i < Storage.size() && Storage.get(i)!=null; i++) {
+                    System.out.println(i+1 + ". " + Storage.get(i));
+                }
+                System.out.println();
+            }
+            else {
+                Storage.add(line);
+                System.out.println("added: " + line + "\n");
+                count++;
+            }
         }
 
 
