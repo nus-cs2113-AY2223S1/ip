5174a771343368a26c22044732832303907f6e2a
Chiew Yi Xiang <yixiangchiew@gmail.com>
2022-08-25 00:31:17 +0800

    updated bug for increment level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 
@@ -7,8 +9,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
 
-        Greet ob = new Greet();
-        ob.printGreet();
+        Scanner in = new Scanner(System.in);
+        String[] items = new String[100];
+        int matchCount = 0;
+
+        while(true) {
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (line.equals("list")) {
+                int counter = 1;
+                for (String item : items) {
+                    if (item != null) {
+                        System.out.println(counter + ". " + item);
+                        counter++;
+                    }
+                }
+            } else {
+                System.out.println("added " + line);
+                items[matchCount] = line;
+                matchCount += 1;
+            }
+        }
     }
 }
