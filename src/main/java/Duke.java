d4355a3d84eda06ba1db22a8900cc154a8488c09
wilsonngja <wilsonngja@gmail.com>
2022-08-24 08:24:32 +0800

    Fix coding standard error

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,9 @@ public class Duke {
     public static void main(String[] args) {
 
         Task[] tasks = new Task[100];
-        // command is the response from user input and string is the
+        // 'command' is the response from user input and string is the
         String command;
-        // list is all the text stored and index where the array will stop
+        // 'list' is all the text stored and index where the array will stop
         String[] list = new String[100];
         int index = 0;
 
@@ -38,17 +38,18 @@ public class Duke {
                 for (int i = 0; i < index; i += 1) {
                     System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getTask());
                 }
-            } //If the command is not 'list' and 'bye', then it should be an 'add','mark' or 'unmark' command
-            else if (!command.equals("bye")) {
+            } else if (!command.equals("bye")) {
+                // If the command is not 'list' and 'bye', then it should be an 'add','mark' or 'unmark' command
+
                 // If the command is to mark or unmark
                 if (command.contains("mark") || (command.contains("unmark"))) {
                     // Since the command is mark or unmark, get the number and change the status to either marked or
                     // unmarked before printing it out in the specified format
                     int taskNumber = Integer.parseInt(command.split(" ")[1]);
                     tasks[taskNumber - 1].setStatusIcon(command.split(" ")[0]);
-                    System.out.println("  [" + tasks[taskNumber - 1].getStatusIcon() + "] " + tasks[taskNumber - 1].getTask());
-                }
-                else {
+                    System.out.println("  [" + tasks[taskNumber - 1].getStatusIcon() + "] " +
+                            tasks[taskNumber - 1].getTask());
+                } else {
                     // If the command does not contain mark or unmark, then it should be an 'add' operation
                     // Create a new task and append the index by 1.
                     // The index would indicate when the iteration would stop.
