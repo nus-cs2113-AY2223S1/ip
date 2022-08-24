9c6d24516d9161b6b30fa485e13ae4f5ada6bf94
Jorelle <jorellesee2000@gmail.com>
2022-08-24 17:27:01 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
         String GREETING = "____________________________________________________________\n" +
-                " Hello! I'm Weng\n" +
+                " Hello! I'm Weng!\n" +
                 " What can I do for ya?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        System.out.println("\n" + GREETING);
+        String EXIT_MESSAGE = "____________________________________________________________\n" +
                 " Bye. Hope to cya again soon!\n" +
                 "____________________________________________________________";
-        System.out.println("\n" + GREETING);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
+        while(!input.equals("bye")){
+            String output = "____________________________________________________________\n " +
+                    input +
+                    "\n____________________________________________________________\n";
+            System.out.println(output);
+            input = sc.nextLine();
+        }
 
+        System.out.println(EXIT_MESSAGE);
     }
 }
