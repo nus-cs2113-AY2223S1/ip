cc6c8e12d49e81053b547f43145b0afda27ecf26
markusteimann <markusteimann13@gmail.com>
2022-08-24 13:53:31 +0800

    Level-2: Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,48 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Duke! \n What can I do for you?");
         Scanner myObj = new Scanner(System.in);
+        String[] todoList = new String[100];
         String userInput;
-        while (true){
+        boolean bye = false;
+
+        Commands commandType;
+        while (!bye){
             userInput = myObj.nextLine();
-            if (userInput.toLowerCase().replaceAll("\\s+","").equals("bye")){
-                break;
+            try{
+                commandType = Commands.valueOf(userInput.toUpperCase());
+                switch (commandType) {
+                    case LIST -> {
+                        System.out.println("____________________________________________________________");
+                        for (int i = 0; i < todoList.length; i++) {
+                            if (todoList[i] != null) {
+                                System.out.println("\t" + (i + 1) + ")  " + todoList[i]);
+                            } else {
+                                break;
+                            }
+                        }
+                        System.out.println("____________________________________________________________");
+                    }
+                    case BYE -> bye = true;
+                }
             }
-            System.out.println(userInput);
+            catch(Exception e){
+                for(int i = 0; i < todoList.length; i++)
+                    if(todoList[i] == null) {
+                        todoList[i] = userInput;
+                        break;
+                    }
+                System.out.println("____________________________________________________________");
+                System.out.println("\t Added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
         }
-        System.out.println("Bye. Hope to see you soon!");
+        System.out.println("____________________________________________________________");
+        System.out.println("\tBye. Hope to see you soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    enum Commands{
+        LIST,
+        BYE
     }
 }
