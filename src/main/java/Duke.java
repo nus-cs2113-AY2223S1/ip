4923cc0e46c22c1637c681ea08df9a427218cd37
FeliciaBeatrice <feliciabeatrice7@gmail.com>
2022-08-25 10:47:10 +0800

    Albot - Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printDivider() {
+        System.out.println("    ----------------------------------------------------");
+    }
+
+    public static void printGreeting() {
+        printDivider();
+        System.out.println("    Hello! I'm Albot, your new best friend! ᕙ(`▿´)ᕗ");
+        System.out.println("    What can I do for you today, friend? ʕ•́ᴥ•̀ʔっ♡");
+        System.out.println();
+        printDivider();
+    }
+
+    public static void printExitMessage() {
+        printDivider();
+        System.out.println("    Aww, you're leaving already? I'm gonna miss you (╥﹏╥)");
+        System.out.println("    But anyways bye! Hope to see you again soon, friend! (ɔ◔‿◔)ɔ ♥");
+        System.out.println();
+        printDivider();
+    }
+
+    public static void echoMessage(String line) {
+        printDivider();
+        System.out.println("    " + line);
+        System.out.println();
+        printDivider();
+    }
+
     public static void main(String[] args) {
-        System.out.println("-------------------------<3-------------------------");
-        System.out.println("Hello! I'm Albot, your only best friend :D");
-        System.out.println("What can I do for you today? ^^");
-        System.out.println();
-        System.out.println("-------------------------<3-------------------------");
-        System.out.println("Why are you shutting me down? :D");
-        System.out.println("But anyways, bye. Hope to see you again soon!");
-        System.out.println();
-        System.out.println("-------------------------<3-------------------------");
+        printGreeting();
+
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            Scanner in = new Scanner(System.in);
+            userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            echoMessage(userInput);
+        }
+
+        printExitMessage();
     }
 }
