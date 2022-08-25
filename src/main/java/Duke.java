001241d3de0f0e229fa5690b613a68e427cd419a
HT-T <kikutahang@gmail.com>
2022-08-26 03:08:09 +0800

    Level 1 commit @ 26Aug - HT

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke \n What can I do for you? \n Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Duke_HTT. \n What can I do for you?");
+        
+        Scanner scan = new Scanner ( System.in ); // take user input
+        String inData = scan.nextLine();
+
+        while (!inData.equals("bye")) {
+            System.out.println(inData);
+            inData = scan.nextLine();
+        }
+
+        
+       
+
+
+
+        
+    
+    
+    
+    
+        System.out.println(" \n Bye. Hope to see you again soon!");
     }
 }
