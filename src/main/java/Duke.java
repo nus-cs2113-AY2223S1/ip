cb17134d75e2c23f244cade6de82ea2db06e8d8c
ivanthengwr <ivanthengwr@gmail.com>
2022-08-25 18:18:35 +0800

    Update code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,15 +5,15 @@ public class Duke {
     /**
      * Prints out a list of the tasks saved from the user inputs
      *
-     * @param assignment variable that is created using the class Task
+     * @param assignments variable that is created using the class Task
      * @param indexTask index the Task array given by the variable assignment
      */
-    public static void printList(Task[] assignment, int indexTask){
+    public static void printList(Task[] assignments, int indexTask){
         int numberOrder = 1;
         for(int i = 0; i < indexTask; i++) {
             System.out.println("\t " + numberOrder + ".["
-                    + assignment[i].getStatusIcon() + "] "
-                    + assignment[i].description);
+                    + assignments[i].getStatusIcon() + "] "
+                    + assignments[i].description);
             numberOrder++;
         }
     }
@@ -34,7 +34,7 @@ public class Duke {
 
         String userInput;
         Scanner in = new Scanner(System.in);
-        Task[] assignment = new Task[100];
+        Task[] assignments = new Task[100];
         int indexTask = 0;
 
         do {
@@ -47,16 +47,16 @@ public class Duke {
                 if(!splitUserInput[0].equals("list")
                         && !splitUserInput[0].equals("mark")
                         && !splitUserInput[0].equals("unmark")) {
-                    assignment[indexTask] = new Task(userInput);
-                    System.out.println("\t added: " + assignment[indexTask].description);
+                    assignments[indexTask] = new Task(userInput);
+                    System.out.println("\t added: " + assignments[indexTask].description);
                     indexTask++;
                 } else if (splitUserInput[0].equals("list")) {
-                    printList(assignment, indexTask);
+                    printList(assignments, indexTask);
                 } else if (splitUserInput[0].equals("mark")) {
                     int markIndex = Integer.parseInt(splitUserInput[1]) - 1;
                     //To handle a case where user tries to mark a task that has not been specified
                     try {
-                        assignment[markIndex].markAsDone();
+                        assignments[markIndex].markAsDone();
                     } catch(NullPointerException e) {
                         System.out.println("\t You are trying to mark a task that has not been specified!");
                         System.out.println("\t" + lineDivider);
@@ -64,22 +64,22 @@ public class Duke {
                     }
 
                     System.out.println("\t Awesome! I've marked this task as done:");
-                    System.out.println("\t\t [" + assignment[markIndex].getStatusIcon()
-                            + "] " + assignment[markIndex].description);
+                    System.out.println("\t\t [" + assignments[markIndex].getStatusIcon()
+                            + "] " + assignments[markIndex].description);
                 } else {
                     //If the splitUserInput[0] is equals to "unmark"
                     int unmarkIndex = Integer.parseInt(splitUserInput[1]) - 1;
                     //To handle a case where user tries to unmark a task that has not been specified
                     try {
-                        assignment[unmarkIndex].unmarkAsDone();
+                        assignments[unmarkIndex].unmarkAsDone();
                     } catch(NullPointerException e) {
                         System.out.println("\t You are trying to unmark a task that has not been specified!");
                         System.out.println("\t" + lineDivider);
                         continue;
                     }
                     System.out.println("\t Awesome! I've marked this task as not done yet:");
-                    System.out.println("\t\t [" + assignment[unmarkIndex].getStatusIcon()
-                            + "] " + assignment[unmarkIndex].description);
+                    System.out.println("\t\t [" + assignments[unmarkIndex].getStatusIcon()
+                            + "] " + assignments[unmarkIndex].description);
                 }
             } else {
                 System.out.println("\t Bye. Hope to see you again soon!");
