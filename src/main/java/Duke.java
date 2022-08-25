9109512d93d1f964ee6b8bc160cecebfde100409
Brandon-OS <hieronimusbrandon@gmail.com>
2022-08-25 23:11:15 +0800

    Level 1 - echo method added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         greet();
-        exit();
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        while (!command.matches("bye")) {
+            echo(command);
+            command = input.nextLine();
+        }
+        exit(); 
     }
 
     public static void greet() {
@@ -12,7 +20,14 @@ public class Duke {
         
     }
 
+    public static void echo(String command) {
+        System.out.println("------------------------------------------------------------");
+        System.out.println(command);
+        System.out.println("------------------------------------------------------------");
+    }
+
     public static void exit() {
+        System.out.println("------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------------------------");
     }
