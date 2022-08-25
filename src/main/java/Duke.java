83eb35f778c92ff7c93fc5c1acba0d9f21c3623c
insafhere <insaf@u.nus.edu>
2022-08-25 21:49:34 +0800

    Added greeting method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
 public class Duke {
+    public void greeting(){
+        String greet = "Hello! I'm Duke";
+        String question = "What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        String line = "---------------------------------------------";
+        System.out.println(line + "\n" + greet + "\n" + question + "\n" + line + "\n" + bye + "\n" + line);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +14,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        new Duke().greeting();
     }
 }
