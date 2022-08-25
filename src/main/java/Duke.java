74111eecd3636a8783ecde3a5fdb89c8edaaf9c6
insafhere <insaf@u.nus.edu>
2022-08-25 22:15:26 +0800

    Modified for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
+import java.util.Scanner;
 public class Duke {
-    public void greeting(){
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String line = "---------------------------------------------";
         String greet = "Hello! I'm Duke";
         String question = "What can I do for you?";
-        String bye = "Bye. Hope to see you again soon!";
-        String line = "---------------------------------------------";
-        System.out.println(line + "\n" + greet + "\n" + question + "\n" + line + "\n" + bye + "\n" + line);
-    }
+        System.out.println(line + "\n" + greet + "\n" + question + "\n" + line);
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        new Duke().greeting();
+        boolean run = true;
+        while(run){
+            Scanner in = new Scanner(System.in);
+            String text;
+            text = in.nextLine();
+            if(text.equals("bye")){
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                run = false;
+            }
+            else {
+                System.out.println(line);
+                System.out.println(text);
+                System.out.println(line);
+            }
+        }
     }
 }
