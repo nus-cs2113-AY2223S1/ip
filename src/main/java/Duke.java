f93aca6e83bb14a07b9e05318db155191b9ad27a
Abhitya Krisnaraj <abhitya@gmail.com>
2022-08-25 16:54:17 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,6 +11,11 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you");
+        String a = in.nextLine();
+        while(!a.equals("bye")){
+            System.out.println(a);
+            a = in.nextLine();
+        }
         System.out.println();
 
         System.out.println("Bye");
