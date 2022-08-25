aebeabed7573456e26ca9f5a3db397a9790b620e
LAPTOP-FOVQQ54S\80ly <junhaoliu2468@gmail.com>
2022-08-25 22:17:00 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,8 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke ");
-        System.out.println("What can I do for you? \n" );
-        System.out.println("Bye. Hope to see you again soon! \n" );
+        proceed();
+
+    }
+    public static void proceed(){
+        Communication.greet();
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit){
+            String inputString = sc.nextLine();
+            if (inputString.equals("bye")){
+                exit = true;
+                Communication.bye();
+                break;
+            }
+            else{
+                Communication.copy(inputString);
+            }
+        }
     }
 }
