e85afad5889c53e08f53152a7b4d906b238141a3
AaronC <acz2606@gmail.com>
2022-08-25 11:36:21 +0800

    Add: Level 0 (Greet)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Level-0: Greet
+        System.out.println("---------------------------------------------------");
+        System.out.println("Hello! What can I do for you today?");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Goodbye!");
+        System.out.println("---------------------------------------------------");
     }
 }
