a99184562b37c687f5d648a90889c84fa04f40e8
ivanthengwr <ivanthengwr@gmail.com>
2022-08-25 12:17:01 +0800

    Add Task class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        //mark done task with X
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+}
