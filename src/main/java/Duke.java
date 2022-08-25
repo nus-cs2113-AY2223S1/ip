f48966d8de19498309bb565d391168f404b9adb4
Jorelle <jorellesee2000@gmail.com>
2022-08-25 15:41:54 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,20 +7,38 @@ public class Duke {
                 " What can I do for ya?\n" +
                 "____________________________________________________________\n";
         System.out.println("\n" + GREETING);
-        String EXIT_MESSAGE = "____________________________________________________________\n" +
-                " Bye. Hope to cya again soon!\n" +
-                "____________________________________________________________";
+//        String EXIT_MESSAGE = "____________________________________________________________\n" +
+//                " Bye. Hope to cya again soon!\n" +
+//                "____________________________________________________________";
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//
+//        while(!input.equals("bye")){
+//            String output = "____________________________________________________________\n " +
+//                    input +
+//                    "\n____________________________________________________________\n";
+//            System.out.println(output);
+//            input = sc.nextLine();
+//        } Level-1
+        String[] myList;
+        myList = new String[100];
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-
-        while(!input.equals("bye")){
+        int numberOfInputs = 0;
+        while(!input.equals("list")){
+            myList[numberOfInputs] = input;
+            numberOfInputs++;
             String output = "____________________________________________________________\n " +
+                    "Added: " +
                     input +
                     "\n____________________________________________________________\n";
             System.out.println(output);
             input = sc.nextLine();
         }
+        for(int i = 0; i < numberOfInputs; i++){
+            System.out.print(i + 1);
+            System.out.println(". " + myList[i]);
+        }
 
-        System.out.println(EXIT_MESSAGE);
     }
 }
