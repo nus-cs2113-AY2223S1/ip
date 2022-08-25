dcb9781685f89e207cec02ba73d1bc6238bfb997
Paul <iampaullow@gmail.com>
2022-08-25 10:29:23 +0800

    Add A-CodingStandard
    Tweak code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,24 +7,43 @@ public class Duke {
         protected String description;
         protected boolean isDone;
 
+
+        /**
+         * Creates Task Object
+         * @param description
+         */
         public Task(String description) {
             this.description = description;
             this.isDone = false;
         }
 
+        /**
+         * Returns the status of the task
+         * @return status as "X" or " "
+         */
         public String getStatusIcon() {
             return (isDone ? "X" : " "); // mark done task with X
         }
 
+        /**
+         * Returns the Description of the task
+         * @return description
+         */
         public String getDescription() {
             return (description);
         }
 
-        public void markStatus() {
+        /**
+         * Sets Status to done
+         */
+        public void setMarkStatus() {
             isDone = true;
         }
 
-        public void unmarkStatus() {
+        /**
+         * Sets Status to uncompleted
+         */
+        public void setUnmarkStatus() {
             isDone = false;
         }
     }
@@ -37,46 +56,54 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
-        List<Task> Storage = new ArrayList<>();
+        //tasks is an array list collection of task objects
+        List<Task> tasks = new ArrayList<>();
         String line = " ";
         Scanner in = new Scanner(System.in);
         int count = 0;
         line = in.nextLine();
 
+        // duke runs until a "bye" is entered
         while (count < 100 && !line.equals("bye")) {
             String [] splitLine = line.split(" ");
 
+            // list commands duke to list all the tasks stored and their completion status
             if (line.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < Storage.size(); i++) {
-                    System.out.println(i+1 + ". [" + Storage.get(i).getStatusIcon() + "] "+ Storage.get(i).getDescription());
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i+1 + ". [" + tasks.get(i).getStatusIcon() + "] "
+                            + tasks.get(i).getDescription());
                 }
                 System.out.println();
             }
+
+            // mark x commands duke to mark the corresponding task as completed
             else if (splitLine[0].equals("mark")) {
+                // Exceptions could occur
                 String numericString = line.substring(line.indexOf(" ")+1);
                 System.out.println(numericString);
                 int markedNum = Integer.parseInt(numericString) - 1;
-                Storage.get(markedNum).markStatus();
+                tasks.get(markedNum).setMarkStatus();
                 System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(" [" + Storage.get(markedNum).getStatusIcon() + "] "+ Storage.get(markedNum).getDescription() + "\n");
-
-                // need to consider that other stuff might cause an error like matcha
+                System.out.println(" [" + tasks.get(markedNum).getStatusIcon() + "] "
+                        + tasks.get(markedNum).getDescription() + "\n");
             }
+
+            // unmark x commands duke to mark the corresponding task as uncompleted
             else if (splitLine[0].equals("unmark")) {
                 String numericString = line.substring(line.indexOf(" ")+1);
                 System.out.println(numericString);
                 int markedNum = Integer.parseInt(numericString) - 1;
-                Storage.get(markedNum).unmarkStatus();
+                tasks.get(markedNum).setUnmarkStatus();
                 System.out.println("Okay, I've marked this task as not done yet sadly: ");
-                System.out.println(" [" + Storage.get(markedNum).getStatusIcon() + "] "+ Storage.get(markedNum).getDescription() + "\n");
-
-                // need to consider that other stuff might cause an error like matcha
+                System.out.println(" [" + tasks.get(markedNum).getStatusIcon() + "] "
+                        + tasks.get(markedNum).getDescription() + "\n");
             }
 
+            // other calls causes duke to add the user-input to tasks
             else {
                 Task t = new Task(line);
-                Storage.add(t);
+                tasks.add(t);
                 System.out.println("added: " + line + "\n");
                 count++;
             }
