bf13ecbce8349461f1ce585361ea3a134b01cf2a
kaseykwok <kaseykwok@gmail.com>
2022-08-26 01:37:20 +0800

    Add methods for add and list items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] items = new String[100];
+    private static int itemsCount = 0;
     private static void greet() {
         String message = "Hello! I'm Ever\n" +
                 "What can I do for you?";
@@ -9,6 +11,14 @@ public class Duke {
     private static void exit() {
         String message = "Bye. Hope to see you again soon!";
         System.out.println(message);
+    }
+    private static void addItem(String item) {
+        items[itemsCount++] = item;
+    }
+    private static void listItems() {
+        for (int i = 0; i < itemsCount; i++) {
+            System.out.printf("%d. %s\n", i + 1, items[i]);
+        }
     }
     public static void main(String[] args) {
         String logo = " ________      ________ _____  \n" +
@@ -30,8 +40,12 @@ public class Duke {
             inputMessage = scanner.nextLine();
             if (inputMessage.equals("bye")) {
                 break;
+            } else if (inputMessage.equals("list")){
+                listItems();
+            } else {
+                addItem(inputMessage);
+                System.out.println("Item \"" + inputMessage + "\" added");
             }
-            System.out.println(inputMessage);
         }
 
         exit();
