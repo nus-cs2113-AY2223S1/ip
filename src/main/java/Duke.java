47c37b1f09bc67cfd0e3034a1f530e7a26e3ca40
nvknow <1375782557@qq.com>
2022-08-25 18:37:39 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         //        String logo = " ____        _        \n"
@@ -6,11 +7,35 @@ public class Duke {
         //                + "| |_| | |_| |   <  __/\n"
         //                + "|____/ \\__,_|_|\\_\\___|\n";
         //        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        request:
+        while(true){
+            line = in.nextLine();
+            switch (line) {
+            case "list":
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     list");
+                System.out.println("    ____________________________________________________________");
+                break;
+            case "blah":
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     blah");
+                System.out.println("    ____________________________________________________________");
+                break;
+            case "bye":
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break request;
+            default:
+                break;
+            }
+        }
     }
 }
