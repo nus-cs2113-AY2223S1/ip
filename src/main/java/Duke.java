1845dde917084e2dcea8f369200f7f14a0b947dc
helpdesk1234 <ongzhihong2000@gmail.com>
2022-08-25 19:33:24 +0800

    Add Increment Level-1 Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         newLine();
         welcomeGreeting();
         newLine();
+        String input = getInput(in);
+        newLine();
+        while (!input.equals("bye")){
+            echo(input);
+            input = getInput(in);
+            newLine();
+        }
+
         goodbyeGreeting();
         newLine();
     }
+
+    private static void echo(String input){
+        System.out.println(" " + input);
+        newLine();
+    }
+    private static String getInput(Scanner in){
+        return in.nextLine();
+    }
+
 
     private static void newLine(){
         for (int i = 0; i < 60; i++) {
