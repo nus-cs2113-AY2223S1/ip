4fceba53867b4f22396fb4ed00aa0167114ea0b7
helpdesk1234 <ongzhihong2000@gmail.com>
2022-08-25 21:14:30 +0800

    Add Increment Level-3 Mark As Done
    Refactor Duke class to move methods into DukeController class
    Add Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,62 +1,24 @@
-import java.lang.reflect.Array;
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Duke {
-
-    static ArrayList<String> taskList = new ArrayList<String>(0);
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        newLine();
-        welcomeGreeting();
-        newLine();
-        String input = getInput(in);
-        newLine();
-        while (!input.equals("bye")){
-            if (input.equals("list")){
-                printTaskList(taskList);
+        DukeController dukeController = new DukeController();
+        dukeController.printWelcome();
+        while (true){
+            String input = dukeController.getInput();
+            if (input.equals("bye")){
+                dukeController.printGoodbye();
+                return;
+            } else if (input.equals("list")){
+                dukeController.printTaskList();
+            } else if (input.startsWith("mark")){
+                int index = Integer.parseInt(input.substring(5));
+                dukeController.markTask(index);
+            } else if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.substring(7));
+                dukeController.unmarkTask(index);
             } else {
-                echo(input);
-                taskList.add(input);
+                dukeController.add(input);
             }
-            input = getInput(in);
-            newLine();
+
         }
-
-        goodbyeGreeting();
-        newLine();
-    }
-
-    private static void printTaskList(ArrayList<String> taskList){
-        for (int i = 0; i < taskList.size(); i++){
-            int listIndex = i+1;
-            System.out.println(" "+listIndex+". "+taskList.get(i));
-        }
-        newLine();
-    }
-
-    private static void echo(String input){
-        System.out.println(" added: " + input);
-        newLine();
-    }
-    private static String getInput(Scanner in){
-        return in.nextLine();
-    }
-
-
-    private static void newLine(){
-        for (int i = 0; i < 60; i++) {
-            System.out.print('_');
-        }
-        System.out.print('\n');
-    }
-
-    private static void welcomeGreeting(){
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-    }
-
-    private static void goodbyeGreeting(){
-        System.out.println(" Bye. Hope to see you again soon!");
     }
 }
---
+++ src/main/java/DukeController.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DukeController {
+    protected ArrayList<Task> taskList = new ArrayList<Task>(0);
+    protected Scanner in = new Scanner(System.in);
+
+    public void printTaskList(){
+        for (int i = 0; i < taskList.size(); i++){
+            int listIndex = i+1;
+            Task taskToPrint = taskList.get(i);
+            System.out.println(" "+listIndex+".["+taskToPrint.getStatusIcon()+"] "+ taskToPrint.description);
+        }
+        printNewLine();
+    }
+
+    public void add(String input){
+        System.out.println(" added: " + input);
+        Task newTask = new Task(input);
+        taskList.add(newTask);
+        printNewLine();
+    }
+
+    public String getInput(){
+        String input = in.nextLine();
+        printNewLine();
+        return input;
+    }
+
+    public void printNewLine(){
+        for (int i = 0; i < 60; i++) {
+            System.out.print('_');
+        }
+        System.out.print('\n');
+    }
+
+    public void printWelcome(){
+        printNewLine();
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        printNewLine();
+    }
+
+    public void printGoodbye(){
+        System.out.println(" Bye. Hope to see you again soon!");
+        printNewLine();
+    }
+
+    public void markTask(int index){
+        index--;
+        Task task = taskList.get(index);
+        task.updateStatus(true);
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println("  ["+task.getStatusIcon()+"] "+task.description);
+        printNewLine();
+
+    }
+
+    public void unmarkTask(int index){
+        index--;
+        Task task = taskList.get(index);
+        task.updateStatus(false);
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println("  ["+task.getStatusIcon()+"] "+task.description);
+        printNewLine();
+
+    }
+}
