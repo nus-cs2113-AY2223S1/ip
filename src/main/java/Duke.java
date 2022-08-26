5b67c7ba22abd97faf5002a1754fef4de5c4768e
Amethyst <e0978391@u.nus.edu>
2022-08-27 01:35:52 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        showWelcomeMessage();
+        handleUserInput();
+    }
+
+    private static void showWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    private static void handleUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String inputText = scanner.nextLine();
+            while (inputText.trim().isEmpty())
+                inputText = scanner.nextLine();
+            switch (inputText) {
+            case "bye":
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                System.exit(0);
+            default:
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    "+inputText);
+                System.out.println("    ____________________________________________________________");
+                System.out.println();
+            }
+        }
     }
 }
