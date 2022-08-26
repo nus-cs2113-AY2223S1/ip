677ff69cd757c3f3411513d69475a444de43037f
kohnh <kohnh88@gmail.com>
2022-08-26 21:59:38 +0800

    Echo commands entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "     ____.  _____ ______________   ____.___  _________\n"
@@ -12,10 +13,19 @@ public class Duke {
                 + logo
                 + "What can I do for you?\n"
                 + "------------------------------------------------\n";
-        String parting = "Bye. Hope to see you again soon!\n"
+        String parting = "------------------------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
                 + "------------------------------------------------\n";
 
         System.out.println(greeting);
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+        while(!command.equals("bye")){
+            System.out.println("------------------------------------------------\n");
+            System.out.println(command);
+            System.out.println("------------------------------------------------\n");
+            command = in.nextLine();
+        }
         System.out.println(parting);
     }
 }
