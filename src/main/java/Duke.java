928ecc7bcd9acf39712a6dad7dce7833a40d7008
sylviahe171 <sylviahe171@gmail.com>
2022-08-26 12:36:37 +0800

    level_1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________");
+        Scanner input = new Scanner(System.in);
+        String val = input.next();
+        while(!val.equals("bye")){
+            System.out.println("____________________________________________________________\n" +
+                    val +
+                    "\n____________________________________________________________");
+            val = input.next();
+        }
+
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________");
     }
