1e0fbb8d944c466681ea0a08cb9ce7bf30705b10
shengiv <66622728+shengiv@users.noreply.github.com>
2022-08-26 15:23:11 +0800

    Add ability to store user input and display back to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<String>();
         String logo = "______________________________________________\n";
         System.out.println(logo+ " Hello! I'm Duke\n" +
                 " What can I do for you?\n" + logo);
@@ -8,7 +10,16 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         lineInput = in.nextLine();
         while(!lineInput.equals("bye")){
-            System.out.println(lineInput + System.lineSeparator());
+            if(lineInput.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(Integer.toString(i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println();
+            }
+            else{
+                tasks.add(lineInput);
+                System.out.println("added: " + lineInput + System.lineSeparator());
+            }
             lineInput = in.nextLine();
         }
         System.out.println(logo + "Bye. Hope to see see you again soon!" + System.lineSeparator() + logo);
