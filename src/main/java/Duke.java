06d1050610ec7113aee07d587e35e40106bfb3e8
sylviahe171 <sylviahe171@gmail.com>
2022-08-26 12:56:01 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,25 @@ public class Duke {
                 "____________________________________________________________");
         Scanner input = new Scanner(System.in);
         String val = input.next();
+        String[] list = new String[100];
+        int length = 0;
         while(!val.equals("bye")){
-            System.out.println("____________________________________________________________\n" +
-                    val +
-                    "\n____________________________________________________________");
-            val = input.next();
+            if(val.equals("list")){
+                System.out.println("____________________________________________________________");
+                for(int i = 0; i < length; i++){
+                    System.out.println("  " + i + 1 + ". " + list[i]);
+                }
+                System.out.println("____________________________________________________________");
+                val = input.next();
+            }else {
+                list[length] = val;
+                length++;
+                System.out.println("____________________________________________________________\n" +
+                        "added: " + val +
+                        "\n____________________________________________________________");
+                val = input.next();
+
+            }
         }
 
         System.out.println("____________________________________________________________\n" +
