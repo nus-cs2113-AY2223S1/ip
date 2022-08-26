f5730af856aad6a1c634a065818e693709ae1c20
markusteimann <markusteimann13@gmail.com>
2022-08-26 14:23:53 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,35 +2,66 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Duke! \n What can I do for you?");
+
         Scanner myObj = new Scanner(System.in);
-        String[] todoList = new String[100];
+
+        Task[] todoList = new Task[100];
         String userInput;
+        String subUserInput;
+        int markedValue = 0;
         boolean bye = false;
 
         Commands commandType;
         while (!bye){
-            userInput = myObj.nextLine();
+            userInput = myObj.nextLine() + " ";
+            subUserInput = userInput.substring(0, userInput.indexOf(' '));
             try{
-                commandType = Commands.valueOf(userInput.toUpperCase());
+                markedValue = Integer.parseInt(userInput.substring(userInput.indexOf(' ')).replaceAll("\\s+","")) - 1;
+            }
+            catch(Exception ignored){
+
+            }
+
+            try{
+                commandType = Commands.valueOf(subUserInput.toUpperCase());
                 switch (commandType) {
-                    case LIST -> {
+                    case LIST:
                         System.out.println("____________________________________________________________");
                         for (int i = 0; i < todoList.length; i++) {
                             if (todoList[i] != null) {
-                                System.out.println("\t" + (i + 1) + ")  " + todoList[i]);
+                                System.out.println("\t" + (i + 1) + ") [" + todoList[i].getStatusIcon() +"] " + todoList[i].getDescription());
                             } else {
                                 break;
                             }
                         }
                         System.out.println("____________________________________________________________");
-                    }
-                    case BYE -> bye = true;
+                        break;
+
+                    case BYE :
+                        bye = true;
+                        break;
+
+                    case MARK :
+                        todoList[markedValue].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("[" +todoList[markedValue].getStatusIcon() +"]  " + todoList[markedValue].getDescription());
+                        System.out.println("____________________________________________________________");
+                        break;
+
+                    case UNMARK :
+                        todoList[markedValue].unMark();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("[" +todoList[markedValue].getStatusIcon() +"]  " + todoList[markedValue].getDescription());
+                        System.out.println("____________________________________________________________");
                 }
             }
             catch(Exception e){
                 for(int i = 0; i < todoList.length; i++)
                     if(todoList[i] == null) {
-                        todoList[i] = userInput;
+                        Task newTask = new Task(userInput);
+                        todoList[i] = newTask;
                         break;
                     }
                 System.out.println("____________________________________________________________");
@@ -45,6 +76,34 @@ public class Duke {
 
     enum Commands{
         LIST,
-        BYE
+        BYE,
+        MARK,
+        UNMARK
+    }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone(){
+            this.isDone = true;
+        }
+
+        public void unMark(){
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : ""); // mark done task with X
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
 }
