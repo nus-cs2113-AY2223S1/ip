20ce88dbdf7839d52054c1370e3caba1e31d121d
kohnh <kohnh88@gmail.com>
2022-08-26 22:47:51 +0800

    Add Task class and ability to mark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
     public static void main(String[] args) {
         String logo = "     ____.  _____ ______________   ____.___  _________\n"
@@ -19,23 +20,34 @@ public class Duke {
 
         System.out.println(greeting);
         Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
-        String[] tasks = new String[100];
+        String command = in.nextLine().trim();
+        Task[] tasks = new Task[100];
         int idx = 0;
         while(!command.equals("bye")){
             System.out.println("------------------------------------------------\n");
             if (command.equals("list")) {
                 for (int i = 0; i < idx; i++) {
                     System.out.print(i+1);
-                    System.out.println(": " + tasks[i]);
+                    System.out.println(".[" + tasks[i].getStatusIcon() + "] "
+                            + tasks[i].getDescription());
                 }
+            } else if (command.length() > 5 && command.substring(0,4).equals("mark")) {
+                int pos = Integer.parseInt(command.substring(5)) - 1;
+                tasks[pos].setDone(true);
+                System.out.print("Nice! I've marked this task as done:\n  [X] ");
+                System.out.println(tasks[pos].getDescription());
+            } else if (command.length() > 7 && command.substring(0,6).equals("unmark")) {
+                int pos = Integer.parseInt(command.substring(7)) - 1;
+                tasks[pos].setDone(false);
+                System.out.print("OK, I've marked this task as not done yet:\n  [ ] ");
+                System.out.println(tasks[pos].getDescription());
             } else {
-                tasks[idx] = command;
+                tasks[idx] = new Task(command);
                 idx++;
                 System.out.println("added: " + command);
             }
             System.out.println("------------------------------------------------\n");
-            command = in.nextLine();
+            command = in.nextLine().trim();
         }
         System.out.println(parting);
     }
