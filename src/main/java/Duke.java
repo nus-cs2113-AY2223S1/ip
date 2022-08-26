9e2ef7209d51533b41dc4d502d5d88a25e124d67
sylviahe171 <sylviahe171@gmail.com>
2022-08-26 13:53:41 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,26 +7,39 @@ public class Duke {
                 " What can I do for you?\n" +
                 "____________________________________________________________");
         Scanner input = new Scanner(System.in);
-        String val = input.next();
+        String val = input.nextLine();
         String[] list = new String[100];
+        String[] status = new String[100];
         int length = 0;
         while(!val.equals("bye")){
+            System.out.println("____________________________________________________________");
             if(val.equals("list")){
-                System.out.println("____________________________________________________________");
-                for(int i = 0; i < length; i++){
-                    System.out.println("  " + i + 1 + ". " + list[i]);
+                for(int i = 0; i < length; i++) {
+                    System.out.println("  " + (i + 1) + ".[" + status[i] + "] " + list[i]);
                 }
-                System.out.println("____________________________________________________________");
-                val = input.next();
-            }else {
+            } else if (!val.contains("mark")) {
                 list[length] = val;
+                status[length] = " ";
                 length++;
-                System.out.println("____________________________________________________________\n" +
-                        "added: " + val +
-                        "\n____________________________________________________________");
-                val = input.next();
-
+                System.out.println("added: " + val);
+            } else if (val.indexOf("mark") == 0) {
+                int id = val.indexOf(" ");
+                String ind = val.substring(id + 1);
+                int index = Integer.parseInt(ind) - 1;
+                status[index] = "X";
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("    [" + status[index] + "] " + list[index]);
+            } else{
+                int id = val.indexOf(" ");
+                String ind = val.substring(id + 1);
+                int index = Integer.parseInt(ind) - 1;
+                status[index] = " ";
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("    [" + status[index] + "] " + list[index]);
             }
+            System.out.println("____________________________________________________________");
+            val = input.nextLine();
+
         }
 
         System.out.println("____________________________________________________________\n" +
