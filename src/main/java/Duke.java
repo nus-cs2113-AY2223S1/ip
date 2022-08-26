6482dfe95861d110c1c663cc325c7a5f2b467df0
FeliciaBeatrice <feliciabeatrice7@gmail.com>
2022-08-26 17:06:09 +0800

    Albot - fixed some code to coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,21 +5,21 @@ public class Duke {
     public static int position = 0;
 
     public static void printDivider() {
-        System.out.println("    ----------------------------------------------------");
+        System.out.println("\t----------------------------------------------------");
     }
 
     public static void printGreeting() {
         printDivider();
-        System.out.println("    Hello! I'm Albot, your new best friend! ᕙ(`▿´)ᕗ");
-        System.out.println("    What can I do for you today, friend? ʕ•́ᴥ•̀ʔっ♡");
+        System.out.println("\tHello! I'm Albot, your new best friend! ᕙ(`▿´)ᕗ");
+        System.out.println("\tWhat can I do for you today, friend? ʕ•́ᴥ•̀ʔっ♡");
         System.out.println();
         printDivider();
     }
 
     public static void printExitMessage() {
         printDivider();
-        System.out.println("    Aww, you're leaving already? I'm gonna miss you (╥﹏╥)");
-        System.out.println("    But anyways bye! Hope to see you again soon, friend! (ɔ◔‿◔)ɔ ♥");
+        System.out.println("\tAww, you're leaving already? I'm gonna miss you (╥﹏╥)");
+        System.out.println("\tBut anyways bye! Hope to see you again soon, friend! (ɔ◔‿◔)ɔ ♥");
         System.out.println();
         printDivider();
     }
@@ -29,16 +29,17 @@ public class Duke {
         position++;
 
         printDivider();
-        System.out.println("    added: " + currentTask.description);
+        System.out.println("\tadded: " + currentTask.description);
         System.out.println();
         printDivider();
     }
 
     public static void printList() {
         printDivider();
-        System.out.println("    Here are the tasks in your list:");
+        System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < position; i++) {
-            System.out.println("    " + String.valueOf(i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
+            System.out.println("    " + String.valueOf(i + 1) + ".[" +
+                    list[i].getStatusIcon() + "] " + list[i].description);
         }
         System.out.println();
         printDivider();
@@ -49,8 +50,8 @@ public class Duke {
         currentTask.markAsDone();
 
         printDivider();
-        System.out.println("    Nice! I've marked this task as done:");
-        System.out.println("      [" + currentTask.getStatusIcon() + "] " + currentTask.description);
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t  [" + currentTask.getStatusIcon() + "] " + currentTask.description);
         System.out.println("");
         printDivider();
     }
@@ -60,8 +61,8 @@ public class Duke {
         currentTask.markAsUndone();
 
         printDivider();
-        System.out.println("    OK, I've marked this task as not done yet:");
-        System.out.println("      [" + currentTask.getStatusIcon() + "] " + currentTask.description);
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        System.out.println("\t  [" + currentTask.getStatusIcon() + "] " + currentTask.description);
         System.out.println("");
         printDivider();
     }
