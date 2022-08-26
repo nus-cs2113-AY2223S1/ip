9fdf1f18fbb0fc555ccf5673e49a54f579de26aa
Amethyst <e0978391@u.nus.edu>
2022-08-27 02:21:55 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,13 +6,15 @@ public class Duke {
      *
      */
     private static final int MAXLISTNUM = 100;
-    private static String[] list = new String[MAXLISTNUM];
-    private static int listLength = 0; 
+    private static Task[] taskList = new Task[MAXLISTNUM];
+    private static int listLength = 0;
+
     public static void main(String[] args) {
-        
+
         showWelcomeMessage();
         handleUserInput();
     }
+
     private static void showWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -29,28 +31,53 @@ public class Duke {
 
     private static void handleUserInput() {
         Scanner scanner = new Scanner(System.in);
+        int index;
         while (true) {
             String inputText = scanner.nextLine();
             while (inputText.trim().isEmpty())
                 inputText = scanner.nextLine();
-            switch (inputText) {
+            final String[] split = inputText.trim().split("\\s+", 2);
+            final String[] commandTypeAndParams = split.length == 2 ? split : new String[] { split[0], "" };
+            final String commandType = commandTypeAndParams[0];
+            final String commandArgs = commandTypeAndParams[1];
+            switch (commandType) {
+            case "list":
+                System.out.println("    ____________________________________________________________");
+                for (int i = 1; i <= listLength; i++) {
+                    System.out.println("    " + String.valueOf(i) + "." + "[" + taskList[i - 1].getStatusIcon()
+                            + "] " + taskList[i - 1].getDescription());
+                }
+                ;
+                System.out.println("    ____________________________________________________________");
+                System.out.println();
+                break;
+            case "mark":
+                index = Integer.valueOf(commandArgs) - 1;
+                taskList[index].setStringState(true);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println(
+                        "      " + "[" + taskList[index].getStatusIcon() + "] " + taskList[index].getDescription());
+                System.out.println("    ____________________________________________________________");
+                break;
+            case "unmark":
+                index = Integer.valueOf(commandArgs) - 1;
+                taskList[index].setStringState(false);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println(
+                        "      " + "[" + taskList[index].getStatusIcon() + "]" + taskList[index].getDescription());
+                System.out.println("    ____________________________________________________________");
+                break;
             case "bye":
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 System.exit(0);
-            case "list":
-                System.out.println("    ____________________________________________________________");
-                for (int i=1;i<=listLength;i++){
-                    System.out.println("    "+String.valueOf(i)+". "+list[i-1]);
-                };
-                System.out.println("    ____________________________________________________________");
-                System.out.println();
-                break;
             default:
-                list[listLength] = inputText;
+                taskList[listLength] = new Task(inputText);
                 listLength++;
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    "+"added: "+inputText);
+                System.out.println("    " + "added: " + inputText);
                 System.out.println("    ____________________________________________________________");
                 System.out.println();
                 break;
