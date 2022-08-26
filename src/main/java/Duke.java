84f21158102834b8c79b2467da8c434c6aca5f07
shengiv <66622728+shengiv@users.noreply.github.com>
2022-08-26 14:57:45 +0800

    Add ability to echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "______________________________________________\n";
         System.out.println(logo+ " Hello! I'm Duke\n" +
-                " What can I do for you?\n" + logo +
-                " Bye. Hope to see you again soon!\n" +logo);
+                " What can I do for you?\n" + logo);
+        String lineInput;
+        Scanner in = new Scanner(System.in);
+        lineInput = in.nextLine();
+        while(!lineInput.equals("bye")){
+            System.out.println(lineInput + System.lineSeparator());
+            lineInput = in.nextLine();
+        }
+        System.out.println(logo + "Bye. Hope to see see you again soon!" + System.lineSeparator() + logo);
     }
 }
