3936521dbc05daa181e48ef474726fba20e2ca1f
morbenami1 <mor.benami1@mail.huji.ac.il>
2022-08-27 20:00:17 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        PrintHorizontalLine();
+        PrintGreeting();
+        PrintHorizontalLine();
+        PrintExit();
+        PrintHorizontalLine();
+    }
+
+    public static void PrintGreeting(){
+        System.out.println("Hello I'm Duke");
+        System.out.println("Hello what can I do for you?");
+    }
+
+    public static void PrintExit(){
+        System.out.println("Bye. Hope to see you soon");
+    }
+    public static void PrintHorizontalLine(){
+        System.out.println("-----------------------------------------------------------");
     }
 }
