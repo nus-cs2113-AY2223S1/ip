6b261132bc45123fd2591a9ff0711da50a412c54
alfred-leong <alfredleongweilun@gmail.com>
2022-08-27 17:18:56 +0800

    Completed Automated Text UI Testing + Removed instructions from welcomeUser

--- src/main/java/Bloom.java
+++ src/main/java/Bloom.java
@@ -7,15 +7,6 @@ public class Bloom {
         drawLine();
         System.out.println("Hello! I'm Bloom, your personal task manager.");
         System.out.println();
-        System.out.println("Here are the available commands: ");
-        System.out.println("todo <task name> -- add task (todo)");
-        System.out.println("deadline <taskname> /by <date and/or time> -- add task (deadline)");
-        System.out.println("event <taskname> /at <date and/or time period -- add task (event)");
-        System.out.println("list -- to list current tasks");
-        System.out.println("mark <task number> -- to mark the respective task as completed");
-        System.out.println("unmark <task number> -- to unmark the respective task as not completed");
-        System.out.println("bye -- to exit the program");
-        System.out.println();
         System.out.println("What can I do for you?");
         drawLine();
     }
