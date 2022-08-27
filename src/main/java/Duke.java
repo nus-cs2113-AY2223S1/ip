5e3cfb8be78ea8770b9ffbfbdef109dc87343054
nvknow <1375782557@qq.com>
2022-08-27 18:52:11 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,6 @@ import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        //        String logo = " ____        _        \n"
-        //                + "|  _ \\ _   _| | _____ \n"
-        //                + "| | | | | | | |/ / _ \\\n"
-        //                + "| |_| | |_| |   <  __/\n"
-        //                + "|____/ \\__,_|_|\\_\\___|\n";
-        //        System.out.println("Hello from\n" + logo);
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
