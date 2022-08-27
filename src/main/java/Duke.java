bf2885c453338f5896324e6cc8b430eeda115885
wangtingjia <wangtingjia49@gmail.com>
2022-08-27 18:04:52 +0800

    Improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,36 @@
 import java.util.Scanner;
 
 public class Duke {
+    static final String GREETING = "Hello! I'm Timmy\n What can I do for you?\n \n";
+    static final String GOODBYE = "Bye. Hope to see you again soon\n";
+
+    static TaskManager myManager = new TaskManager();
+
+    public static void handleInput(String input) {
+        if (input.equals("list")) {
+            myManager.print();
+        } else if (input.startsWith("mark")){
+            myManager.markAsDone(input);
+        } else if (input.startsWith("unmark")) {
+            myManager.removeMark(input);
+        } else if (input.startsWith("todo")){
+            myManager.addTodo(input);
+        } else if (input.startsWith("deadline")) {
+            myManager.addDeadline(input);
+        } else if (input.startsWith("event")) {
+            myManager.addEvent(input);
+        }
+    }
+
     public static void main(String[] args) {
-        final String GREETING = "Hello! I'm Timmy\n What can I do for you?\n \n";
-        final String GOODBYE = "Bye. Hope to see you again soon\n";
-
-        TaskManager myManager = new TaskManager();
-
         System.out.println(GREETING);
-
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+        String userInput = in.nextLine();
 
-        while (true) {
-            if (input.equals("bye")) {
-                break;
-            } else if (input.equals("list")) {
-                myManager.print();
-            } else if (input.startsWith("mark")){
-                myManager.markAsDone(input);
-            } else if (input.startsWith("unmark")) {
-                myManager.removeMark(input);
-            } else if (input.startsWith("todo")){
-                myManager.addTodo(input);
-            } else if (input.startsWith("deadline")) {
-                myManager.addDeadline(input);
-            } else if (input.startsWith("event")) {
-                myManager.addEvent(input);
-            }
+        while (!userInput.equals("bye")) {
+            handleInput(userInput);
             System.out.println();
-            input = in.nextLine();
+            userInput = in.nextLine();
         }
 
         System.out.println(GOODBYE);
