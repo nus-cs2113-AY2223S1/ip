8aced86e2ea20792d732ddecac885a34405abc4a
cheehengk <cheehengk@yahoo.com.sg>
2022-08-28 15:24:23 +0800

    Add Level-1 increment echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static boolean processCommand(String command) {
+        if (command.equals("bye")) {
+            System.out.println("Bye and see you again soon!");
+            return false;
+        }
+        System.out.println("Echoing..." + command + "\n____________________");
+        return true;
+    }
+
     public static void main(String[] args) {
-        String InitialGreeting = "Hello! I'm Azmuth\n" +
+        String command;
+        Boolean online = true;
+        Scanner in = new Scanner(System.in);
+        String InitialGreeting = "Hello! I'm Duke\n" +
                 "What can I do for you?\n" +
-                "____________________\n" +
-                "Bye. Hope to see you again soon!";
+                "____________________";
         System.out.println(InitialGreeting);
+        while (online) {
+            command= in.nextLine();
+            online = processCommand(command);
+        }
+        in.close();
     }
 }
