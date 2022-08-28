ec7ba0542cb8fc91783538fb60308f6990881159
INDR <indraneel.paranjape@gmail.com>
2022-08-28 18:30:22 +0800

    Add Duke Level-0 with tags

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("test commit");
+        String line = "------------------------\n";
+        System.out.println(line + "Hello! I'm Duke!\n What can I do for you?\n" + line);
+        System.out.println("Bye! See you again!");
     }
 }
