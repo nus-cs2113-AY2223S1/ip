fa9ba6a9aad1334b0fffc4ed8af77766edbfda1d
LAPTOP-KFC5FK3E\kwuac <kwuachunren@gmail.com>
2022-08-29 17:09:07 +0800

    Add, List with static String array

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,9 @@
 import java.util.Scanner;
+
 public class Duke {
-    public static void Echo(){
-        boolean isRunning = true;
-        while(isRunning){
-            String line;
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
-            if(line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                isRunning = false;
-            }
-            else {
-                System.out.println(line);
-            }
-        }
-    }
-    public static void main(String[] args) {
+    public static Boolean isRunning = true;
+
+    public static void Greetings() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -23,6 +11,55 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?\n");
-        Echo();
+    }
+
+    public static void Echo() {
+
+        while (isRunning) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isRunning = false;
+            } else {
+                System.out.println(line);
+            }
+        }
+    }
+
+    public static void AddandList() {
+        int counter = 0;
+        String[] tasks = new String[100];
+
+        while (isRunning) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+
+            switch (line) {
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                isRunning = false;
+                break;
+            case "list":
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                break;
+            default:
+                System.out.println("added: " + line);
+                tasks[counter] = line;
+                counter++;
+                break;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Greetings();
+        AddandList();
+
     }
 }
