79abfd5f954fbc6081d607188277d5c6049dc30d
INDR <indraneel.paranjape@gmail.com>
2022-08-29 18:46:08 +0800

    Add level-1 (echo user input)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
+
+    // MAIN FUNCTION
     public static void main(String[] args) {
+        
+        // INITIAL PRINTING
         String line = "------------------------\n";
-        System.out.println(line + "Hello! I'm Duke!\n What can I do for you?\n" + line);
-        System.out.println("Bye! See you again!");
+        System.out.println(line + "Hello! I'm Zuck!\n What can I do for you?\n" + line);
+        
+        // VARS
+        String userInput = "";
+        Scanner scanObj = new Scanner(System.in); // creates new scanner object
+        
+        while(!userInput.equals("bye")){
+            userInput = scanObj.nextLine();  // Read user input
+            System.out.println("You said:" + userInput);
+        }
+
+        scanObj.close();
+        System.out.println("See you again!");
     }
 }
