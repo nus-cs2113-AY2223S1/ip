61d5b158ac330db041b9d5765418428631aac8f8
Ng Yong Jie <e0978309@u.nus.edu>
2022-08-30 17:37:07 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +11,14 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+
+        Scanner sc = new Scanner(System.in);
+        String echo = sc.next();
+        while (!Objects.equals(echo, "bye")){
+            System.out.println(echo);
+            echo = sc.next();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
