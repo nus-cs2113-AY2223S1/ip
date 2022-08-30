369e45da2030be35f03a69fa8b59ecb0b9e00fc7
Ng Yong Jie <e0978309@u.nus.edu>
2022-08-30 17:53:50 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-
+        // level 1
         Scanner sc = new Scanner(System.in);
         String echo = sc.next();
         while (!Objects.equals(echo, "bye")){
@@ -20,5 +20,29 @@ public class Duke {
             echo = sc.next();
         }
         System.out.println("Bye. Hope to see you again soon!");
+
+        // level 2
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        int count = 0;
+        String[] storage = new String[100];
+        while (true){
+            Scanner sc2 = new Scanner(System.in);
+            String add = sc2.nextLine();
+            if(Objects.equals(add, "list")){
+                for(int i=0; i<count; i++){
+                    System.out.println((i+1)+". " +storage[i]);
+                }
+                continue;
+            } else if (Objects.equals(add, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                storage[count] = add;
+                System.out.println("added: " + add);
+            }
+            count++;
+        }
+
     }
 }
