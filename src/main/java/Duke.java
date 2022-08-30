8241d590cc816c1e985faaa139033434d3f3d6bc
lcsroy <lcsroy99@gmail.com>
2022-08-30 21:17:54 +0800

    Level 2- Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,12 +12,20 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
+        String[] List = new String[100];
+        int ListNo = 0;
         do{
             line = in.nextLine();
             if (line.equals("bye")){
                 break;
-            }else {
-                System.out.println(line);
+            }else if(line.compareTo("list") != 0) {
+                System.out.println("added: " + line);
+                List[ListNo] = line;
+                ListNo++;
+            }else if(line.compareTo("list") == 0){
+                for(int i = 0; i < ListNo; i++) {
+                    System.out.println((i+1) + ". " + List[i]);
+                }
             }
         } while (line.compareTo("bye") != 0);
         System.out.println("Bye. Hope to see you again soon!");
