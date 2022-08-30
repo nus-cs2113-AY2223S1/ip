df9d154b6bf0c8e1fc981ac1657968c0c1b79c3f
huy_huy <e0978364@u.nus.edu>
2022-08-30 17:13:40 +0800

    Add error handler but doesn't work

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 
 public class Duke {
-    //========================================= GLOBAL CONSTANT ========================//
+    // ========================================= GLOBAL CONSTANT
     private static final int MAXIMUM_NUMBER_OF_TASKS = 100;
 
     private static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!";
     private static final String MESSAGE_HELLO = "Hello! I'm a chatbot Duke made by Than Duc Huy\n" +
             "Type the command to start interacting with Duke";
-    private static final String MESSAGE_COMMAND_LISTS = "Supported commands: list, mark, todo, deadline, event, bye";
-    private static final String MESSAGE_HORIZONTAL_LINE = "===================================================";
+    private static final String MESSAGE_COMMAND_LISTS = "Supported commands: list, mark, unmark, todo, deadline, event, bye";
+    private static final String MESSAGE_DIVIDER = "===================================================";
+    private static final String MESSAGE_UNKNOWN_COMMAND = "Unknown Command";
 
+    private static final String ERROR_MESSAGE_BYE = "";
+    private static final String ERROR_MESSAGE_LIST = "";
+    private static final String ERROR_MESSAGE_MARK = "";
+    private static final String ERROR_MESSAGE_UNMARK = "";
+    private static final String ERROR_MESSAGE_TODO = "";
+    private static final String ERROR_MESSAGE_DEADLINE = "";
+    private static final String ERROR_MESSAGE_EVENT = "";
 
-    //========================================= GLOBAL VARIABLES ========================//
+    // ========================================= GLOBAL VARIABLES
     static Scanner in = new Scanner(System.in);
     static Task[] tasksList = new Task[MAXIMUM_NUMBER_OF_TASKS];
     static boolean isRunning = true;
 
-
-    //========================================= MAIN ========================//
+    // ========================================= MAIN
     public static void main(String[] args) {
         helloMessage();
         while (isRunning) {
@@ -25,73 +32,155 @@ public class Duke {
         }
     }
 
-    private static void parseCommand() {
-        String line = in.nextLine();
-        String lowerCaseLine = line.toLowerCase();
-        if (lowerCaseLine.contains("bye")) {
-            exit();
-        } else if (lowerCaseLine.contains("list")) {
-            list();
-        } else if (lowerCaseLine.contains("mark")) {
-            mark(line);
-        } else if (lowerCaseLine.contains("todo") || lowerCaseLine.contains("deadline") || lowerCaseLine.contains("event")) {
-            add(line);
-        } else {
-            unknownCommandMessage();
+    // ========================================= GLOBAL METHOD
+    private static void showToUser(String... message) {
+        for (String m : message) {
+            System.out.println(message);
         }
     }
-    //========================================= GLOBAL METHOD ========================//
 
-    private static void exit() {
+    private static void parseCommand() {
+        String line = in.nextLine();
+        String[] parsedLine = line.split(" ");
+        String commandWord = parsedLine[0].toLowerCase();
+
+        switch (commandWord) {
+            case "bye":
+                exit(parsedLine);
+                break;
+            case "list":
+                list(parsedLine);
+                break;
+            case "mark":
+                mark(parsedLine);
+                break;
+            case "unmark":
+                mark(parsedLine);
+                break;
+            case "todo":
+                add(line);
+                break;
+            case "deadline":
+                add(line);
+                break;
+            case "event":
+                add(line);
+                break;
+            default:
+                unknownCommand();
+                break;
+        }
+    }
+
+    /*
+     * private static boolean isCommandBye(String line){
+     * return line.toLowerCase().contains("bye");
+     * }
+     * private static boolean isCommandList(String line){
+     * return line.toLowerCase().contains("list");
+     * }
+     * private static boolean isCommandMark(String line){
+     * return line.toLowerCase().contains("mark");
+     * }
+     * private static boolean isCommandAdd(String line){
+     * String lowerCase = line.toLowerCase();
+     * return lowerCase.contains("todo") || lowerCase.contains("deadline") ||
+     * lowerCase .contains("event");
+     * }
+     * 
+     */
+
+    private static void commandErrorHandler(String errorMessage) {
+        switch (errorMessage) {
+            case "bye":
+                showToUser(ERROR_MESSAGE_BYE);
+            case "list":
+                showToUser(ERROR_MESSAGE_LIST);
+            case "mark":
+                showToUser(ERROR_MESSAGE_MARK);
+            case "unmark":
+                showToUser(ERROR_MESSAGE_UNMARK);
+            case "todo":
+                showToUser(ERROR_MESSAGE_TODO);
+            case "deadline":
+                showToUser(ERROR_MESSAGE_DEADLINE);
+            case "event":
+                showToUser(ERROR_MESSAGE_EVENT);
+            default:
+                break;
+        }
+    }
+
+    private static void exit(String[] parsedLine) {
+        try {
+            if (parsedLine[1] != null) {
+                throw new DukeException("bye");
+            }
+        } catch (DukeException e) {
+            commandErrorHandler(e.getMessage());
+        }
         isRunning = false;
         exitMessage();
     }
 
-    private static void unknownCommandMessage() {
-        System.out.println("Please indicate task type: todo/deadline/event");
+    private static void unknownCommand() {
+        showToUser(MESSAGE_UNKNOWN_COMMAND, MESSAGE_COMMAND_LISTS);
     }
 
-    private static void list() {
-        System.out.println("========================LIST=======================");
+    private static void list(String[] parsedLine) {
+        try {
+            if (parsedLine[1] != null) {
+                throw new DukeException("list");
+            }
+        } catch (DukeException e) {
+            commandErrorHandler(e.getMessage());
+        }
+        showToUser("LIST", MESSAGE_DIVIDER);
         for (int i = 0; i < Task.numberOfTasks; i++) {
             System.out.print((i + 1) + ".");
             tasksList[i].printTask();
         }
-        System.out.println("===================================================");
+        showToUser(MESSAGE_DIVIDER);
+    }
+
+    private static void list() {
+        showToUser("LIST", MESSAGE_DIVIDER);
+        for (int i = 0; i < Task.numberOfTasks; i++) {
+            System.out.print((i + 1) + ".");
+            tasksList[i].printTask();
+        }
+        showToUser(MESSAGE_DIVIDER);
     }
 
     private static void add(String line) {
         String newTaskType = line.split(" ")[0];
         String[] taskDescriptor = line.replace(newTaskType, "").trim().split("/");
         switch (newTaskType.toLowerCase()) {
-        case "todo":
-            tasksList[Task.numberOfTasks] = new Todo(taskDescriptor[0]);
-            break;
-        case "deadline":
-            tasksList[Task.numberOfTasks] = new Deadline(taskDescriptor[0], taskDescriptor[1].trim());
-            break;
-        case "event":
-            tasksList[Task.numberOfTasks] = new Event(taskDescriptor[0], taskDescriptor[1].trim());
-            break;
-        default:
-            break;
+            case "todo":
+                tasksList[Task.numberOfTasks] = new Todo(taskDescriptor[0]);
+                break;
+            case "deadline":
+                tasksList[Task.numberOfTasks] = new Deadline(taskDescriptor[0], taskDescriptor[1].trim());
+                break;
+            case "event":
+                tasksList[Task.numberOfTasks] = new Event(taskDescriptor[0], taskDescriptor[1].trim());
+                break;
+            default:
+                break;
         }
         addTaskMessage();
     }
 
     private static void addTaskMessage() {
-        drawHorizontal();
         System.out.println("Got it! You just add a new Task");
         System.out.print("\t");
         tasksList[Task.numberOfTasks - 1].printTask();
         System.out.println("Number of tasks in the list: " + Task.numberOfTasks);
-        drawHorizontal();
     }
 
-    public static void mark(String line) {
-        String[] parseArg = line.split(" ");
-        int index = Integer.parseInt(parseArg[1]);
-        if (parseArg[0].equalsIgnoreCase("mark")) {
+    public static void mark(String[] parsedLine) {
+        int index = Integer.parseInt(parsedLine[1]);
+        if (parsedLine[0].equalsIgnoreCase("mark")) {
             tasksList[index - 1].isDone = true;
         } else {
             tasksList[index - 1].isDone = false;
@@ -100,19 +189,11 @@ public class Duke {
     }
 
     private static void exitMessage() {
-        System.out.println(MESSAGE_EXIT);
-        drawHorizontal();
-    }
-
-    private static void drawHorizontal() {
-        System.out.println(MESSAGE_HORIZONTAL_LINE);
+        showToUser(MESSAGE_DIVIDER, MESSAGE_EXIT, MESSAGE_DIVIDER);
     }
 
     private static void helloMessage() {
-        drawHorizontal();
-        System.out.println(MESSAGE_HELLO);
-        System.out.println(MESSAGE_COMMAND_LISTS);
-        drawHorizontal();
+        showToUser(MESSAGE_DIVIDER, MESSAGE_HELLO, MESSAGE_COMMAND_LISTS, MESSAGE_DIVIDER);
     }
 
 }
