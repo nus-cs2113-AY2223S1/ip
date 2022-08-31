5b1fa3667ddd878280f0e4007901d91933233d2e
John <johntohjiajun@gmail.com>
2022-09-01 02:06:28 +0800

    Add test cases
    Has issues running the tests

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -4,14 +4,14 @@ public class UI {
     public static void welcomeUser() {
         String welcomeText = "-------------------------------------\n"
                 + "Hello! I am not Duke.\n"
-                + "Here are a list of commands: \n"
+                + "Here are a list of commands:\n"
                 + "\ttodo <string> : adds a Todo to the list\n"
                 + "\tdeadline <string> /by <string> : adds a deadline and its deadline...\n"
                 + "\tevent <string> /at <string> : adds an event and its time...\n"
-                + "\tmark <task id> : marks a task \n"
-                + "\tunmark <task id> : unmarks a task \n"
-                + "\tlist : lists out all your tasks \n"
-                + "\tbye : exits the program \n"
+                + "\tmark <task id> : marks a task\n"
+                + "\tunmark <task id> : unmarks a task\n"
+                + "\tlist : lists out all your tasks\n"
+                + "\tbye : exits the program\n"
                 + "I WILL CRASH IF YOU DO NOT COMPLY!!!\n"
                 + "-------------------------------------";
         System.out.println(welcomeText);
