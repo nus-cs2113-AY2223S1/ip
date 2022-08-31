c6c73766ce0f40ffe1d533a5274b9e0483c5b44e
LAPTOP-KFC5FK3E\kwuac <kwuachunren@gmail.com>
2022-08-31 11:13:28 +0800

    Fixed certain parts of the code and checked to ensure it follows coding standard

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -14,6 +14,7 @@ public class Task {
     }
 
     public void markasDone() {
+
         this.isDone = true;
     }
 
@@ -22,6 +23,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
+
         return (isDone ? "X" : " ");
     }
 }
