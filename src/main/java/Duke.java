25899fd06b8e6a39e4268d972f7c6ccbe0441624
Devesh Logendran <deveshl@users.noreply.github.com>
2022-09-01 15:25:01 +0800

    Duke: break out runCommand into functions, add some error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,7 +18,7 @@ public class Duke {
     static List<Task> tasks = new ArrayList<>();
 
     public static void runCommand(String command) {
-        String[] args = command.split(" ");
+        String[] args = command.split(" ", 2);
         String action = args[0];
         switch (action) {
         case "bye":
@@ -26,29 +26,57 @@ public class Duke {
             System.exit(0);
             // no break needed, the code has already exited
         case "list":
-            for (int i = 0; i < tasks.size(); i++) {
-                Task task = tasks.get(i);
-                String checkmark = task.isDone() ? "X" : " ";
-                System.out.printf("%d.[%s] %s\n", i + 1, checkmark, task.getDescription());
-            }
+            listTasks();
             break;
         case "mark":
         case "unmark": {
             boolean isDone = action.equals("mark");
-            int taskIndex = Integer.parseInt(args[1]) - 1; // add code to handle oob, missing argument
-
-            Task task = tasks.get(taskIndex);
-            task.setDone(isDone);
-            System.out.println("Okay, dude, I've marked this task as " + (isDone ? "done" : "not done yet") + ": ");
-            System.out.println(task.getDescription());
+            if (args.length < 2) {
+                System.out.println("Not enough arguments, dude.");
+                break;
+            }
+            int index;
+            try {
+                index = Integer.parseInt(args[1]) - 1;
+            } catch (NumberFormatException e) {
+                // user either didn't enter a number or number was too big for integer
+                // not a nice way to use exceptions but alternatives are cumbersome
+                System.out.println("Invalid argument, dude.");
+                break;
+            }
+            setTaskStatus(index, isDone);
             break;
         }
         default: {
-            Task task = new Task(command);
-            tasks.add(task);
-            System.out.println("added: " + command);
+            addTask(command);
             break;
         }
+        }
+    }
+
+    private static void addTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        System.out.println("added: " + description);
+    }
+
+    private static void setTaskStatus(int taskIndex, boolean isDone) {
+        if (taskIndex < 0 || taskIndex >= tasks.size()) {
+            System.out.println("There's no task with that number, dude.");
+            return;
+        }
+
+        Task task = tasks.get(taskIndex);
+        task.setDone(isDone);
+        System.out.println("Okay, dude, I've marked this task as " + (isDone ? "done" : "not done yet") + ": ");
+        System.out.println(task.getDescription());
+    }
+
+    private static void listTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            String checkmark = task.isDone() ? "X" : " ";
+            System.out.printf("%d.[%s] %s\n", i + 1, checkmark, task.getDescription());
         }
     }
 
