adb91168910afe4596b0ddac774e40e442ef273e
deenliong <deenliong@gmail.com>
2022-09-02 02:59:14 +0800

    Add todo, deadline and event.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Duke {
@@ -8,51 +7,83 @@ public class Duke {
         String line;
         Scanner in = new Scanner(System.in);
 
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        int id = 1;
-
-        List<Task> tasks = new ArrayList<Task>();
-//        tasks.add(null);
         do {
             line = in.nextLine();
-            if (line.equals("bye")) {
+            String breakLine[] = line.split(" ", 2);
+            String firstWord = breakLine[0];
+
+            if (firstWord.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             }
 
-            if (line.equals("list")) {  // list out the items
+            if (firstWord.equals("list")) {  // list out the items
                 System.out.println("\t_____________________");
                 System.out.println("\tHere are the tasks in your list:");
-                for (Task task : tasks) {
-                    System.out.println("\t" + task.id + "." + task.getStatusIcon() + task.description);
+                for (int i = 0; i < taskCount; i += 1) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks[i]);
                 }
 
+
                 System.out.println("\t_____________________\n");
-            } else if (line.contains("mark")) {
+            } else if (firstWord.equals("mark")) {
                 int markId = Integer.parseInt(line.replaceAll("[^0-9]", ""));    // gets the id
                 int taskId = markId - 1;
                 System.out.println("\tNice! I've marked this task as done:");
-                tasks.get(taskId).setDone(tasks.get(taskId).isDone);
-                System.out.println("\t" + tasks.get(taskId).getStatusIcon() + tasks.get(taskId).getDescription());
-            } else if (line.contains("unmark")) {
+                tasks[taskId].setDone(tasks[taskId].isDone);
+                System.out.println("\t" + tasks[taskId].getStatusIcon() + tasks[taskId].getDescription());
+            } else if (firstWord.equals("unmark")) {
                 int markId = Integer.parseInt(line.replaceAll("[^0-9]", ""));    // gets the id
                 int taskId = markId - 1;
-                System.out.println("\tOK, I've marked this task as not done yet:");
-                tasks.get(taskId).setDone(tasks.get(taskId).isDone);
-                System.out.println("\t" + tasks.get(taskId).getStatusIcon() + tasks.get(taskId).getDescription());
-            } else if (line.equals("total")) {
-                System.out.println(tasks.size());
+                if (!tasks[taskId].isDone) {
+                    System.out.println("this is already unmarked");
+                } else {
+                    System.out.println("\tOK, I've marked this task as not done yet:");
+                    tasks[taskId].setDone(tasks[taskId].isDone);
+                    System.out.println("\t" + tasks[taskId].getStatusIcon() + tasks[taskId].getDescription());
+                }
+
+            } else if (firstWord.equals("total")) {
+                System.out.println(taskCount);
             } else {
-                Task t = new Task(line, id);
-                id += 1;
-                tasks.add(t);
+
+                if (firstWord.equals("todo")) {
+                    String desc = breakLine[1];
+                    Task t = new Todo(desc);
+                    tasks[taskCount] = t;
+                }
+                else if (firstWord.equals("deadline")){
+                    String desc = breakLine[1];
+                    String breakBy[] = desc.split("/by", 2);
+                    String detail = breakBy[0];
+                    String by = breakBy[1];
+                    Task d = new Deadline(detail, by);
+                    tasks[taskCount] = d;
+                }
+                else if (firstWord.equals("event")){
+                    String desc = breakLine[1];
+                    String breakAt[] = desc.split("/at", 2);
+                    String detail = breakAt[0];
+                    String at = breakAt[1];
+                    Task e = new Event(detail, at);
+                    tasks[taskCount] = e;
+                }
+
                 System.out.println("\t_____________________");
-                System.out.println("\t" + "added: " + t.description);
+                System.out.println("\t" + "Got it. I've added this task:");
+                System.out.println("\t" + "added: " + tasks[taskCount]);
                 System.out.println("\t_____________________\n");
+                taskCount += 1;
             }
 
         } while (!line.equals("bye"));
+
+
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,13 @@
 public class Task {
     protected String description;
+
     protected boolean isDone;
 
-    protected int id;
-
-    public Task(String description, int id) {
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
-        this.id = id;
     }
+
 
     public String getStatusIcon() {
         if (isDone) { // mark done task with X
@@ -28,5 +27,12 @@ public class Task {
         } else {
             this.isDone = false;
         }
+    }
+
+    @Override
+    public String toString() {
+
+        return getStatusIcon() + description;
+//        return getStatusIcon() ;
     }
 }
