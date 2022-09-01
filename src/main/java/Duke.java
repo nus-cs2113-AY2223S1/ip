194f359afb81e69686bb76c58af77bef3893423f
karthikstar <64301417+karthikstar@users.noreply.github.com>
2022-09-01 23:11:03 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void generateTaskStatus(String taskIcon, String statusIcon, String description) {
+        System.out.println("\t[" + taskIcon + "]" + "[" + statusIcon + "] " + description);
+    }
+    public static void drawLine() {
+        System.out.println("------------------------------------");
+    }
+    public static void addedMsg() {
+        System.out.println("Got it. I've added this task:");
+    }
+
+    public static void taskCountReminder(int noOfTasks) {
+        System.out.printf("Now you have %d tasks in the list.\n",noOfTasks);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,11 +22,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        System.out.println("------------------------------------");
+        drawLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("------------------------------------");
+        drawLine();
         Scanner in = new Scanner(System.in);
         String input;
         Task[] taskArray = new Task[100];
@@ -20,64 +33,64 @@ public class Duke {
         do {
             input = in.nextLine();
             if(input.equals("bye")){
-                System.out.println("------------------------------------");
+                drawLine();
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("------------------------------------");
+                drawLine();
             } else if (input.equals("list")){
-                System.out.println("------------------------------------");
+                drawLine();
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= addCount; i++){
-                    System.out.println(i + "."+ "[" +taskArray[i-1].getTaskIcon() + "]" + "[" + taskArray[i-1].getStatusIcon() + "] "+ taskArray[i-1].getDescription());
+                    generateTaskStatus(taskArray[i-1].getTaskIcon(), taskArray[i-1].getStatusIcon(), taskArray[i-1].getDescription());
                 }
-                System.out.println("------------------------------------");
+                drawLine();
             } else if (input.contains("unmark")){
                 String[] inputWords = input.split(" ");
                 int choiceToUnMark = Integer.parseInt(inputWords[1]);
                 taskArray[choiceToUnMark - 1].unMarkTask();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("\t[" +taskArray[choiceToUnMark-1].getTaskIcon() + "]" + "[" + taskArray[choiceToUnMark-1].getStatusIcon() + "] " + taskArray[choiceToUnMark-1].getDescription());
-                System.out.println("------------------------------------");
+                generateTaskStatus(taskArray[choiceToUnMark-1].getTaskIcon(), taskArray[choiceToUnMark-1].getStatusIcon(),taskArray[choiceToUnMark-1].getDescription() );
+                drawLine();
 
             } else if (input.contains("mark")){
                 String[] inputWords = input.split(" ");
                 int choiceToMark = Integer.parseInt(inputWords[1]);
                 taskArray[choiceToMark - 1].markTask();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t[" +taskArray[choiceToMark-1].getTaskIcon() + "]" + "[" + taskArray[choiceToMark-1].getStatusIcon() + "] " + taskArray[choiceToMark-1].getDescription());
-                System.out.println("------------------------------------");
+                generateTaskStatus(taskArray[choiceToMark-1].getTaskIcon(), taskArray[choiceToMark-1].getStatusIcon(), taskArray[choiceToMark-1].getDescription());
+                drawLine();
             } else if(input.contains("todo")) {
                 taskArray[addCount] = new Todo(input);
-                System.out.println("------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("\t[" +taskArray[addCount].getTaskIcon() + "]" + "[" + taskArray[addCount].getStatusIcon() + "] " + taskArray[addCount].getDescription());
+                drawLine();
+                addedMsg();
+                generateTaskStatus(taskArray[addCount].getTaskIcon(), taskArray[addCount].getStatusIcon(), taskArray[addCount].getDescription());
                 addCount +=1;
-                System.out.printf("Now you have %d tasks in the list\n",addCount);
-                System.out.println("------------------------------------");
+                taskCountReminder(addCount);
+                drawLine();
             } else if (input.contains("deadline")) {
                 taskArray[addCount] = new Deadline(input);
-                System.out.println("------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("\t[" +taskArray[addCount].getTaskIcon() + "]" + "[" + taskArray[addCount].getStatusIcon() + "] " + taskArray[addCount].getDescription());
+                drawLine();
+                addedMsg();
+                generateTaskStatus(taskArray[addCount].getTaskIcon(), taskArray[addCount].getStatusIcon(), taskArray[addCount].getDescription());
                 addCount +=1;
-                System.out.printf("Now you have %d tasks in the list\n",addCount);
-                System.out.println("------------------------------------");
+                taskCountReminder(addCount);
+                drawLine();
             } else if (input.contains("event")) {
                 taskArray[addCount] = new Event(input);
-                System.out.println("------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("\t[" +taskArray[addCount].getTaskIcon() + "]" + "[" + taskArray[addCount].getStatusIcon() + "] " + taskArray[addCount].getDescription());
+                drawLine();
+                addedMsg();
+                generateTaskStatus(taskArray[addCount].getTaskIcon(), taskArray[addCount].getStatusIcon(), taskArray[addCount].getDescription());
                 addCount +=1;
-                System.out.printf("Now you have %d tasks in the list\n",addCount);
-                System.out.println("------------------------------------");
+                taskCountReminder(addCount);
+                drawLine();
             }
             else {
                 taskArray[addCount] = new Task(input);
-                System.out.println("------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("\t[" +taskArray[addCount].getTaskIcon() + "]" + "[" + taskArray[addCount].getStatusIcon() + "] " + taskArray[addCount].getDescription());
+                drawLine();
+                addedMsg();
+                generateTaskStatus(taskArray[addCount].getTaskIcon(), taskArray[addCount].getStatusIcon(), taskArray[addCount].getDescription());
                 addCount +=1;
-                System.out.printf("Now you have %d tasks in the list\n",addCount);
-                System.out.println("------------------------------------");
+                taskCountReminder(addCount);
+                drawLine();
             }
         } while (!input.equals("bye"));
     }
