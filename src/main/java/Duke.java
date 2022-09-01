d90add66efe7554be24b6308c60ba9bc120c6689
sylviahe171 <sylviahe171@gmail.com>
2022-09-01 23:14:04 +0800

    more refactor

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,12 +17,10 @@ public class Duke {
         while(!val.equals("bye")){
             System.out.println("____________________________________________________________");
             if(val.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for(int i = 0; i < length; i++) {
-                    System.out.println("  " + (i + 1) + "." + tasks[i]);
-                }
+                printList(tasks, length);
             } else if (!val.contains("mark")) {
                 System.out.println("Got it. I've added this task:");
+
                 int index = val.indexOf("/");
 
                 if(val.contains(TODO)){
@@ -37,26 +35,31 @@ public class Duke {
                 length++;
                 System.out.println("Now you have " + length + " tasks in the list.");
             } else if (val.indexOf("mark") == 0) {
-                int id = val.indexOf(" ");
-                String ind = val.substring(id + 1);
-                int index = Integer.parseInt(ind) - 1;
-                tasks[index].setDone(true);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[index]);
+                markTask(val, tasks, true, "Nice! I've marked this task as done:");
             } else{
-                int id = val.indexOf(" ");
-                String ind = val.substring(id + 1);
-                int index = Integer.parseInt(ind) - 1;
-                tasks[index].setDone(false);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(tasks[index]);
+                markTask(val, tasks, false, "OK, I've marked this task as not done yet:");
             }
             System.out.println("____________________________________________________________");
             val = input.nextLine();
-
         }
 
         byeMsg();
+    }
+
+    private static void markTask(String val, Task[] tasks, boolean status, String x) {
+        int id = val.indexOf(" ");
+        String ind = val.substring(id + 1);
+        int index = Integer.parseInt(ind) - 1;
+        tasks[index].setDone(status);
+        System.out.println(x);
+        System.out.println(tasks[index]);
+    }
+
+    private static void printList(Task[] tasks, int length) {
+        System.out.println("Here are the tasks in your list:");
+        for(int i = 0; i < length; i++) {
+            System.out.println("  " + (i + 1) + "." + tasks[i]);
+        }
     }
 
     private static void byeMsg() {
