2c85f967c64d73f81874a4ac229b288b701d76ea
wangtingjia <wangtingjia49@gmail.com>
2022-09-03 12:54:27 +0800

    Add DukeExeception class and removed magic number in TaskManager

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -2,13 +2,17 @@ public class TaskManager {
     protected Task[] tasks;
     protected int taskCount;
 
+    private final int START_INDEX_TODO = 5;
+    private final int START_INDEX_DEADLINE = 9;
+    private final int START_INDEX_EVENT = 6;
+
     public TaskManager() {
         tasks = new Task[100];
         taskCount = 0;
     }
 
     public void addTodo(String input) {
-        String myTask = input.substring(5);
+        String myTask = input.substring(START_INDEX_TODO);
         tasks[taskCount] = new Todo(myTask);
         System.out.println("Added: " + tasks[taskCount].toString());
         taskCount++;
@@ -20,7 +24,7 @@ public class TaskManager {
         if (indexSlash == -1) {
             return;
         }
-        String myTask = input.substring(9, indexSlash-1);
+        String myTask = input.substring(START_INDEX_DEADLINE, indexSlash-1);
         String by = input.substring(indexSlash+4);
         tasks[taskCount] = new Deadline(myTask, by);
         System.out.println("Added: " + tasks[taskCount].toString());
@@ -33,7 +37,7 @@ public class TaskManager {
         if (indexSlash == -1) {
             return;
         }
-        String myTask = input.substring(6, indexSlash-1);
+        String myTask = input.substring(START_INDEX_EVENT, indexSlash-1);
         String at = input.substring(indexSlash+4);
         tasks[taskCount] = new Event(myTask, at);
         System.out.println("Added: " + tasks[taskCount].toString());
