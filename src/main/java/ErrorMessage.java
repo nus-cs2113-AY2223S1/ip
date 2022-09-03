c1d32814a8fbac192075cf2a59d397c9de7ba142
wcwy <thinhong99@gmail.com>
2022-09-03 18:21:24 +0800

    Create two more exceptions and link all exceptions in the program to display error message

--- src/main/java/ErrorMessage.java
+++ src/main/java/ErrorMessage.java
@@ -3,14 +3,21 @@ public enum ErrorMessage {
     INVALID_MARK_OR_UNMARK_INDEX_ERROR_MESSAGE("The selected task index value does not exists."),
     TODO_MISSING_DESCRIPTION_ERROR_MESSAGE("The description of a todo cannot be empty."),
     DEADLINE_MISSING_DESCRIPTION_ERROR_MESSAGE("The description of a todo cannot be empty."),
+    DEADLINE_MISSING_TAG_ERROR_MESSAGE("Separator \" /by \" is not found."),
+    DEADLINE_MISSING_TIME_ERROR_MESSAGE("The time of a deadline cannot be empty."),
     EVENT_MISSING_DESCRIPTION_ERROR_MESSAGE("The description of a todo cannot be empty."),
-    EVENT_MISSING_TIME_ERROR_MESSAGE("Separator \" /at \" is not found."),
-    DEADLINE_MISSING_TIME_ERROR_MESSAGE("Separator \" /by \" is not found."),
+    EVENT_MISSING_TAG_ERROR_MESSAGE("Separator \" /at \" is not found."),
+    EVENT_MISSING_TIME_ERROR_MESSAGE("The time of an event cannot be empty."),
     BANNER("____________________________________________________________");
 
     final String message;
 
     ErrorMessage(String message) {
         this.message = message;
+    }
+
+    @Override
+    public String toString(){
+        return message;
     }
 }
