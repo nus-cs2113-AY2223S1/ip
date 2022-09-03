fe11de7c728f201b2a7e6daa8bc88c80b5dcd029
Paul <iampaullow@gmail.com>
2022-09-03 16:11:38 +0800

    Add A-Exceptions Minimal

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownInputException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -20,69 +20,95 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         Boolean isMarked;
         input = in.nextLine();
+        input = input.trim();
 
         // duke runs until a "bye" is entered
         while (tasks.size() < 100 && !input.equals("bye")) {
-            String [] splitLine = input.split(" ");
+            String [] splitInput = input.split(" ");
             isMarked = Boolean.FALSE;
 
+
+
             // list commands duke to list all the tasks stored and their completion status
-            switch (splitLine[0]) {
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(i + 1 + ". " + tasks.get(i));
-                    }
-                    System.out.println();
-                    break;
-                case "mark":
-                    // mark x commands duke to mark the corresponding task as completed
-                    isMarked = Boolean.TRUE;
-                    System.out.println("Nice! I've marked this task as done: ");
-                    // without the break, both mark and unmark will trigger the similar process
+            // try at the start cos of the errors possibly
+            try {
+                switch (splitInput[0]) {
+                    case "list":
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(i + 1 + ". " + tasks.get(i));
+                        }
+                        System.out.println();
+                        break;
+                    case "mark":
+                        // mark x commands duke to mark the corresponding task as completed
+                        isMarked = Boolean.TRUE;
+                        System.out.println("Nice! I've marked this task as done: ");
+                        // without the break, both mark and unmark will trigger the similar process
 
-                case "unmark":
-                    // unmark x commands duke to mark the corresponding task as uncompleted
-                    // Exceptions could occur
-                    String numericString = input.substring(input.indexOf(" ") + 1);
-                    int markedNum = Integer.parseInt(numericString) - 1;
-                    tasks.get(markedNum).setStatus(isMarked);
-                    if (!isMarked) {
-                        System.out.println("Oh no :( I've marked it as uncompleted: ");
-                    }
-                    System.out.println("  " + tasks.get(markedNum) + "\n");
-                    break;
+                    case "unmark":
+                        // unmark x commands duke to mark the corresponding task as uncompleted
+                        // Exceptions could occur
+                        String numericString = input.substring(input.indexOf(" ") + 1);
+                        int markedNum = Integer.parseInt(numericString) - 1;
+                        tasks.get(markedNum).setStatus(isMarked);
+                        if (!isMarked) {
+                            System.out.println("Oh no :( I've marked it as uncompleted: ");
+                        }
+                        System.out.println("  " + tasks.get(markedNum) + "\n");
+                        break;
 
-                case "todo":
-                    description = input.substring(input.indexOf(" ") + 1);
-                    Task td = new Todo(description);
-                    // this function does all the printing
-                    taskPrint(tasks, td);
-                    break;
+                    case "todo":
+                        if (splitInput.length <= 1) {
+                            throw new StringIndexOutOfBoundsException();
+                        }
+                        description = input.substring(input.indexOf(" ") + 1);
+                        Task td = new Todo(description);
+                        // this function does all the printing
+                        taskPrint(tasks, td);
+                        break;
 
-                case "deadline":
-                    description = input.substring(input.indexOf(" ") + 1, input.indexOf(" /by "));
-                    String by = input.substring(input.indexOf("/by ") + 4);
-                    Task d = new Deadline(description, by);
-                    taskPrint(tasks, d);
-                    break;
+                    case "deadline":
+                        description = input.substring(input.indexOf(" ") + 1, input.indexOf(" /by "));
+                        String by = input.substring(input.indexOf("/by ") + 4);
+                        Task d = new Deadline(description, by);
+                        taskPrint(tasks, d);
+                        break;
 
-                case "event":
-                    description = input.substring(input.indexOf(" ") + 1, input.indexOf(" /at "));
-                    String at = input.substring(input.indexOf("/at ") + 4);
-                    Task e = new Event(description, at);
-                    taskPrint(tasks, e);
-                    break;
+                        //StringIndexOutOfBoundsException
 
-                default:
-                    // other calls causes duke to add the user-input to tasks
-                    Task t = new Task(input);
-                    tasks.add(t);
-                    System.out.println("added: " + input + "\n");
-                    break;
+                    case "event":
+                        description = input.substring(input.indexOf(" ") + 1, input.indexOf(" /at "));
+                        String at = input.substring(input.indexOf("/at ") + 4);
+                        Task e = new Event(description, at);
+                        taskPrint(tasks, e);
+                        break;
+
+                    case "blah":
+                    case "idk":
+                        throw new UnknownInputException();
+
+
+                    default:
+                        // other calls causes duke to add the user-input to tasks
+                        Task t = new Task(input);
+                        tasks.add(t);
+                        System.out.println("added: " + input + "\n");
+                        break;
+                }
             }
-            input = in.nextLine();
+            catch (UnknownInputException e) {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-( \n");
+            }
 
+            catch (StringIndexOutOfBoundsException e) {
+                System.out.println("OOPS!!! The description of a " + splitInput[0] + " cannot be empty.\n");
+
+            }
+
+            // input after the try, catch and switch case to obtain the next input statement
+            input = in.nextLine();
+            input = input.trim();
 
         }
 
