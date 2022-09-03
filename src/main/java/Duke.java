3742f1b0fe1fc56d75d5d1389208dee3555f035c
CheahHaoYi <haoyicheah@gmail.com>
2022-09-04 03:45:04 +0800

    Refactoring, adjusted spacing on methods and if-else clauses

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,65 +1,69 @@
 import java.util.Scanner;
 public class Duke {
+    private static int NUM_TASK = 100;
+    public static Task[] tasks = new Task[NUM_TASK];
+    public static int taskCount = 0;
 
-    public static Task[] tasks = new Task[100];
-    public static int numTasks = 0;
-
-    public static void listTasks(){
+    public static void listTasks() {
         System.out.println("Beep beep, listing out the tasks....Loading.....");
-        for(int i = 0; i < numTasks; i++){
+        for(int i = 0; i < taskCount; i++){
             System.out.println((i + 1) + "." + tasks[i].toString());
         }
     }
 
-    public static void addTask(String newTask){
-        tasks[numTasks] = new Task(newTask);
+    public static void addTask(String newTask) {
+        tasks[taskCount] = new Task(newTask);
         System.out.println("added: " + newTask);
-        numTasks += 1;
-        System.out.println("Beep boop, now you have " + numTasks + " tasks");
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
 
     }
 
-    public static void addTodo(String newToDo){
-        tasks[numTasks] = new Todo(newToDo);
+    public static void addTodo(String newToDo) {
+        tasks[taskCount] = new Todo(newToDo);
         System.out.println("HELLO BEEP, added a new ToDo: ");
-        System.out.println("\t" + tasks[numTasks].toString());
-        numTasks += 1;
-        System.out.println("Beep boop, now you have " + numTasks + " tasks");
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
     }
 
-    public  static void addDeadline(String param){
+    public static void addDeadline(String param) {
+        final int COMMAND_LEN = 4;
         int deadlineIndex = param.indexOf("/by");
         String description = param.substring(0, deadlineIndex);
-        String deadlineBy = param.substring(deadlineIndex + 4);
-        tasks[numTasks] = new Deadline(description, deadlineBy);
+        String deadlineBy = param.substring(deadlineIndex + COMMAND_LEN);
+        
+        tasks[taskCount] = new Deadline(description, deadlineBy);
         System.out.println("OH NO BEEP BEEP, a new Deadline: " + description);
-        System.out.println("\t" + tasks[numTasks].toString());
-        numTasks += 1;
-        System.out.println("Beep boop, now you have " + numTasks + " tasks");
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
     }
 
-    public  static void addEvent(String param){
+    public static void addEvent(String param) {
+        final int COMMAND_LEN = 4;
         int deadlineIndex = param.indexOf("/at");
         String description = param.substring(0, deadlineIndex);
-        String deadlineBy = param.substring(deadlineIndex + 4);
-        tasks[numTasks] = new Event(description, deadlineBy);
+        String deadlineBy = param.substring(deadlineIndex + COMMAND_LEN);
+
+        tasks[taskCount] = new Event(description, deadlineBy);
         System.out.println("OH NO BEEP BEEP, a new Event: ");
-        System.out.println("\t" + tasks[numTasks].toString());
-        numTasks += 1;
-        System.out.println("Beep boop, now you have " + numTasks + " tasks");
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
     }
 
-    public static void setTask(int whichTask, boolean done){
+    public static void setTask(int whichTask, boolean isDone) {
         final String MESSAGE_DONE = "Nice! I've marked this task as done:";
         final String MESSAGE_NOT_DONE = "OK, I've marked this task as not done yet:";
         final String ERROR_OUT_OF_BOUND = "Sorry, the task does not seem to exist :<";
-        tasks[whichTask].setStatus(done);
+        tasks[whichTask].setStatus(isDone);
 
-        if(whichTask > numTasks){
+        if (whichTask > taskCount) {
             System.out.println(ERROR_OUT_OF_BOUND);
         }
 
-        if(done){
+        if (isDone) {
             System.out.println(MESSAGE_DONE);
             System.out.println("\t" + tasks[whichTask].toString());
         } else {
@@ -72,11 +76,12 @@ public class Duke {
         String[] splitInput = userInput.split(" ", 2);
         String command = splitInput[0];
         String parameters = "false input";
-        if (splitInput.length > 1){
+
+        if (splitInput.length > 1) {
             parameters = splitInput[1];
         }
 
-        switch (command){
+        switch (command) {
         case("list"):
             listTasks();
             break;
@@ -101,7 +106,8 @@ public class Duke {
             break;
         }
     }
-    public static void greetUser(){
+
+    public static void greetUser() {
         /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -111,31 +117,31 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         */
 
-        final String LOGO = "\n" +
-        "     _________________________________________\n" +
-        "   /  _____________________________________   \\ \n" +
-        "   | |                                     |  | \n" +
-        "   | |  C:\\> Initiating programme_         |  | \n" +
-        "   | |                                     |  | \n" +
-        "   | |                                     |  | \n" +
-        "   | |                                     |  | \n" +
-        "   | |                                     |  | \n" +
-        "   | |                                     |  | \n" +
-        "   | |_____________________________________|  | \n" +
-        "    \\_____________________________________/ \n" +
-        "       \\________________________________/ \n" +
-        "        _________________________________ \n" +
-        "   _-'.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--- `-_ \n" +
-        "_-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--..-.-.`-_ \n";
+        final String LOGO = "\n"
+                + "     _________________________________________\n"
+                + "   /  _____________________________________   \\ \n"
+                + "   | |                                     |  | \n"
+                + "   | |  C:\\> Initiating programme. .  .   |  | \n"
+                + "   | |                                     |  | \n"
+                + "   | |  C:\\> Creating Duke...             |  | \n"
+                + "   | |                                     |  | \n"
+                + "   | |                                     |  | \n"
+                + "   | |                                     |  | \n"
+                + "   | |_____________________________________|  | \n"
+                + "    \\_____________________________________/ \n"
+                + "       \\________________________________/ \n"
+                + "        _________________________________ \n"
+                + "   _-'.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--- `-_ \n"
+                + "_-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--..-.-.`-_ \n";
 
         System.out.println(LOGO);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
 
-    public static boolean toExit(String userInput) {
-        if (userInput.equals("bye")){
-            System.out.println("BEEP BEEP >>>> TERMINATING >>>> PROCESS >>>>>>");
+    public static boolean isToExit(String userInput) {
+        if (userInput.equals("bye")) {
+            System.out.println("BEEP BEEP >>>> SEE >>> YOU >>>> AGAIN >>> BEEP BEWWWWW >>>");
             return true;
         }
         return false;
@@ -146,7 +152,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
 
-        while(!toExit(userInput)){
+        while (!isToExit(userInput)) {
             processUserInput(userInput);
             userInput = sc.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,36 +4,39 @@ public class Task {
 
     private final String STATUS_DONE_ICON = "X";
     private final String STATUS_NOTDONE_ICON = " ";
-    public Task(){
+
+    public Task() {
         this("");
     }
 
-    public Task(String description){
+    public Task(String description) {
         this.taskDescription = description;
         this.isDone = false;
     }
 
-    public String getTask(){
+    public String getTask() {
         return this.taskDescription;
     }
 
-    public void setStatus(boolean isDone){
+    public void setStatus(boolean isDone) {
         this.isDone = isDone;
     }
 
-    public boolean getStatus(){
+    public boolean getStatus() {
         return this.isDone;
     }
 
-    public String getStatusIcon(){
-        if(this.isDone){
+    public String getStatusIcon() {
+
+        if (this.isDone) {
             return STATUS_DONE_ICON;
         } else {
             return STATUS_NOTDONE_ICON;
         }
+        
     }
 
-    public String toString(){
+    public String toString() {
         return "[" + getStatusIcon() + "] " + taskDescription;
     }
 }
