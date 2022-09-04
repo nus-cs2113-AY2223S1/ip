8172c8e0f60218b15a918ecd808120def8c5ba55
Jorelle <jorellesee2000@gmail.com>
2022-09-04 17:18:01 +0800

    Level-4 and A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class Duke {
+    public static int numberOfTasks = 0;
+    public static Task[] myList = new Task[1000];
+
     public static void main(String[] args) {
 
         String GREETING = "____________________________________________________________\n" +
@@ -8,93 +11,144 @@ public class Duke {
                 "\\   \\/\\/   // __ \\ /    \\  / ___\\ \n" +
                 " \\        /\\  ___/|   |  \\/ /_/  >\n" +
                 "  \\__/\\  /  \\___  >___|  /\\___  / \n" +
-                "       \\/       \\/     \\//_____/  "+
+                "       \\/       \\/     \\//_____/  " +
                 "\n Hello! I'm Weng!\n" +
                 " What can I do for ya?\n" +
                 "____________________________________________________________\n";
         System.out.println("\n" + GREETING);
-//        String EXIT_MESSAGE = "____________________________________________________________\n" +
-//                " Bye. Hope to cya again soon!\n" +
-//                "____________________________________________________________";
-//        Scanner sc = new Scanner(System.in);
-//        String input = sc.nextLine();
-//
-//        while(!input.equals("bye")){
-//            String output = "____________________________________________________________\n " +
-//                    input +
-//                    "\n____________________________________________________________\n";
-//            System.out.println(output);
-//            input = sc.nextLine();
-//        }
-//        Level-1
-//        String[] myList;
-//        myList = new String[100];
-//        Scanner sc = new Scanner(System.in);
-//        String input = sc.nextLine();
-//        int numberOfInputs = 0;
-//        while(!input.equals("list")){
-//            myList[numberOfInputs] = input;
-//            numberOfInputs++;
-//            String output = "____________________________________________________________\n " +
-//                    "Added: " +
-//                    input +
-//                    "\n____________________________________________________________\n";
-//            System.out.println(output);
-//            input = sc.nextLine();
-//        }
-//        for(int i = 0; i < numberOfInputs; i++){
-//            System.out.print(i + 1);
-//            System.out.println(". " + myList[i]);
-//        }
-//        Level-2
 
 
-        Task[] myList;
-        myList = new Task[1000];
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int numberOfInputs = 0;
-        while(!input.equals("bye")) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
             String words[] = input.split(" ");
-            if (input.equals("list")) {
-                for (int i = 0; i < numberOfInputs; i++) {
-                    System.out.print(i + 1);
-                    System.out.print(".");
-                    System.out.print(myList[i].getStatusIcon());
-                    System.out.println(myList[i].description);
-                }
+            if (input.startsWith("bye")) {
+                sayBye();
+                break;
+            } else if (input.startsWith("list")) {
+                printList();
+            } else if (input.startsWith("mark")) {
+                markTask(words);
+            } else if (input.startsWith("unmark")) {
+                unmarkTask(words);
+            } else if (input.startsWith("todo")) {
+                addTodo(input);
+            } else if (input.startsWith("event")) {
+                addEvent(input);
+            } else if (input.startsWith("deadline")) {
+                addDeadline(input);
             }
-            else if (words[0].equals("mark")) {//pending check if second character is number AND smaller than numberOfInputs
-                int index = Integer.parseInt(words[1]);
-                myList[index-1].markAsDone();
-                System.out.println("____________________________________________________________\n" +
-                        "     Nice! I've marked this task as done:\n" +
-                        myList[index-1].getStatusIcon() + " " + myList[index-1].description +
-                        "    ____________________________________________________________");
-            }
-            else if (words[0].equals("unmark")) {//pending check if second character is number AND smaller than numberOfInputs
-                int index = Integer.parseInt(words[1]);
-                myList[index-1].markAsNotDone();
-                System.out.println("____________________________________________________________\n" +
-                        "     Alright! I've marked this task as not done yet:\n" +
-                        myList[index-1].getStatusIcon() + " " + myList[index-1].description +
-                        "    ____________________________________________________________");
-            } else {
-                myList[numberOfInputs] = new Task(input);
-                numberOfInputs++;
-                String output = "____________________________________________________________\n " +
-                    "Added: " +
-                    input +
-                    "\n____________________________________________________________\n";
-            System.out.println(output);
-            }
-            input = sc.nextLine();
         }
+    }
+
+    //        while(!input.equals("bye")) {
+//            String words[] = input.split(" ");
+//            if (input.equals("list")) {
+//                for (int i = 0; i < numberOfTasks; i++) {
+//                    System.out.print(i + 1);
+//                    System.out.print(".");
+//                    System.out.println(myList[i].toString());
+//                }
+//            }
+//            else if (words[0].equals("mark")) {//pending check if second character is number AND smaller than numberOfInputs
+//                int index = Integer.parseInt(words[1]);
+//                myList[index-1].markAsDone();
+//                System.out.println("____________________________________________________________\n" +
+//                        "Nice! I've marked this task as done:\n" +
+//                        myList[index-1].getStatusIcon() + " " + myList[index-1].description +
+//                        "\n____________________________________________________________");
+//            }
+//            else if (words[0].equals("unmark")) {//pending check if second character is number AND smaller than numberOfInputs
+//                int index = Integer.parseInt(words[1]);
+//                myList[index-1].markAsNotDone();
+//                System.out.println("____________________________________________________________\n" +
+//                        "     Alright! I've marked this task as not done yet:\n" +
+//                        myList[index-1].getStatusIcon() + " " + myList[index-1].description +
+//                        "    ____________________________________________________________");
+//            }
+//            else if (words[0].equals("deadline")){
+//                String description = input.substring(input.indexOf(" ")+1, input.indexOf("/by")-1);
+//                String by = input.substring(input.indexOf("/by")+4);
+//                myList[numberOfTasks] = new Deadline(description, by);
+//                numberOfTasks++;
+//                System.out.println("deadline added!");
+//            }
+//            else if (words[0].equals("event")){
+//                add
+//                String description = input.substring(input.indexOf(" ")+1, input.indexOf("/at")-1);
+//                String at = input.substring(input.indexOf("/at")+4);
+//                myList[numberOfTasks] = new Event(description, at);
+//                numberOfTasks++;
+//                System.out.println("event added!");
+//            }
+//            else if (words[0].equals("todo")){
+//                String description = input.substring(input.indexOf(" ")+1);
+//                myList[numberOfTasks] = new Todo(description);
+//                numberOfTasks++;
+//                System.out.println("todo added!");
+//            }
+//            input = sc.nextLine();
+//        }
+    private static void sayBye() {
         String EXIT_MESSAGE = "____________________________________________________________\n" +
                 " Bye. Hope to cya again soon!\n" +
                 "____________________________________________________________";
         System.out.println(EXIT_MESSAGE);
+    }
 
+    private static void printList() {
+        for (int i = 0; i < numberOfTasks; i++) {
+            System.out.print(i + 1);
+            System.out.print(".");
+            System.out.println(myList[i].toString());
+        }
+    }
 
+    private static void markTask(String[] words) {
+        int index = Integer.parseInt(words[1]);
+        myList[index - 1].markAsDone();
+        System.out.println("____________________________________________________________\n" +
+                "Nice! I've marked this task as done:\n" +
+                myList[index - 1].getStatusIcon() + " " + myList[index - 1].description +
+                "\n____________________________________________________________");
+    }
+
+    private static void unmarkTask(String[] words) {
+        int index = Integer.parseInt(words[1]);
+        myList[index - 1].markAsNotDone();
+        System.out.println("____________________________________________________________\n" +
+                "     Ok, I've marked this task as not done yet:\n" +
+                myList[index - 1].getStatusIcon() + " " + myList[index - 1].description +
+                "    ____________________________________________________________");
+    }
+
+    private static void addDeadline(String input) {
+        String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/by") - 1);
+        String by = input.substring(input.indexOf("/by") + 4);
+        myList[numberOfTasks] = new Deadline(description, by);
+        numberOfTasks++;
+        printCurrentTaskMessage();
+    }
+
+    private static void addEvent(String input) {
+        String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/at") - 1);
+        String at = input.substring(input.indexOf("/at") + 4);
+        myList[numberOfTasks] = new Event(description, at);
+        numberOfTasks++;
+        printCurrentTaskMessage();
+    }
+
+    private static void addTodo(String input) {
+        String description = input.substring(input.indexOf(" ") + 1);
+        myList[numberOfTasks] = new Todo(description);
+        numberOfTasks++;
+        printCurrentTaskMessage();
+    }
+
+    private static void printCurrentTaskMessage(){
+        System.out.println("Got it. I've added this task:");
+        System.out.println(myList[numberOfTasks-1].toString());
+        System.out.println("Now you have "+numberOfTasks+" tasks in the list");
     }
 }
+
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,11 +2,10 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description){
         this.description = description;
         this.isDone = false;
     }
-
     public String getStatusIcon() {
         return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
@@ -18,4 +17,14 @@ public class Task {
         isDone = false;
     }
 
+    public String toString(){
+        return this.getStatusIcon() + " " + description;
+    }
+
+
 }
+
+
+
+
+
