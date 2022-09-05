743ca05647669941779ee7bbbe4837739db44418
eehongchan <eehongchan@gmail.com>
2022-09-06 02:32:08 +0800

    Add classes to package duke

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,19 +1,19 @@
-public class Event extends Task{
+package duke;
+public class Deadline extends Task{
 
     protected String date;
 
-    public Event(String description, String date) {
+    public Deadline(String description, String date) {
         super(description);
         this.date = date;
     }
 
     @Override
     public String print() {
-        return ("[E]" + super.print() + " (at: " + this.date + ")");
+        return ("[D]" + super.print() + " (by: " + this.date + ")");
     }
 
     public String getDate() {
         return this.date;
     }
-
 }
