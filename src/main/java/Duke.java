32e438ce5a0d4e4a2ebc5f303ac17c24d79ffbee
KYRENE\cwxky <cwx.kyrene@gmail.com>
2022-09-06 11:20:02 +0800

    Edited the if-else block to a switch block for better readibility. Added constants for better readability.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
+    private static final String COMMAND_BYE = "bye";
+    private static final String MESSAGE_BYE = "Duke: Bye. Hope to see you again soon!";
+    private static final String COMMAND_LIST = "list";
+    private static final String COMMAND_MARK = "mark";
+    private static final String MESSAGE_MARK = "The task has already been completed! Try doing another task now :)";
+    private static final String COMMAND_UNMARK = "unmark";
+    private static final String MESSAGE_UNMARK = "The task hasn't been completed. Do it soon :(";
+    private static final String COMMAND_TODO = "todo";
+    private static final String COMMAND_DEADLINE = "deadline";
+    private static final String COMMAND_EVENT = "event";
+
     public static void main(String[] args) {
         String line;
         ArrayList<Task> tasksList = new ArrayList<Task>();
@@ -10,59 +21,67 @@ public class Duke {
 
         while (true) {
             line = in.nextLine();
-            if (line.equals("bye")) {
-                System.out.println("Duke: Bye. Hope to see you again soon!");
+            String[] words = line.split(" ");
+            switch (words[0]) {
+            case COMMAND_BYE:
+                System.out.println(MESSAGE_BYE);
                 break;
-            } else if (line.equals("list")) {
+            case COMMAND_LIST:
                 System.out.println("Here are the tasks for today: ");
                 for (int i = 0; i < tasksList.size(); i++) {
                     System.out.println(tasksList.get(i));
                 }
-            } else if (line.startsWith("mark")) {
+                break;
+            case COMMAND_MARK:
                 int itemNumber = Integer.parseInt(line.split(" ")[1]) - 1;
                 Task toBeChanged = tasksList.get(itemNumber);
                 if (toBeChanged.isDone) {
-                    System.out.println("The task has already been completed! Try doing another task now :)");
+                    System.out.println(MESSAGE_MARK);
                 } else {
                     toBeChanged.markAsDone(toBeChanged);
                 }
-            } else if (line.startsWith("unmark")) {
-                int itemNumber = Integer.parseInt(line.split(" ")[1]) - 1;
-                Task toBeChanged = tasksList.get(itemNumber);
+                break;
+            case COMMAND_UNMARK:
+                itemNumber = Integer.parseInt(line.split(" ")[1]) - 1;
+                toBeChanged = tasksList.get(itemNumber);
                 if (toBeChanged.isDone) {
                     toBeChanged.markAsUndone(toBeChanged);
                 } else {
-                    System.out.println("The task hasn't been completed. Do it soon :(");
+                    System.out.println(MESSAGE_UNMARK);
                 }
-            } else if (line.startsWith("todo")) {
+                break;
+            case COMMAND_TODO:
                 String description = line.substring(line.indexOf("todo") + 5);
-                Todo t = new Todo(description);
-                tasksList.add(t);
+                Todo td = new Todo(description);
+                tasksList.add(td);
                 System.out.println("Got it. I've added this task: " + System.lineSeparator() +
-                        t + System.lineSeparator() + "Now you have " + tasksList.size() + " tasks in the list");
-            } else if (line.startsWith("deadline")) {
-                String description = line.substring(line.indexOf("deadline") + 9, line.indexOf("/"));
+                        td + System.lineSeparator() + "Now you have " + tasksList.size() + " tasks in the list");
+                break;
+            case COMMAND_DEADLINE:
+                description = line.substring(line.indexOf("deadline") + 9, line.indexOf("/"));
                 String by = line.substring(line.indexOf("by") + 3);
                 Deadline d = new Deadline(description, by);
                 tasksList.add(d);
                 System.out.println("Got it. I've added this task: " + System.lineSeparator() +
                         d + System.lineSeparator() + "Now you have " + tasksList.size() + " tasks in the list");
-            } else if (line.startsWith("event")) {
-                String description = line.substring(line.indexOf("event") + 6, line.indexOf("/"));
+                break;
+            case COMMAND_EVENT:
+                description = line.substring(line.indexOf("event") + 6, line.indexOf("/"));
                 String time = line.substring(line.indexOf("at") + 3);
                 Event e = new Event(description, time);
                 tasksList.add(e);
                 System.out.println("Got it. I've added this task: " + System.lineSeparator() +
-                        e + System.lineSeparator() + "Now you have " + tasksList.size() + "tasks in the list.");
-            } else {
+                        e + System.lineSeparator() + "Now you have " + tasksList.size() + " tasks in the list.");
+                break;
+            default:
                 System.out.println("added: " + line);
                 Task t = new Task();
                 t.description = line;
                 t.number = count + 1;
                 t.isDone = false;
                 tasksList.add(t);
+                break;
             }
         }
-
     }
 }
