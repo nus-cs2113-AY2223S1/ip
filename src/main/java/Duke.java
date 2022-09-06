3892000fba71ab076d05a18dc310f5312418cac0
Abhitya Krisnaraj <abhitya@gmail.com>
2022-09-06 21:42:28 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ package main.java;
 
 import java.util.*;
 public class Duke {
+    public static ArrayList<Task> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -14,7 +15,6 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you");
         String inp = in.nextLine();
-        ArrayList<Task> list = new ArrayList<>();
         while(!inp.equals("bye")){
             if(inp.equals("list")) {
                 for (int i = 1; i <= list.size(); i++) {
@@ -42,36 +42,72 @@ public class Duke {
 
             }
             else {
-                String comm = inp.substring(0, inp.indexOf(" ")).toLowerCase();
-                String tas = inp.substring(inp.indexOf(" ")+1);
-
-                if(comm.equals("deadline")) {
-                    String name = tas.substring(0, tas.indexOf("/")-1);
-                    String time = tas.substring(tas.indexOf("/"));
-                    String date = time.substring(time.indexOf(" ")+1);
-                    list.add(new Deadline(name, false, date));
+                try{
+                    taskReader(inp);
                 }
-                else if(comm.equals("event")){
-                    String name = tas.substring(0, tas.indexOf("/")-1);
-                    String time = tas.substring(tas.indexOf("/"));
-                    String date = time.substring(time.indexOf(" ")+1);
-                    list.add(new Event(name, false, date));
+                catch (DukeException e){
+                    e.printError();
                 }
-                else if(comm.equals("todo")) {
-                    list.add(new Todo(tas, false));
-                }
-                else{
-                    list.add(new Task(inp, false));
-
-                }
-                System.out.println("Got it. I have added this task: ");
-                System.out.println(list.get(list.size()-1));
-                System.out.println("Now you have "+list.size()+" tasks in your list.");
             }
             inp = in.nextLine();
         }
         System.out.println();
 
         System.out.println("Bye");
+    }
+    public static void taskReader(String inp) throws DukeException{
+        String comm, tas, name, date, time;
+        try{
+            comm = inp.substring(0, inp.indexOf(" ")).toLowerCase();
+        }
+        catch (StringIndexOutOfBoundsException e){
+            throw new DukeException(inp);
+        }
+        try{
+            tas = inp.substring(inp.indexOf(" ")+1);
+        }
+        catch (IndexOutOfBoundsException e){
+            throw new DukeException(comm);
+        }
+        //System.out.println(inp+"\n"+comm+"\n"+tas);
+        if(comm.equals("deadline")) {
+            try {
+                name = tas.substring(0, tas.indexOf("/"));
+                time = tas.substring(tas.indexOf("/"));
+            }
+            catch (StringIndexOutOfBoundsException e){
+                throw new DukeException(comm, tas);
+            }
+            if(time.equals("/")){
+                throw new DukeException(comm, name);
+            }
+            date = time.substring(time.indexOf(" ")+1);
+            list.add(new Deadline(name, false, date));
+            printLastTask();
+        }
+        else if(comm.equals("event")){
+            try {
+                name = tas.substring(0, tas.indexOf("/"));
+                time = tas.substring(tas.indexOf("/"));
+            }
+            catch (StringIndexOutOfBoundsException e){
+                throw new DukeException(comm, tas);
+            }
+            if(time.equals("/")) {
+                throw new DukeException(comm, name);
+            }
+            date = time.substring(time.indexOf(" ")+1);
+            list.add(new Event(name, false, date));
+            printLastTask();
+        }
+        else if(comm.equals("todo")) {
+            list.add(new Todo(tas, false));
+            printLastTask();
+        }
+    }
+    public static void printLastTask(){
+        System.out.println("Got it. I have added this task: ");
+        System.out.println(list.get(list.size()-1));
+        System.out.println("Now you have "+list.size()+" tasks in your list.");
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class DukeException extends Exception{
+    private String type;
+    private String name;
+    public DukeException(String type){
+        this.type = type;
+        name = "";
+    }
+    public DukeException(String type, String name){
+        this.type = type;
+        this.name = name;
+    }
+    public void printError() {
+        if (name.equals("")){
+            if (type.equals("todo")||type.equals("deadline")||type.equals("event")) {
+                System.out.println(type+" command requires a description to be added to the list");
+            }
+            else{
+                System.out.println("I'm sorry I don't know what that is, please enter another command");
+            }
+        }
+        else{
+            System.out.println("Please add a date range for this event: "+name);
+        }
+    }
+}
