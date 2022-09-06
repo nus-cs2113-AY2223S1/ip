026a9cb9d1cc3a1e1c7ec46d66828aae219b551c
Amethyst <e0978391@u.nus.edu>
2022-09-07 03:33:21 +0800

    another try to fix the conflict

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,5 +1,48 @@
 public class DukeException extends Exception{
-    public commandTypeError(String str){
-        super(str);
+    private String errorType;
+    public DukeException(String str){
+        super();
+        errorType = str;
+    }
+
+    void handleError(){
+        switch(errorType){
+        case "TaskTypeError":
+            printTaskTypeErrorMessage();
+            break;
+        case "TodoDescriptionError":
+            printTodoDescriptionErrorMessage();
+            break;
+        case "EventDescriptionError":
+            printEventDescriptionErrorMessage();
+            break;
+        case "DeadlineDescriptionError":
+            printDeadlineDescriptionErrorMessage();
+            break;
+        }
+    }
+
+    void printTaskTypeErrorMessage(){
+        Util.printSplitLine();
+        System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        Util.printSplitLine();
+    }
+
+    void printTodoDescriptionErrorMessage(){
+        Util.printSplitLine();
+        System.out.println("     ☹ OOPS!!! The description of a todo cannot be empty.");
+        Util.printSplitLine();
+    }
+
+    void printEventDescriptionErrorMessage(){
+        Util.printSplitLine();
+        System.out.println("     ☹ OOPS!!! The description of a event is wrong.");
+        Util.printSplitLine();
+    }
+
+    void printDeadlineDescriptionErrorMessage(){
+        Util.printSplitLine();
+        System.out.println("     ☹ OOPS!!! The description of a deadline is wrong.");
+        Util.printSplitLine();
     }
 }
