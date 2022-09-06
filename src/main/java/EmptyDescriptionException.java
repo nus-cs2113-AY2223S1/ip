5026b82d26cb16da1cd1154f9502b27b80990b0f
gaoyunfan <gaoyunfan8@gmail.com>
2022-09-06 21:25:31 +0800

    Add handle empty description and invalid command errors

---
+++ src/main/java/EmptyDescriptionException.java
@@ -0,0 +1,9 @@
+public class EmptyDescriptionException extends DukeException{
+        private String task;
+        public EmptyDescriptionException(String task) {
+            this.task = task;
+        }
+        String getExceptionDescription() {
+            return "OOPS!!! The description of a " + task + " cannot be empty";
+        }
+}
---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends DukeException{
+     String getExceptionDescription() {
+        return "OOPS!!! I'm sorry, but I don't know what that means :-(";
+    }
+}
