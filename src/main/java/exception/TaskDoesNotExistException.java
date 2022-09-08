e0b44a99c86803536077595482c32c2cb6c69182
jeyvia <e0725215@u.nus.edu>
2022-09-09 01:21:40 +0800

    9 sep commit
    Not complete with exceptions

---
+++ src/main/java/exception/TaskDoesNotExistException.java
@@ -0,0 +1,4 @@
+package exception;
+
+public class TaskDoesNotExistException extends Exception{
+}
--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,16 +1,15 @@
-public class Event extends Task{
+package task;
 
+public class Deadline extends Task {
     protected String by;
-
-    public Event(String description, String by) {
+    public Deadline(String description, String by) {
         super(description);
         this.by = by;
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + toString());
     }
-
     @Override
     public String toString() {
-        return ("[E][" + super.getStatusIcon() + "] " + super.description + " (at: " + by + ")");
+        return ("[D][" + super.getStatusIcon() + "] " + super.description + " (by: " + by + ")");
     }
 }
