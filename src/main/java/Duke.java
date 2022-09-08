1ab27180611adb46fd5f4770d6614c9e84176385
sylviahe171 <sylviahe171@gmail.com>
2022-09-09 01:32:35 +0800

    error handle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,40 +19,15 @@ public class Duke {
 
         while(!val.equals("bye")){
             System.out.println(SEPARATOR);
-            //Solution below adapted from https://github.com/kohnh/ip/blob/master/src/main/java/TaskManager.java
-            if(val.contains("list")){
+            if(val.equals("list")){
                 printList(tasks, length);
-                System.out.println(SEPARATOR);
-                val = input.nextLine();
-                continue;
-            }
-
-
-            String firstWord = val.substring(0, val.indexOf(' '));
-            switch(firstWord){
-            case "mark":
-                markTask(val, tasks, true, "Nice! I've marked this task as done:");
-                break;
-            case "unmark":
+            } else if (val.contains("unmark")) {
                 markTask(val, tasks, false, "OK, I've marked this task as not done yet:");
-                break;
-            case TODO:
-                tasks[length] = new Todo(val.substring(TODO.length()));
-                length++;
-                System.out.println("Now you have " + length + " tasks in the list.");
-                break;
-            case DEADLINE:
-                tasks[length] = new Deadline(val.substring(DEADLINE.length(), val.indexOf("/")), val.substring((val.indexOf("/") + 4)));
-                length++;
-                System.out.println("Now you have " + length + " tasks in the list.");
-                break;
-            case EVENT:
-                tasks[length] = new Event(val.substring(EVENT.length(), val.indexOf("/")), val.substring((val.indexOf("/") + 4)));
-                length++;
-                System.out.println("Now you have " + length + " tasks in the list.");
-                break;
+            } else if (val.contains("mark")){
+                markTask(val, tasks, true, "Nice! I've marked this task as done:");
+            } else{
+                length += addTask(val, tasks, length);
             }
-
             System.out.println(SEPARATOR);
             val = input.nextLine();
         }
@@ -60,8 +35,48 @@ public class Duke {
         byeMsg();
     }
 
+    private static int addTask(String val, Task[] tasks, int length) {
+        if(val.contains(TODO)){
+            if(val.substring(TODO.length()).trim().isEmpty()){
+                emptyError(TODO);
+                return 0;
+            }
+
+            tasks[length] = new Todo(val.substring(TODO.length()));
+
+        } else if (val.contains(DEADLINE)) {
+            if(val.substring(DEADLINE.length()).trim().isEmpty()){
+                emptyError(DEADLINE);
+                return 0;
+            }
+            tasks[length] = new Deadline(val.substring(DEADLINE.length(), val.indexOf("/")), val.substring((val.indexOf("/") + 4)));
+
+        } else if (val.contains(EVENT)) {
+            if(val.substring(EVENT.length()).trim().isEmpty()){
+                emptyError(EVENT);
+                return 0;
+            }
+            tasks[length] = new Event(val.substring(EVENT.length(), val.indexOf("/")), val.substring((val.indexOf("/") + 4)));
+
+        }else{
+            System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            return 0;
+        }
+        length++;
+        System.out.println("Now you have " + length + " tasks in the list.");
+        return 1;
+    }
+
+    private static void emptyError(String taskName){
+        System.out.println( "☹ OOPS!!! The description of a " + taskName + " cannot be empty.");
+    }
+
     private static void markTask(String val, Task[] tasks, boolean status, String x) {
         int id = val.indexOf(" ");
+        if(id < 0){
+            System.out.println("☹ OOPS!!! The index of the task cannot be empty.");
+            return;
+        }
         String ind = val.substring(id + 1);
         int index = Integer.parseInt(ind) - 1;
         tasks[index].setDone(status);
