e0363c8983621e1d61599474c1f3bd7bfe5481f2
CheahHaoYi <haoyicheah@gmail.com>
2022-09-09 02:29:45 +0800

    Revert "Add InputParser, TaskManager Class, Exceptions and functionality to handle errors"
    This reverts commit 1f2180e243ee970895c9e1649fe09534004c6501.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,121 @@
 import java.util.Scanner;
 public class Duke {
-    public static Scanner sc;
-    public static TaskManager taskManager = new TaskManager();
-    public static InputParser parser = new InputParser();
+    private static int NUM_TASK = 100;
+    public static Task[] tasks = new Task[NUM_TASK];
+    public static int taskCount = 0;
 
-    public static void executeUserInput() throws UnknownCommandException {
-        String command = parser.getCommand();
-        String[] parameters = parser.getTaskParameters();
-
-        switch (command) {
-            case ("list"):
-                taskManager.listAllTask();
-                break;
-            case ("mark"):
-                taskManager.setTask(Integer.parseInt(parameters[0]) - 1, true);
-                break;
-            case ("unmark"):
-                taskManager.setTask(Integer.parseInt(parameters[0]) - 1, false);
-                break;
-            case ("todo"):
-                taskManager.addTodo(parameters[0]);
-                break;
-            case ("deadline"):
-                taskManager.addDeadline(parameters[0], parameters[1]);
-                break;
-            case ("event"):
-                taskManager.addEvent(parameters[0], parameters[1]);
-                break;
-            default:
-                throw new UnknownCommandException();
+    public static void listTasks() {
+        System.out.println("Beep beep, listing out the tasks....Loading.....");
+        for(int i = 0; i < taskCount; i++){
+            System.out.println((i + 1) + "." + tasks[i].toString());
         }
+    }
+
+    public static void addTask(String newTask) {
+        tasks[taskCount] = new Task(newTask);
+        System.out.println("added: " + newTask);
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
 
     }
 
+    public static void addTodo(String newToDo) {
+        tasks[taskCount] = new Todo(newToDo);
+        System.out.println("HELLO BEEP, added a new ToDo: ");
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
+    }
+
+    public static void addDeadline(String param) {
+        final int COMMAND_LEN = 4;
+        int deadlineIndex = param.indexOf("/by");
+        String description = param.substring(0, deadlineIndex);
+        String deadlineBy = param.substring(deadlineIndex + COMMAND_LEN);
+        
+        tasks[taskCount] = new Deadline(description, deadlineBy);
+        System.out.println("OH NO BEEP BEEP, a new Deadline: " + description);
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
+    }
+
+    public static void addEvent(String param) {
+        final int COMMAND_LEN = 4;
+        int deadlineIndex = param.indexOf("/at");
+        String description = param.substring(0, deadlineIndex);
+        String deadlineBy = param.substring(deadlineIndex + COMMAND_LEN);
+
+        tasks[taskCount] = new Event(description, deadlineBy);
+        System.out.println("OH NO BEEP BEEP, a new Event: ");
+        System.out.println("\t" + tasks[taskCount].toString());
+        taskCount += 1;
+        System.out.println("Beep boop, now you have " + taskCount + " tasks");
+    }
+
+    public static void setTask(int whichTask, boolean isDone) {
+        final String MESSAGE_DONE = "Nice! I've marked this task as done:";
+        final String MESSAGE_NOT_DONE = "OK, I've marked this task as not done yet:";
+        final String ERROR_OUT_OF_BOUND = "Sorry, the task does not seem to exist :<";
+        tasks[whichTask].setStatus(isDone);
+
+        if (whichTask > taskCount) {
+            System.out.println(ERROR_OUT_OF_BOUND);
+        }
+
+        if (isDone) {
+            System.out.println(MESSAGE_DONE);
+            System.out.println("\t" + tasks[whichTask].toString());
+        } else {
+            System.out.println(MESSAGE_NOT_DONE);
+            System.out.println("\t" + tasks[whichTask].toString());
+        }
+    }
+
+    public static void processUserInput(String userInput){
+        String[] splitInput = userInput.split(" ", 2);
+        String command = splitInput[0];
+        String parameters = "false input";
+
+        if (splitInput.length > 1) {
+            parameters = splitInput[1];
+        }
+
+        switch (command) {
+        case("list"):
+            listTasks();
+            break;
+        case("mark"):
+            //assume input of length 2
+            setTask(Integer.parseInt(splitInput[1]) - 1,true);
+            break;
+        case("unmark"):
+            setTask(Integer.parseInt(splitInput[1]) - 1, false);
+            break;
+        case("todo"):
+            addTodo(parameters);
+            break;
+        case("deadline"):
+            addDeadline(parameters);
+            break;
+        case("event"):
+            addEvent(parameters);
+            break;
+        default:
+            addTask(userInput);
+            break;
+        }
+    }
+
     public static void greetUser() {
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        */
 
         final String LOGO = "\n"
                 + "     _________________________________________\n"
@@ -67,21 +149,12 @@ public class Duke {
 
     public static void main(String[] args) {
         greetUser();
-        sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
 
         while (!isToExit(userInput)) {
-            try {
-                parser.parseUserInput(userInput);
-                executeUserInput();
-            } catch (UnknownCommandException e) {
-                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            } catch (DukeException e) {
-                System.out.println("☹ OOPS!!! The description cannot be empty.");
-            }
+            processUserInput(userInput);
             userInput = sc.nextLine();
         }
-
-        sc.close();
     }
 }
