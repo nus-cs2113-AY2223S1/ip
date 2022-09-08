70de262bf0d0f970b5c4ebb1b9646709a0f9a749
jeyvia <e0725215@u.nus.edu>
2022-09-09 01:27:47 +0800

    fixes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import task.TaskList;
 import java.util.Scanner;
 
 public class Duke {
---
+++ src/main/java/task/Task.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) { //creating new object
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        if (isDone) {
+            return "X";
+        }
+        return " ";
+    }
+    public String toString () {
+        return toString();
+    }
+}
