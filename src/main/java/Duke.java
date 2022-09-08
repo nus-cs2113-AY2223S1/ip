8f5f70740df9d50da16a87b4b41a1d324d79bedd
YugeCheng <1351425316@qq.com>
2022-09-08 18:01:32 +0800

    Add class DukeException

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,8 +11,8 @@ public class Duke {
     }
 
     private static void showLogo() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+        String logo = " ____        _\n"
+                + "|  _ \\ _   _| | _____\n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
