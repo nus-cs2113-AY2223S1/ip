8a5fc5719f58b52ebd6d192dd1dce77ceaaba2d9
shengiv <66622728+shengiv@users.noreply.github.com>
2022-09-09 14:30:54 +0800

    Add exceptions to handle errors
    Duke accepts null descriptions.
    
    We do not want to accept null descriptions for the tasks since each task should at least have a task name.
    
    Creating a new exception class and throwing the exception can prevent null entries in the tasks list.
    
    Let's create a new exception NoDescriptionException to handle such exceptions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<Task>();
     private static final String LINE_BREAK = "______________________________________________\n";
 
-    public static void printList() { //prints list of tasks
+    public static void printList() {
         System.out.println(LINE_BREAK);
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println( Integer.toString(i + 1) + "." + tasks.get(i).toString());
@@ -14,32 +14,41 @@ public class Duke {
         System.out.println(LINE_BREAK);
     }
 
-    public static void printMarkedTask(Task markedTask) { //prints that the task is marked
+    public static void printMarkedTask(Task markedTask) {
         System.out.println(LINE_BREAK + "Nice! I've marked this task as done: ");
         System.out.println("  " + markedTask.toString());
         System.out.println();
     }
 
-    public static void printUnmarkedTask(Task unmarkedTask) { //prints that the task is not done yet
+    public static void printUnmarkedTask(Task unmarkedTask) {
         System.out.println(LINE_BREAK + "OK, I've marked this task as not done yet: ");
         System.out.println("  " + unmarkedTask.toString());
         System.out.println();
     }
 
-    public static void printAddedTask(Task task) { //prints that the task has been added to list
+    public static void printAddedTask(Task task) {
         System.out.println(LINE_BREAK + "Got it. I've added this task: ");
         System.out.println(" " + task.toString());
         System.out.println("Now you have " + Integer.toString(tasks.size()) + " tasks in the list \n" + LINE_BREAK);
     }
 
-    public static ToDo getToDo(String[] inputWords) { //returns a todo object from the user input sentence
+    public static ToDo getToDo(String[] inputWords) throws NoDescriptionException {
+        if (inputWords.length == 1) {
+            throw new NoDescriptionException();
+        }
         String[] taskNameWords = Arrays.copyOfRange(inputWords, 1, inputWords.length);
         String taskName = String.join(" ", taskNameWords);
         ToDo toDo = new ToDo(taskName);
         return toDo;
     }
 
-    public static Deadline getDeadline(String[] inputWords) { //returns a Deadline object from the user input sentence
+    public static Deadline getDeadline(String[] inputWords) throws NoDescriptionException, InvalidTaskFormatException {
+        if (inputWords.length == 1) {
+            throw new NoDescriptionException();
+        }
+        if (!Arrays.asList(inputWords).contains("/by")) {
+            throw new InvalidTaskFormatException();
+        }
         int indexOfBy = Arrays.asList(inputWords).indexOf("/by");
         String deadlineName = String.join(" ", Arrays.copyOfRange(inputWords, 1, indexOfBy));
         String dueDate = String.join(" ", Arrays.copyOfRange(inputWords, indexOfBy + 1, inputWords.length));
@@ -47,7 +56,13 @@ public class Duke {
         return deadline;
     }
 
-    public static Event getEvent(String[] inputWords) { //returns an event object from the user input sentence
+    public static Event getEvent(String[] inputWords) throws NoDescriptionException, InvalidTaskFormatException {
+        if (inputWords.length == 1) {
+            throw new NoDescriptionException();
+        }
+        if (!Arrays.asList(inputWords).contains("/at")) {
+            throw new InvalidTaskFormatException();
+        }
         int indexOfAt = Arrays.asList(inputWords).indexOf("/at");
         String eventName = String.join(" ", Arrays.copyOfRange(inputWords, 1, indexOfAt));
         String timeOfEvent = String.join(" ", Arrays.copyOfRange(inputWords, indexOfAt + 1, inputWords.length));
@@ -56,59 +71,65 @@ public class Duke {
     }
 
     public static void printInvalidDeadline() {
-        System.out.println('\n' + "Invalid Deadline. Please provide due date!" + '\n');
+        System.out.println(LINE_BREAK + "Invalid Deadline. Please provide due date!\n" + LINE_BREAK);
     }
 
     public static void printInvalidEvent() {
-        System.out.println('\n' + "Invalid Event. Please provide event time!" + '\n');
+        System.out.println(LINE_BREAK + "Invalid Event. Please provide event time!\n" + LINE_BREAK);
     }
 
-    public static void markTask(String[] inputWords) { //marks task as done in the list
+    public static void markTask(String[] inputWords) {
         int listIndex = Integer.parseInt(inputWords[1]) - 1;
         Task markedTask = tasks.get(listIndex);
         markedTask.setIsCompleted(true);
         printMarkedTask(markedTask);
     }
 
-    public static void unmarkTask(String[] inputWords){ //marks task as not done in the list
+    public static void unmarkTask(String[] inputWords){
         int listIndex = Integer.parseInt(inputWords[1]) - 1;
         Task unmarkedTask = tasks.get(listIndex);
         unmarkedTask.setIsCompleted(false);
         printUnmarkedTask(unmarkedTask);
     }
 
-    public static void addTask(String lineInput) { //adds task to list
+    public static void addTask(String lineInput) {
         Task task = new Task(lineInput);
         tasks.add(task);
         printAddedTask(task);
     }
 
-    public static void addDeadline(String[] inputWords) { //adds deadline to list of tasks if keyword "/by" is found
-        if(!Arrays.asList(inputWords).contains("/by")) {
-            printInvalidDeadline();
-        }
-        else {
-            Deadline deadline = getDeadline(inputWords);
-            tasks.add(deadline);
-            printAddedTask(deadline);
-        }
+    public static void addDeadline(String[] inputWords) {
+         try {
+             Deadline deadline = getDeadline(inputWords);
+             tasks.add(deadline);
+             printAddedTask(deadline);
+         } catch (NoDescriptionException e) {
+             System.out.println(LINE_BREAK + "Oh No! The description of a deadline cannot be empty.\n" + LINE_BREAK);
+         } catch (InvalidTaskFormatException e) {
+             printInvalidDeadline();
+         }
     }
 
-    public static void addEvent(String[] inputWords) { //adds event to list of tasks if keyword "/at" is found
-        if(!Arrays.asList(inputWords).contains("/at")) {
-            printInvalidEvent();
-        }
-        else {
+    public static void addEvent(String[] inputWords) {
+        try {
             Event event = getEvent(inputWords);
             tasks.add(event);
             printAddedTask(event);
+        } catch (NoDescriptionException e) {
+            System.out.println(LINE_BREAK + "Oh No! The description of an event cannot be empty.\n" + LINE_BREAK);
+        } catch (InvalidTaskFormatException e) {
+            printInvalidEvent();
         }
     }
 
-    public static void addToDo(String[] inputWords){ //adds todo to list of tasks
-        ToDo toDo = getToDo(inputWords);
-        tasks.add(toDo);
-        printAddedTask(toDo);
+    public static void addToDo(String[] inputWords){
+        try {
+            ToDo toDo = getToDo(inputWords);
+            tasks.add(toDo);
+            printAddedTask(toDo);
+        } catch (NoDescriptionException e) {
+            System.out.println(LINE_BREAK + "Oh No! The description of a todo cannot be empty.\n" + LINE_BREAK);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/NoDescriptionException.java
@@ -0,0 +1,3 @@
+public class NoDescriptionException extends Exception {
+
+}
