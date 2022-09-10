7ab3f6eb415619bf338620e4503395bd7fd4dbe2
chydarren <chydarren@gmail.com>
2022-09-10 09:04:21 +0800

    Add ability to delete tasks

--- src/main/java/duke/text/ErrorText.java
+++ src/main/java/duke/text/ErrorText.java
@@ -2,11 +2,12 @@ package duke.text;
 
 public enum ErrorText {
     ERROR_INVALID_COMMAND("    Oh no, Doraemon didn't understand your command."),
-    ERROR_INVALID_STATUS_TASK("    Nobita, the specified task does not exist in Doramon's 4D pocket."),
+    ERROR_INVALID_TASK_INDEX("    Nobita, the specified task does not exist in Doramon's 4D pocket."),
     ERROR_INVALID_STATUS_FORMAT("    Nobita, the format is: mark <taskIndex> or unmark <taskIndex>, e.g. mark 1."),
     ERROR_INVALID_TODO_FORMAT("    Nobita, the format is todo <description>, e.g. todo read books."),
     ERROR_INVALID_DEADLINE_FORMAT("    Nobita, the format is: deadline <description> /by <dueBy>, e.g. deadline marry Shizuka /by September 3rd."),
-    ERROR_INVALID_EVENT_FORMAT("    Nobita, the format is: event <description> /at <eventTime>, e.g. event marry Shizuka /at September 3rd 10-12pm.");
+    ERROR_INVALID_EVENT_FORMAT("    Nobita, the format is: event <description> /at <eventTime>, e.g. event marry Shizuka /at September 3rd 10-12pm."),
+    ERROR_INVALID_DELETE_FORMAT("    Nobita, the format is: delete <taskIndex>, e.g. delete 1.");
 
     final String text;
 
--- src/main/java/duke/text/InfoText.java
+++ src/main/java/duke/text/InfoText.java
@@ -7,6 +7,7 @@ public enum InfoText {
     INFO_TASK_MARKED("    Subarashi! Good job in completing your task, Nobita:"),
     INFO_TASK_UNMARKED("    Gambate Nobita, complete it soon! Don't procrastinate:"),
     INFO_TASK_COUNT("    Now you have %d tasks in Doraemon's 4D pocket."),
+    INFO_TASK_DELETED("    Nobita, sugoi! One task has been deleted from Doraemon's 4D pocket."),
     INFO_BYE("    Sayonara. Hope to see you again soon!");
 
     final String text;
