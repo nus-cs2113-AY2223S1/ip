7117344989b6e0edb99d54bb58bdd2344cbc5eb0
LAPTOP-KFC5FK3E\kwuac <kwuachunren@gmail.com>
2022-09-15 22:55:45 +0800

    Converted from using arrays to using ArrayList

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,19 +1,19 @@
 package duke;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static Boolean isRunning = true;
-    public static int numberOfTasks = 0;
     public static String inputLine;
-    public static Task[] tasks = new Task[100];
+    public static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void printGreetings() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
@@ -26,9 +26,9 @@ public class Duke {
         return typeOfTask;
     }
 
-    public static void printDefaultTaskResponse(int number, Task[] tasks) {
-        System.out.println("Got it. I've added this task:\n" + tasks[number].toString() +
-                "\n" + "Now you have " + (number + 1) + " tasks in the list.");
+    public static void printDefaultTaskResponse(int number, ArrayList<Task> tasks) {
+        System.out.println("Got it. I've added this task:\n" + tasks.get(number-1).toString() +
+                "\n" + "Now you have " + tasks.size() + " tasks in the list.");
     }
 
     public static void sayGoodbye() {
@@ -36,60 +36,57 @@ public class Duke {
         isRunning = false;
     }
 
-    public static void insertDeadlineTask(String inputLine, Task[] tasks) {
+    public static void insertDeadlineTask(String inputLine, ArrayList<Task> tasks) {
         try {
             String deadlineSpecifics = findTaskSpecifics(inputLine);
             String deadline = inputLine.substring(inputLine.indexOf("/by") + 3);
-            tasks[numberOfTasks] = new Deadline(deadlineSpecifics, deadline);
-            printDefaultTaskResponse(numberOfTasks, tasks);
-            numberOfTasks++;
+            tasks.add(new Deadline(deadlineSpecifics, deadline));
+            printDefaultTaskResponse(tasks.size(),tasks);
         } catch (IllegalTaskException exception) {
             System.out.println("Uh oh, the description of a deadline task cannot be empty. Try again.");
         }
     }
 
-    public static void insertToDoTask(String inputLine, Task[] tasks) {
+    public static void insertToDoTask(String inputLine, ArrayList<Task> tasks) {
         try {
             String todoSpecifics = inputLine.substring(inputLine.indexOf(" "));
-            tasks[numberOfTasks] = new Todo(todoSpecifics);
-            printDefaultTaskResponse(numberOfTasks, tasks);
-            numberOfTasks++;
+            tasks.add(new Todo(todoSpecifics));
+            printDefaultTaskResponse(tasks.size(),tasks);
         } catch (StringIndexOutOfBoundsException exception) {
             System.out.println("Uh oh, the description of a todo task cannot be empty. Try again.");
         }
     }
 
-    public static void insertEventTask(String inputLine, Task[] tasks) {
+    public static void insertEventTask(String inputLine, ArrayList<Task> tasks) {
         try {
             String eventSpecifics = findTaskSpecifics(inputLine);
             String eventDate = inputLine.substring(inputLine.indexOf("/at") + 3);
-            tasks[numberOfTasks] = new Event(eventSpecifics, eventDate);
-            printDefaultTaskResponse(numberOfTasks, tasks);
-            numberOfTasks++;
+            tasks.add(new Event(eventSpecifics, eventDate));
+            printDefaultTaskResponse(tasks.size(),tasks);
         } catch (IllegalTaskException exception) {
             System.out.println("Uh oh, the description of a event task cannot be empty. Try again");
         }
     }
 
-    public static void printTaskList(int numberOfTasks, Task[] tasks) {
+    public static void printTaskList(ArrayList<Task> tasks) {
         System.out.println("Here are the tasks in your list:\n");
-        for (int i = 0; i < numberOfTasks; i++) {
-            System.out.println((i + 1) + "." + tasks[i].toString());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i+1 + "." + tasks.get(i).toString());
         }
     }
 
-    public static void markTask(String[] taskDescriptions, Task[] tasks) {
+    public static void markTask(String[] taskDescriptions, ArrayList<Task> tasks) {
         int taskToMark = Integer.parseInt(taskDescriptions[1]) - 1;
         System.out.println("Nice! I've marked this task as done:\n");
-        tasks[taskToMark].markAsDone();
-        System.out.println(tasks[taskToMark].toString());
+        tasks.get(taskToMark).markAsDone();
+        System.out.println(tasks.get(taskToMark).toString());
     }
 
-    public static void unmarkTask(String[] taskDescriptions, Task[] tasks) {
+    public static void unmarkTask(String[] taskDescriptions, ArrayList<Task> tasks) {
         int taskToUnmark = Integer.parseInt(taskDescriptions[1]) - 1;
         System.out.println("OK, I've marked this task as not done yet:\n");
-        tasks[taskToUnmark].unmark();
-        System.out.println(tasks[taskToUnmark].toString());
+        tasks.get(taskToUnmark).unmark();
+        System.out.println(tasks.get(taskToUnmark).toString());
     }
 
     public static void runTaskList() {
@@ -118,7 +115,7 @@ public class Duke {
                 unmarkTask(details, tasks);
                 break;
             case "list":
-                printTaskList(numberOfTasks, tasks);
+                printTaskList(tasks);
                 break;
             default:
                 System.out.println("You have entered an invalid command, please try again.");
