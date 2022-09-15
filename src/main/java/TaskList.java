9c0f351ee18e525c8450322a06bec4aecc82475a
amitrahman1026 <amit.amit.rahman@gmail.com>
2022-09-15 13:42:13 +0800

    Edit code style

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 
 /**
-* Add the ability to store whatever text entered by the user and display them back to the user when requested.
-* */
+ * Add the ability to store whatever text entered by the user and display them back to the user when requested.
+ */
 
 public class TaskList {
     private static ArrayList<Task> taskList = new ArrayList<>();
 
-    /** Commands */
+    /**
+     * Commands
+     */
     public static final String LIST = "list";
     public static final String DONE = "done";
     public static final String TODO = "todo";
     public static final String DEADLINE = "deadline";
     public static final String EVENT = "event";
 
-    /** Parser delimiters */
+    /**
+     * Parser delimiters
+     */
 
     public static final String GET_DESCRIPTION_DELIMITER = "/.+";
     public static final String GET_AT_DELIMITER = ".+/at ";
@@ -27,28 +31,32 @@ public class TaskList {
 
     public static final String DIVIDER = "______________________________________";
 
-    /** Constructor */
+    /**
+     * Constructor
+     */
 
-    public TaskList(){
+    public TaskList() {
     }
 
-    /** Parser and command executor */
-    public static void parseInput(String userInput){
+    /**
+     * Parser and command executor
+     */
+    public static void parseInput(String userInput) {
         String[] delimitedInputArray = userInput.split(SPACE_DELIMITER, 2);
         String command = delimitedInputArray[0];
 
         userInput = userInput.replaceAll(START_LINE_DELIMITER + command + SPACE_DELIMITER, "");
 
-        if(command.equals(DONE)){
+        if (command.equals(DONE)) {
             markAsDone(userInput);
         } else if (command.equals(LIST)) {
             String taskList = getTaskList();
             System.out.println(
                     "______________________________________\n" +
-                    taskList +
-                    "______________________________________"
+                            taskList +
+                            "______________________________________"
             );
-        } else if (command.equals(TODO)){
+        } else if (command.equals(TODO)) {
             addTodo(userInput);
         } else if (command.equals(EVENT)) {
             String taskDescription = userInput.replaceAll(GET_DESCRIPTION_DELIMITER, "");
@@ -62,16 +70,19 @@ public class TaskList {
     }
 
 
-    public static void printAcknowledgement(){
+    public static void printAcknowledgement() {
         int taskCount = taskList.size();
         String latestTaskAdded = taskList.get(taskCount - 1).showTask();
         System.out.println("Got it. I've added this task:\n  " +
-                        latestTaskAdded +
+                latestTaskAdded +
                 "\nNow you have " + taskCount + " tasks in the list"
         );
     }
-    /** Add todo task */
-    public static void addTodo(String taskDescription){
+
+    /**
+     * Add todo task
+     */
+    public static void addTodo(String taskDescription) {
         Task newTask = new Todo(taskDescription);
         taskList.add(newTask);
 
@@ -79,9 +90,12 @@ public class TaskList {
         printAcknowledgement();
         System.out.println(DIVIDER);
     }
-    /** Add event task */
 
-    public static void addEvent(String taskDescription, String at){
+    /**
+     * Add event task
+     */
+
+    public static void addEvent(String taskDescription, String at) {
         Task newTask = new Event(taskDescription, at);
         taskList.add(newTask);
         System.out.println(DIVIDER);
@@ -89,7 +103,9 @@ public class TaskList {
         System.out.println(DIVIDER);
     }
 
-    /** Add deadline task */
+    /**
+     * Add deadline task
+     */
     public static void addDeadline(String taskDescription, String by) {
         Task newTask = new Deadline(taskDescription, by);
         taskList.add(newTask);
@@ -101,9 +117,10 @@ public class TaskList {
     /**
      * Function that retrieves all
      * tasks in taskList. To check if '\n' is needed
+     *
      * @return String of all tasks
-     * */
-    public static String getTaskList(){
+     */
+    public static String getTaskList() {
         StringBuilder listOutput = new StringBuilder();
 
 
@@ -118,16 +135,16 @@ public class TaskList {
 
     /**
      * Function sets task in task-list to be done
-     * */
-    public static void markAsDone(String userInput){
-        int index =  Integer.parseInt(userInput) - 1;
+     */
+    public static void markAsDone(String userInput) {
+        int index = Integer.parseInt(userInput) - 1;
         taskList.get(index).markDone();
         System.out.println(
                 DIVIDER + "\n" +
-                "Nice! I've marked this task as done:\n  " +
-                taskList.get(index).showTask() +
-                "\n" +
-                DIVIDER
+                        "Nice! I've marked this task as done:\n  " +
+                        taskList.get(index).showTask() +
+                        "\n" +
+                        DIVIDER
         );
     }
 
