ff6d5c420ed64b79c3b854e2bf2b9123a57aff1c
Jorelle <jorellesee2000@gmail.com>
2022-09-15 18:14:17 +0800

    Merge branch 'branch-level-6'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Duke {
     public static int numberOfTasks = 0;
-    public static Task[] myList = new Task[1000];
+    public static ArrayList<Task> myList = new ArrayList<Task>();
 
     public static void main(String[] args) throws MethodNotRecognisedException {
 
@@ -17,11 +21,12 @@ public class Duke {
                 "____________________________________________________________\n";
         System.out.println("\n" + GREETING);
 
-        try {
-            while (true) {
-                Scanner sc = new Scanner(System.in);
-                String input = sc.nextLine();
-                String words[] = input.split(" ");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            String words[] = input.split(" ");
+            try {
                 if (input.startsWith("bye")) {
                     sayBye();
                     break;
@@ -37,13 +42,18 @@ public class Duke {
                     addEvent(input);
                 } else if (input.startsWith("deadline")) {
                     addDeadline(input);
-                } else throw new MethodNotRecognisedException();
+                } else if (input.startsWith("delete")) {
+                    myList.remove(Integer.parseInt(words[1]) - 1);
+                    numberOfTasks--;
+                } else {
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+
+                saveTasks();
+
+            } catch (NoTaskSpecifiedException e) {
+                System.out.println("OOPS!!! I'm sorry, no task specified :-(");
             }
-        }catch(MethodNotRecognisedException e){
-            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-        }
-        catch(NoTaskSpecifiedException e){
-            System.out.println("OOPS!!! I'm sorry, no task specified :-(");
         }
     }
 
@@ -58,32 +68,34 @@ public class Duke {
         for (int i = 0; i < numberOfTasks; i++) {
             System.out.print(i + 1);
             System.out.print(".");
-            System.out.println(myList[i].toString());
+            System.out.println(myList.get(i).toString());
         }
+        System.out.println("You have "+numberOfTasks+" tasks in the list");
     }
 
     private static void markTask(String[] words) {
         int index = Integer.parseInt(words[1]);
-        myList[index - 1].markAsDone();
+        myList.get(index - 1).markAsDone();
         System.out.println("____________________________________________________________\n" +
                 "Nice! I've marked this task as done:\n" +
-                myList[index - 1].getStatusIcon() + " " + myList[index - 1].description +
+                myList.get(index - 1).getStatusIcon() + " " + myList.get(index - 1).description +
                 "\n____________________________________________________________");
     }
 
     private static void unmarkTask(String[] words) {
         int index = Integer.parseInt(words[1]);
-        myList[index - 1].markAsNotDone();
+        myList.get(index - 1).markAsNotDone();
         System.out.println("____________________________________________________________\n" +
                 "     Ok, I've marked this task as not done yet:\n" +
-                myList[index - 1].getStatusIcon() + " " + myList[index - 1].description +
+                myList.get(index - 1).getStatusIcon() + " " + myList.get(index - 1).description +
                 "    ____________________________________________________________");
     }
 
     private static void addDeadline(String input) {
         String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/by") - 1);
         String by = input.substring(input.indexOf("/by") + 4);
-        myList[numberOfTasks] = new Deadline(description, by);
+        Task newElement = new Deadline(description, by);
+        myList.add(newElement);
         numberOfTasks++;
         printCurrentTaskMessage();
     }
@@ -91,7 +103,8 @@ public class Duke {
     private static void addEvent(String input) {
         String description = input.substring(input.indexOf(" ") + 1, input.indexOf("/at") - 1);
         String at = input.substring(input.indexOf("/at") + 4);
-        myList[numberOfTasks] = new Event(description, at);
+        Task newElement = new Event(description, at);
+        myList.add(newElement);
         numberOfTasks++;
         printCurrentTaskMessage();
     }
@@ -102,15 +115,55 @@ public class Duke {
             throw new NoTaskSpecifiedException();
         }
         String description = input.substring(input.indexOf(" ") + 1);
-        myList[numberOfTasks] = new Todo(description);
+        Task newElement = new Todo(description);
+        myList.add(newElement);
         numberOfTasks++;
         printCurrentTaskMessage();
     }
 
     private static void printCurrentTaskMessage(){
         System.out.println("Got it. I've added this task:");
-        System.out.println(myList[numberOfTasks-1].toString());
+        System.out.println(myList.get(numberOfTasks-1).toString());
         System.out.println("Now you have "+numberOfTasks+" tasks in the list");
     }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+    private static void saveTasks() {
+        String home = System.getProperty("user.home");
+        boolean directoryExists = new java.io.File(home + "/Desktop/tasks.txt").exists();
+        if(!directoryExists){
+            System.out.println("directory does not exist");
+        }
+        String filePath = home + "/Desktop/tasks.txt";
+        String fileContent = "";
+        for (int i = 0; i < numberOfTasks; i++) {
+            fileContent += (myList.get(i).toString());
+            fileContent += "\n";
+        }
+        try {
+            writeToFile(filePath, fileContent);
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+//    private static void retrieveTasks() {
+//        String home = System.getProperty("user.home");
+//        boolean directoryExists = new java.io.File(home + "/Desktop/tasks.txt").exists();
+//        if(!directoryExists){
+//            System.out.println("directory does not exist");
+//        }
+//        String filePath = home + "/Desktop/tasks.txt";
+//        Scanner s = new Scanner(filePath);
+//        while (s.hasNext()) {
+//            String next = s.nextLine();
+//            if(next.charAt(1) == 'T'){
+//                addTodo();
+//            }
+//        }
+//    }
 }
 
