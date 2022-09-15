9ecc7fada09f0751443c46a6a525b68219041046
HT-T <kikutahang@gmail.com>
2022-09-15 16:09:27 +0800

    Level-7 funct

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,149 @@
 import Tasks.*;
-import Exceptions.*;
+import Exceptions.*;////////////////////////
 
 import java.util.Scanner;
 import java.util.Vector;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class Duke {
-    public static final int EVENT_KEYWORD_LENGTH = 6;
-    public static final int DEADLINE_KEYWORD_LENGTH = 9;
+    protected static final int EVENT_KEYWORD_LENGTH = 6;
+    protected static final int DEADLINE_KEYWORD_LENGTH = 9;
+    private static final String FILE_DIRECTORY_LOCATION = Paths.get(".", "save").toString(); ////////
+    protected static final String DATA_FILE_NAME = "duke" + ".txt";
+    private static final String DATA_FILE_PATH = Paths.get(FILE_DIRECTORY_LOCATION, DATA_FILE_NAME).toString();//////////
+    //protected static final String DATA_FILE_PATH = "src/main/java/" + DATA_FILE_NAME;
+    protected static final String SEPARATOR = "|";
+    protected static final String TASK_DONE_FLAG = "1";
+    protected static final String TASK_NOT_DONE_FLAG = "0";
 
-    protected static void printGreet(){
+    public static void createFile() { // create file if it is not present
+        File fileHandle = new File(DATA_FILE_PATH);
+        if (!fileHandle.exists()) {
+            try {
+                fileHandle.createNewFile();
+            } catch (IOException e) {
+                System.out.println(fileHandle.getParentFile().mkdirs()); ////////
+                System.out.println("Error - An error has occurred when creating 'duke.txt': ");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void fileParse(String filePath) {
+        File fileHandler = new File(DATA_FILE_PATH);
+        Scanner fileScanner = null; 
+        Vector<Task> tasks = new Vector<>(); 
+        try {
+            fileScanner = new Scanner(fileHandler);
+            fileScanner.useDelimiter(System.lineSeparator());
+        } catch (FileNotFoundException e) {
+            System.out.println("Error - 'duke.txt' not found");
+        }
+
+        while (fileScanner.hasNext()) {
+            String nextTask = fileScanner.next();
+            String[] taskTypes = nextTask.split(SEPARATOR);
+            Boolean taskDone = null;
+
+            switch (taskTypes[0]) {
+            case "T":
+                if (Objects.equals(taskTypes[1], TASK_DONE_FLAG)) {
+                    taskDone = true;
+                } else if ((Objects.equals(taskTypes[1], TASK_NOT_DONE_FLAG))) {
+                    taskDone = false;
+                }
+                tasks.add(new ToDo(taskTypes[2], taskDone));
+                break;
+
+            case "D":
+                if (Objects.equals(taskTypes[1], TASK_DONE_FLAG)) {
+                    taskDone = true;
+                } else if ((Objects.equals(taskTypes[1], TASK_NOT_DONE_FLAG))) {
+                    taskDone = false;
+                }
+                tasks.add(new Deadline(taskTypes[2], taskDone, taskTypes[3]));
+                break;
+
+            case "E":
+                if (Objects.equals(taskTypes[1], TASK_DONE_FLAG)) {
+                    taskDone = true;
+                } else if ((Objects.equals(taskTypes[1], TASK_NOT_DONE_FLAG))) {
+                    taskDone = false;
+                }
+                tasks.add(new Event(taskTypes[2], taskDone, taskTypes[3]));
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    public static void writeDisk(String newContent) throws IOException {
+        File fileHandle = new File(DATA_FILE_PATH);
+        try (FileWriter fileWriter = new FileWriter(fileHandle)) {
+        if (!fileHandle.exists()){
+            fileHandle.createNewFile();
+        }
+        fileWriter.write(newContent);
+        fileWriter.close();
+        } catch (IOException e) { // any exceptions, file will be deleted
+            fileHandle.delete();
+        }
+    }
+    
+    public static String parseTaskToString(Vector <Task> tasks) {
+        String resultList = "";
+        String taskString = "";
+
+        for (int i = 1; i <= tasks.size(); i++) {
+            String taskType = tasks.get(i - 1).getTaskType();
+            String markDone;
+            if (tasks.get(i - 1).getTaskDone()) {
+                markDone = TASK_DONE_FLAG;
+            } else {
+                markDone = TASK_NOT_DONE_FLAG;
+            }
+
+            switch (taskType) {
+            case "ToDo":
+                taskString = "T " + SEPARATOR + " " + markDone + " " + SEPARATOR + " " + tasks.get(i - 1).getTaskTitle() +
+                        System.lineSeparator();
+                resultList += taskString;
+                break;
+
+            case "Deadline":
+                String deadline = tasks.get(i - 1).getTaskDeadline();
+                taskString = "D " + SEPARATOR + " " + markDone + " " + SEPARATOR + " " + tasks.get(i - 1).getTaskTitle() +
+                        " " + SEPARATOR + " " + deadline + System.lineSeparator();
+                resultList += taskString;
+                break;
+
+            case "Event":
+                String eventTime = tasks.get(i - 1).getTaskDate();
+                taskString = "E " + SEPARATOR + " " + markDone + " " + SEPARATOR + " " + tasks.get(i - 1).getTaskTitle() +
+                        " " + SEPARATOR + " " + eventTime + System.lineSeparator();
+                resultList += taskString;
+                break;
+
+            default:
+                break;
+            }
+        }
+        return resultList;
+    }
+
+    
+    
+    
+    
+    
+    public static void printGreet(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -57,8 +192,13 @@ public class Duke {
 
                 System.out.println("Nice! I've marked this task as done: \n" + doneMark +
                         tasks.get(taskIndex).getTaskTitle());
+
+                String newEntry = parseTaskToString(tasks);
+                writeDisk(newEntry);
             } catch (InvalidListIndexException | NumberFormatException e) {
                 System.out.println("Error - List index given invalid. Please check again.");
+            } catch (IOException e) {
+                System.out.println("Error when saving file: " + e.getMessage());
             }
             break;
             
@@ -79,8 +219,13 @@ public class Duke {
 
                 System.out.println("Nice! I've marked this task as not done: \n" + doneMark +
                         tasks.get(taskIndex).getTaskTitle());
+
+                String newEntry = parseTaskToString(tasks);
+                writeDisk(newEntry);
             } catch (InvalidListIndexException | NumberFormatException e) {
                 System.out.println("Error - List index given invalid. Please check again.");
+            } catch (IOException e) {
+                System.out.println("Error when saving file: " + e.getMessage());
             }
             break;
             
@@ -95,8 +240,13 @@ public class Duke {
                 System.out.println("Got it. I've added this task: \n" + "[T][ ] "
                         + inData.substring(5) ); //////////magic number problem
                 System.out.println("Now you have " + tasks.size() +  " tasks in the list.");
+
+                String newEntry = parseTaskToString(tasks);
+                writeDisk(newEntry);
             } catch (InvalidTodoException e) {
                 System.out.println("Error - Please input a description for your To do. ");
+            } catch (IOException e) {
+                System.out.println("Error when saving file: " + e.getMessage());
             }
             break;
 
@@ -118,12 +268,17 @@ public class Duke {
                     System.out.println("Got it. I've added this event: \n" + "[E] [ ]"
                             + eventTitle + " (at: " + eventDate + ")");
                     System.out.println("Now you have " + tasks.size() +  " tasks in the list.");
+
+                    String newEntry = parseTaskToString(tasks);
+                    writeDisk(newEntry);
                 } catch (InvalidEventException e) {
                     System.out.println("Error - '/at' must be included.");
                 } catch (InvalidTaskTitleException e) {
                     System.out.println("Error - input an event description");
                 } catch (InvalidTaskDateException e) {
                     System.out.println("Error - input an event date.");
+                } catch (IOException e) {
+                    System.out.println("Error when saving file: " + e.getMessage());
                 }
                 break;
 
@@ -147,12 +302,18 @@ public class Duke {
                     System.out.println("Now you have " + tasks.size() +  " tasks in the list.");
 
 
+                    String newEntry = parseTaskToString(tasks);
+                    writeDisk(newEntry);
+
+
                 } catch (InvalidDeadlineException e){
                     System.out.println("Error - '/by' must be included.");
                 } catch (InvalidTaskTitleException e) {
                     System.out.println("Error - input a deadline description");
                 } catch (InvalidTaskDateException e) {
                     System.out.println("Error - input a deadline date.");
+                } catch (IOException e) {
+                    System.out.println("Error when saving file: " + e.getMessage());
                 }
                 break;
 
@@ -165,7 +326,7 @@ public class Duke {
             }
         }
     }
-    
+  
     protected static void printGoodbyeMeessage(){
         System.out.println("Bye. Hope to see you again soon!");
     }
@@ -209,6 +370,8 @@ public class Duke {
 
     public static void main(String[] args) {
         printGreet();
+        createFile();
+        fileParse(DATA_FILE_PATH);
         processInput();
         printGoodbyeMeessage();  
     }
--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -6,4 +6,10 @@ public class Event extends Task {
         super.setTaskType("Event");
         super.setTaskDate(taskDate);
     }
+
+    public Event(String taskTitle, boolean taskDone, String taskDate) {
+        super(taskTitle, taskDone);
+        super.setTaskType("Event");
+        super.setTaskDate(taskDate);
+    }
 }
