c89702f49fa2b596f7d7afda3cc81e341fbac72c
owenl131 <owenl131@gmail.com>
2022-09-17 21:14:01 +0800

    Parse dates and times flexibly

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,7 +1,16 @@
 package duke;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.undo.CompoundEdit;
 
 import duke.command.AddCommand;
 import duke.command.Command;
@@ -72,5 +81,90 @@ public class Parser {
             paramsMap.put(key, value);
         }
         return paramsMap;
+    }
+
+    public static LocalDate parseDateString(String input) {
+        if (input.contains(" ")) {
+            for (String part : input.split(" ")) {
+                LocalDate attempt = parseDateString(part); // attempt to process recursively
+                if (attempt != null) {
+                    return attempt;
+                }
+            }
+        } else if (input.contains("-") || input.contains("/")) {
+            String[] components = input.split("[-/]");
+            boolean yearFirst = components[0].length() == 4;
+            int year = Integer.parseInt(yearFirst ? components[0] : components[2]);
+            int month = Integer.parseInt(components[1]);
+            int day = Integer.parseInt(yearFirst ? components[2] : components[0]);
+            return LocalDate.of(year, month, day);
+        }
+        try {
+            Pattern yearPattern = Pattern.compile("\\b(2\\d{3})\\b"); // four digit number starting with 2
+            Matcher yearMatcher = yearPattern.matcher(input);
+            // default to current year
+            int year = yearMatcher.find() ? Integer.parseInt(yearMatcher.group()) : LocalDate.now().getYear();
+            List<String> months = List.of("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov",
+                    "dec");
+            Pattern monthPattern = Pattern.compile("(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)");
+            Matcher monthMatcher = monthPattern.matcher(input.toLowerCase());
+            if (!monthMatcher.find()) {
+                return null;
+            }
+            int month = months.indexOf(monthMatcher.group()) + 1;
+            Pattern dayPattern = Pattern.compile("\\b(\\d{1,2})\\b");
+            Matcher dayMatcher = dayPattern.matcher(input);
+            if (!dayMatcher.find()) {
+                return null;
+            }
+            int day = Integer.parseInt(dayMatcher.group());
+            return LocalDate.of(year, month, day);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    public static LocalTime parseTimeString(String input) {
+        input = input.trim();
+        // if input contains spaces, then it may contain both a
+        // date component and time component
+        // attempt to parse each component and take the one that succeeds
+        if (input.contains(" ")) {
+            for (String part : input.split(" ")) {
+                LocalTime attempt = parseTimeString(part);
+                if (attempt != null) {
+                    return attempt;
+                }
+            }
+            return null;
+        }
+        // assume that the time must be formatted as HH:MM(AM|PM)?
+        if (!input.contains(":")) {
+            return null;
+        }
+        String suffix = "";
+        input = input.toUpperCase();
+        if (input.endsWith("PM")) {
+            suffix = "PM";
+            input = input.replace("PM", "");
+        } else if (input.endsWith("AM")) {
+            suffix = "AM";
+            input = input.replace("AM", "");
+        }
+        try {
+            String[] components = input.split(":");
+            int hours = Integer.parseInt(components[0]);
+            int minutes = Integer.parseInt(components[1]);
+            if (suffix.equals("PM")) {
+                hours += 12;
+            } else if (suffix.equals("AM") && hours == 12) {
+                hours -= 12;
+            }
+            return LocalTime.of(hours, minutes);
+        } catch (NumberFormatException e) {
+            return null;
+        } catch (DateTimeException e) {
+            return null;
+        }
     }
 }
