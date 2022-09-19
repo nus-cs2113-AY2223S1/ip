526f742131a7eee87a85c4bab11cfad7264d8143
xzynos <lim_fang_jie@hotmail.com>
2022-09-19 23:55:12 +0800

    Add Configurations.java

---
+++ src/main/java/duke/common/Configurations.java
@@ -0,0 +1,9 @@
+package duke.common;
+
+/**
+ * Stores configuration parameters.
+ */
+public class Configurations {
+    public static final String TASKS_DIRECTORY_PATH = "./data/";
+    public static final String TASKS_FILENAME = "tasks.txt";
+}
