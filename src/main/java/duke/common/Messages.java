358e8d0727485fea3ec0421835a7de630c6c7965
xzynos <lim_fang_jie@hotmail.com>
2022-09-20 00:25:21 +0800

    Add Messages.java

---
+++ src/main/java/duke/common/Messages.java
@@ -0,0 +1,11 @@
+package duke.common;
+
+/**
+ * Defines messages used by program.
+ */
+public class Messages {
+    public static final String CONSOLE_ERROR_ARGUMENT_NOT_INTEGER = "The argument provided is not a valid integer.";
+    public static final String CONSOLE_ERROR_COMMAND_TODO_INVALID_SYNTAX = "The arguments are invalid. SYNTAX: todo DESCRIPTION";
+    public static final String CONSOLE_ERROR_COMMAND_DEADLINE_INVALID_SYNTAX = "The arguments are invalid. SYNTAX: deadline DESCRIPTION /by dd/MM/yyyy HHmm";
+    public static final String CONSOLE_ERROR_COMMAND_EVENT_INVALID_SYNTAX = "The arguments are invalid. SYNTAX: event DESCRIPTION /at dd/MM/yyyy HHmm dd/MM/yyyy HHmm";
+}
