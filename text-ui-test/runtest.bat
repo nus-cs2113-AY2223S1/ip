e867ade9776c086f637a62b8884c8f05a79c3c81
Devesh Logendran <deveshl@users.noreply.github.com>
2022-09-20 01:21:15 +0800

    Add delete task command and tests

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,7 +6,8 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-
+REM delete existing build artifacts
+del /S/Q ..\bin\*
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\duke\Duke.java
@@ -17,7 +18,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin duke.Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
