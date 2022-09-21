50ed695c65f8c6413d0a81c001c66bb21dcf6b0f
chydarren <chydarren@gmail.com>
2022-09-21 12:04:44 +0800

    Teach Duke how to understand dates and times for the deadline class

--- src/main/java/duke/data/task/Deadline.java
+++ src/main/java/duke/data/task/Deadline.java
@@ -1,13 +1,72 @@
 package duke.data.task;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     private static final String TASK_TYPE = "[D]";
     private static final String TIME_PREFIX = " (by: ";
     private static final String TIME_POSTFIX = ")";
-    private String dueBy;
+    private static final String TIME_INFIX = ", ";
+    private static final String EMPTY_SPACE = " ";
+    private static final String DELIMITER = "/";
 
-    public Deadline(String title, String dueBy, boolean isDone) {
+    private static LocalDate dueByDate;
+    private static LocalTime dueByTime;
+    public static String dueBy;
+
+    /**
+     * Stores the deadline of task as LocalDate and LocalTime objects in the program.
+     *
+     * @param dueBy A string that represents the deadline of the task.
+     * @throws DateTimeParseException If an error is caught in converting the string to datetime format.
+     */
+    public static void ConvertStringToDatetime(String dueBy) throws DateTimeParseException {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
+
+        // Splits the deadline into two parts, i.e. date and/or time, whichever applicable
+        if (dueBy.contains(EMPTY_SPACE)) {
+            String[] dueBySplits = dueBy.split(EMPTY_SPACE);
+            dueByDate = LocalDate.parse(dueBySplits[0], dateFormatter);
+            dueByTime = LocalTime.parse(dueBySplits[1], timeFormatter);
+        } else if (dueBy.contains(DELIMITER)) {
+            dueByDate = LocalDate.parse(dueBy, dateFormatter);
+        } else {
+            dueByTime = LocalTime.parse(dueBy, timeFormatter);
+        }
+    }
+
+    /**
+     * Converts the LocalDate and LocalTime objects into a meaningful deadline format.
+     *
+     * @param dueByDate A LocalDate object that represents the date portion of the task deadline.
+     * @param dueByTime A LocalTime object that represents the time portion of the task deadline.
+     * @return A string that represents the deadline of the task in a meaningful format.
+     */
+    public String ConvertDatetimeToString(LocalDate dueByDate, LocalTime dueByTime) {
+        DateTimeFormatter datePrintFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy (E)");
+        DateTimeFormatter timePrintFormatter = DateTimeFormatter.ofPattern("h:mm a");
+
+        String dueBy;
+        // Joins the two parts, i.e. date and/or time into a single deadline, whichever applicable
+        if (dueByDate != null && dueByTime != null) {
+            dueBy = dueByDate.format(datePrintFormatter) + TIME_INFIX +
+                    dueByTime.format(timePrintFormatter);
+        } else if (dueByTime != null) {
+            dueBy = dueByTime.format(timePrintFormatter);
+        } else {
+            dueBy = dueByDate.format(datePrintFormatter);
+        }
+        return dueBy;
+    }
+
+    public Deadline(String title, String dueBy, boolean isDone) throws DateTimeParseException {
         super(title, isDone);
+        // Stores the deadline of task as LocalDate and LocalTime objects in the program.
+        ConvertStringToDatetime(dueBy);
         this.dueBy = dueBy;
     }
 
@@ -17,6 +76,7 @@ public class Deadline extends Task {
 
     @Override
     public String getTaskDetails() {
-        return TASK_TYPE + super.getTaskDetails() + TIME_PREFIX + getDueBy() + TIME_POSTFIX;
+        return TASK_TYPE + super.getTaskDetails() + TIME_PREFIX +
+                ConvertDatetimeToString(dueByDate, dueByTime) + TIME_POSTFIX;
     }
 }
