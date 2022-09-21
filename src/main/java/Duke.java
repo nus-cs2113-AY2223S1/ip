247e795859040bc9b80551770626f04a488e27ad
DESKTOP-8022BOQ\Admin <lcsroy99@gmail.com>
2022-09-21 15:47:26 +0800

    Redone level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,39 +10,43 @@ public class Duke {
 
         String line;
         Scanner in = new Scanner(System.in);
+        System.out.println("-------------------------------------------------------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon");
+        System.out.println("-------------------------------------------------------------------------------");
         String[] List = new String[100];
-        int ListNo = 0;
-        do{
-            line = in.nextLine();
-            if (line.equals("bye")){
-                break;
-            }else if(line.compareTo("list") != 0) {
-                if(line.contains("mark")){
-                    int i = line.indexOf(" ") + 1;
-                    Task t = new Task(List[Integer.parseInt(line.substring(i))-1]);
-                    t.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("[" + t.getStatusIcon() + "] " + List[Integer.parseInt(line.substring(i))-1]);
-                } else if(line.contains("unmark")) {
-                    int i = line.indexOf(" ") + 1;
-                    Task t = new Task(List[Integer.parseInt(line.substring(i))-1]);
-                    t.unmarkAsNotDone();
-                    System.out.println("Ok, I've marked this task as not done yet:");
-                    System.out.println("[" + t.getStatusIcon() + "] " + List[Integer.parseInt(line.substring(i))-1]);
-                } else {
-                    System.out.println("added: " + line);
-                    List[ListNo] = line;
-                    ListNo++;
-                }
-            }else if(line.compareTo("list") == 0){
-                for(int i = 0; i < ListNo; i++) {
-                    Task t = new Task(List[i]);
-                    System.out.println((i+1) + ".[ " + t.getStatusIcon() + "] " + List[i]);
-                }
-            }
-        } while (line.compareTo("bye") != 0);
-        System.out.println("Bye. Hope to see you again soon!");
+//        int ListNo = 0;
+//        do{
+//            line = in.nextLine();
+//            if (line.equals("bye")){
+//                break;
+//            }else if(line.compareTo("list") != 0) {
+//                if(line.contains("mark")){
+//                    int i = line.indexOf(" ") + 1;
+//                    Task t = new Task(List[Integer.parseInt(line.substring(i))-1]);
+//                    t.markAsDone();
+//                    System.out.println("Nice! I've marked this task as done:");
+//                    System.out.println("[" + t.getStatusIcon() + "] " + List[Integer.parseInt(line.substring(i))-1]);
+//                } else if(line.contains("unmark")) {
+//                    int i = line.indexOf(" ") + 1;
+//                    Task t = new Task(List[Integer.parseInt(line.substring(i))-1]);
+//                    t.unmarkAsNotDone();
+//                    System.out.println("Ok, I've marked this task as not done yet:");
+//                    System.out.println("[" + t.getStatusIcon() + "] " + List[Integer.parseInt(line.substring(i))-1]);
+//                } else {
+//                    System.out.println("added: " + line);
+//                    List[ListNo] = line;
+//                    ListNo++;
+//                }
+//            }else if(line.compareTo("list") == 0){
+//                for(int i = 0; i < ListNo; i++) {
+//                    Task t = new Task(List[i]);
+//                    System.out.println((i+1) + ".[ " + t.getStatusIcon() + "] " + List[i]);
+//                }
+//            }
+//        } while (line.compareTo("bye") != 0);
+//        System.out.println("Bye. Hope to see you again soon!");
     }
 }
