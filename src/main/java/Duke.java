4d4fce09cee74232cdf8a5445f12923fc835274e
Ishita <ishitamandal06@gmail.com>
2022-09-22 16:50:04 +0800

    level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     static String inputText = "";
-    static Task[] tasks = new Task[100];
+    static ArrayList<Task> tasks = new ArrayList<>();
     static int taskCount = 0;
     static boolean isRunning = true;
     public static void main(String[] args) throws NullCommandException {
@@ -30,7 +31,7 @@ public class Duke {
     public static void handleListMessage(){
         System.out.println("____________________________________________________________");
         for(int i=0;i< taskCount;i++){
-            System.out.println(tasks[i].toString() );
+            System.out.println(tasks.get(i).toString() );
         }
         System.out.println("____________________________________________________________");
     }
@@ -38,11 +39,14 @@ public class Duke {
     public static void addNewTask(String command) {
         try {
             if(command.equals("todo")){
-                tasks[taskCount++] = new Todo(inputText.substring(5));
+                tasks.add( new Todo(inputText.substring(5)));
+                taskCount++;
             } else if (command.equals("deadline")) {
-                tasks[taskCount++] = new Deadline(inputText.substring(9,inputText.indexOf('/')),inputText.substring(inputText.indexOf('/')+4));
+                tasks.add(new Deadline(inputText.substring(9,inputText.indexOf('/')),inputText.substring(inputText.indexOf('/')+4)));
+                taskCount++;
             }else if(command.equals("event")){
-                tasks[taskCount++] = new Event(inputText.substring(6,inputText.indexOf('/')),inputText.substring(inputText.indexOf('/')+4));
+                tasks.add(new Event(inputText.substring(6,inputText.indexOf('/')),inputText.substring(inputText.indexOf('/')+4)));
+                taskCount++;
             }else{
                 throw new InvalidCommandException();
             }
@@ -57,18 +61,30 @@ public class Duke {
 
 
     }
+    public static void handleDeleteMessage(int position) {
+        String str = tasks.get(position-1).toString();
+        tasks.remove(position-1);
+        taskCount--;
+        System.out.println("____________________________________________________________");
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(str);
+        System.out.println("Now you have "+taskCount+ " tasks in the list.");
+        System.out.println("____________________________________________________________");
+
+
+    }
     
     public static void handleMarkMessage(){
         int position = Integer.parseInt(inputText.substring(inputText.length()-1));
         if ((inputText.substring(0,4).equals("mark"))){
-            tasks[position-1].markAsDone();
+            tasks.get(position-1).markAsDone();
             System.out.println("Nice! I've marked this task as done:");
 
         }else{
-            tasks[position-1].markAsUndone();
+            tasks.get(position-1).markAsUndone();
             System.out.println("OK, I've marked this task as not done yet:");
         }
-        System.out.println("["+ tasks[position-1].getStatusIcon()+"] "+tasks[position-1].description);
+        System.out.println("["+ tasks.get(position-1).getStatusIcon()+"] "+tasks.get(position-1).description);
     }
 
     public static void handleUserInput() {
@@ -87,6 +103,8 @@ public class Duke {
                         handleListMessage();
                     }else if ((splitText[0].equals("mark")) || splitText[0].equals("unmark") ) {
                         handleMarkMessage();
+                    }else if(splitText[0].equals("delete")){
+                        handleDeleteMessage(Integer.parseInt(splitText[1]));
                     }
                     else{
                         addNewTask(splitText[0]);
