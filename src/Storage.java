4309f9975a519843a0bbc576650be8f0d90b222b
jingwei55 <e0725540@u.nus.edu>
2022-09-25 16:41:02 +0800

    Added more java documentation + closed scanner

--- src/Storage.java
+++ src/Storage.java
@@ -1,37 +1,39 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
 
-public class Storage{
+public class Storage {
     private final File file;
     private final Ui ui = new Ui();
     private final Parser parse = new Parser();
+
     public Storage(String filePath) {
         file = new File(filePath);
     }
 
     /**
      * Creates the initial task list when first booting up the Duke bot
-     * Throws an error if the file is not found and creates a new task list if the file is empty
+     * Throws an error if the file is not found and creates a new task list if the
+     * file is empty
+     * 
      * @return the task list after loading the data from the data file
      */
-    public List<Task> loadFileData(){
+    public List<Task> loadFileData() {
         ArrayList<Task> tasks = new ArrayList<>();
         try {
             if (file.createNewFile()) {
                 ui.createNewListMessage();
-            }
-            else {
+            } else {
                 ui.loadListMessage();
                 Scanner scan = new Scanner(file);
                 while (scan.hasNext()) {
                     String task = scan.nextLine();
                     tasks.add(parse.taskFromFile(task));
                 }
+                scan.close();
                 return tasks;
             }
         } catch (IOException error) {
@@ -44,8 +46,10 @@ public class Storage{
     }
 
     /**
-     * Get tasks from the task list and write them in a different format to be displayed
+     * Get tasks from the task list and write them in a different format to be
+     * displayed
      * in the data file
+     * 
      * @param tasklist list of tasks to get the tasks from
      */
     public void writeToFile(TaskList tasklist) {
@@ -63,7 +67,9 @@ public class Storage{
 
     /**
      * Adding a new task to on a new line of the data file
-     * @param textToAppend task details in specific file format to be added to the data file
+     * 
+     * @param textToAppend task details in specific file format to be added to the
+     *                     data file
      */
     public void appendToFile(String textToAppend) {
         try {
