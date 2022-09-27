fc6931b05ec678ef01105a03611355614db9545a
kaseykwok <kaseykwok@gmail.com>
2022-09-27 19:43:38 +0800

    Add JavaDoc for Duke and TaskList class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,15 +3,26 @@ package duke;
 import duke.command.Command;
 import duke.task.TaskList;
 
+/**
+ * Represent the chat bot Duke. The application starts in this class.
+ * Duke can interact with users through the user interface, and maintain the task list through different commands.
+ */
 public class Duke {
     private final Ui ui;
     private final TaskList taskList;
 
+    /**
+     * Constructor of <code>Duke</code> class.
+     * It initializes the user interface and the task list.
+     */
     public Duke() {
         ui = new Ui();
         taskList = new TaskList(ui);
     }
 
+    /**
+     * Start running the application. The entire flow of application is maintained here.
+     */
     public void run() {
         ui.showLogo();
         ui.showGreetMessage();
@@ -28,6 +39,10 @@ public class Duke {
         ui.showExitMessage();
     }
 
+    /**
+     * Main function of application
+     * @param args Optional arguments to be inputted through the command line.
+     */
     public static void main(String[] args) {
         new Duke().run();
     }
--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -6,23 +6,43 @@ import duke.Storage;
 
 import java.util.ArrayList;
 
+/**
+ * Represent a resizable list of Task objects with the list operations.
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
     public static final String FILE_STRING_SEPARATOR = " // ";
 
     private final Storage storage;
 
+    /**
+     * Constructor of <code>TaskList</code> object with the specified user interface.
+     * It initializes the storage and list of the tasks.
+     * @param ui User interface of the application.
+     */
     public TaskList(Ui ui) {
         storage = new Storage();
         tasks = new ArrayList<>(storage.loadTasks(ui));
     }
 
+    /**
+     * Add the <code>Task</code> object into the task list.
+     * @param task <code>Task</code> object to be added to the task list.
+     * @param ui User interface of the application.
+     */
     public void add(Task task, Ui ui) {
         tasks.add(task);
         ui.showAddTaskSuccessMessage(task);
         save(ui);
     }
 
+    /**
+     * List all the <code>Task</code> objects in the task list.
+     * If there are no tasks in the list, it shows a message that tell that there are not tasks.
+     * If there are tasks in the list, it shows the tasks in a numbered list, where the number represent the
+     * task number of the task.
+     * @param ui User interface of the application.
+     */
     public void list(Ui ui) {
         if (tasks.size() > 0) {
             ui.showTasks(tasks);
@@ -31,6 +51,11 @@ public class TaskList {
         }
     }
 
+    /**
+     * Mark the <code>Task</code> object specified by the task number as done.
+     * @param taskNumber Task number representing the <code>Task</code> object to be processed.
+     * @param ui User interface of the application
+     */
     public void markTaskAsDone(int taskNumber, Ui ui) {
         try {
             int taskIndex = taskNumber - 1;
@@ -44,6 +69,11 @@ public class TaskList {
         }
     }
 
+    /**
+     * Mark the <code>Task</code> object specified by the task number as not done.
+     * @param taskNumber Task number representing the <code>Task</code> object to be processed.
+     * @param ui User interface of the application
+     */
     public void markTaskAsUndone(int taskNumber, Ui ui) {
         try {
             int taskIndex = taskNumber - 1;
@@ -57,6 +87,11 @@ public class TaskList {
         }
     }
 
+    /**
+     * Delete the <code>Task</code> object specified by the task number from the task list.
+     * @param taskNumber Task number representing the <code>Task</code> object to be deleted.
+     * @param ui User interface of the application.
+     */
     public void delete(int taskNumber, Ui ui) {
         try {
             int taskIndex = taskNumber - 1;
@@ -69,7 +104,11 @@ public class TaskList {
         }
     }
 
-    public void save(Ui ui) {
+    /**
+     * Save all the tasks in the list to the storage.
+     * @param ui User interface of the application.
+     */
+    private void save(Ui ui) {
         storage.saveTasks(tasks, ui);
     }
 }
