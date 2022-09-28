312de8d14446774f241d23fe5f2445134e9a6f1b
insafhere <insaf@u.nus.edu>
2022-09-28 17:25:33 +0800

    command file

---
+++ src/main/java/Command.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Command {
+
+    public void execute(ArrayList<String> types, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
+        System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+
+    boolean isExit() {
+        return false;
+    }
+}
