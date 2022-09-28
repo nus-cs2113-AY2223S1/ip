4f74ebd03119c8d1c50eaf36ac5282aaee423619
Matthew Phua <matthew.phua@icloud.com>
2022-09-28 15:36:47 +0800

    Leve-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) throws DukeException {
         super(description);
         if (by.isBlank()) {
             throw new DukeException();
         }
-        this.by = by;
-    }
+        LocalDate date;
+        try {
+            date = LocalDate.parse(by);
+        } catch(DateTimeParseException e) {
+            throw new DukeException();
+        }
+        this.by = date;    }
 
     /**
      * Returns formatted string
@@ -17,7 +26,7 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
