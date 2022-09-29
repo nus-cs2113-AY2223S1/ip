51173fc14c7bfa022f24c94a7107008ff4a5d711
insafhere <insaf@u.nus.edu>
2022-09-30 00:41:50 +0800

    modified

--- src/main/java/unmark.java
+++ src/main/java/unmark.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 
+
 public class unmark extends Command {
     String fullCommand;
+
+    /**
+     * constructor for unmark.
+     */
     unmark(String fullCommand) {
         this.fullCommand = fullCommand;
     }
-
+    /**
+     * execute method prints out task that has been marked and updates the task list accordingly.
+     */
     public void execute(ArrayList<String> type, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
         System.out.println("OK, I've marked this task as not done yet:");
         String strNum = fullCommand.substring(7, fullCommand.length());
@@ -13,6 +20,9 @@ public class unmark extends Command {
         marks.set(num - 1, "[ ]");  // To Modify
         System.out.println(marks.get(num - 1) + " " + tasks.get(num - 1));
     }
+    /**
+     * program does not exit after this class is called.
+     */
     boolean isExit() {
         return false;
     }
