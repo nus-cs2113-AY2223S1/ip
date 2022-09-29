5cc7e285f3674426cc77fb3cfa9c6980bb503fad
insafhere <insaf@u.nus.edu>
2022-09-30 00:42:54 +0800

    modified

--- src/main/java/event.java
+++ src/main/java/event.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
 
+/**
+ * event class extend from command class.
+ */
 public class event extends Command {
     String fullCommand;
+
     event(String fullCommand) {
         this.fullCommand = fullCommand;
     }
 
+    /**
+     * execute method prints new event task and adds it into task list.
+     */
     public void execute(ArrayList<String> type, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
 
         System.out.println("Got it. I've added this task");
 
         // Modify text according to deadline
         String editedCommand = fullCommand.substring(6, fullCommand.length());
-        int indexOfSlash = editedCommand.indexOf("/", 9);
+        int indexOfSlash = editedCommand.indexOf("/", 0);
         String day = editedCommand.substring(indexOfSlash + 4, editedCommand.length());
         String task = editedCommand.substring(0, indexOfSlash);
         editedCommand = task + "(at: " + day + ")";
@@ -25,6 +32,10 @@ public class event extends Command {
         Integer count = storage.getCount();
         storage.updateCount(count+1);
     }
+
+    /**
+     * program does not exit after this class is called.
+     */
     boolean isExit() {
         return false;
     }
