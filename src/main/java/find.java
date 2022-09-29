349f3eee554b8b9a03750dfe808400472f80a402
insafhere <insaf@u.nus.edu>
2022-09-29 13:18:51 +0800

    “Modified”

--- src/main/java/find.java
+++ src/main/java/find.java
@@ -9,9 +9,9 @@ public class find extends Command {
     public void execute(ArrayList<String> type, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
         System.out.println("Here are the matching tasks in your list:");
         int numbering = 1;
-        String wordToFind = fullCommand.substring(5,fullCommand.length());
+        String wordToFind = fullCommand.substring(5,fullCommand.length()).toLowerCase();
         for (int i = 1; i < storage.getCount(); i++) {
-            if(tasks.get(i - 1).contains(wordToFind)){
+            if(tasks.get(i - 1).toLowerCase().contains(wordToFind)){
                 System.out.println(numbering + "." + type.get(i-1) + marks.get(i-1) + " " + tasks.get(i-1));
                 numbering++;
             }
