a32bcbcb6ece0bca8b2d8698de83926fb8bd859f
insafhere <insaf@u.nus.edu>
2022-09-30 00:41:35 +0800

    modified

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,12 +1,26 @@
 import java.util.Scanner;
 
+/**
+ * Ui Class.
+ */
 public class Ui {
-    Ui() {
 
+    /**
+     * empty UI Constructor.
+     */
+    Ui() {
     }
+
+    /**
+     * show error message when there is loading issues.
+     */
     String showLoadingError() {
-        return "";
+        return "Loading Error!";
     }
+
+    /**
+     * show welcome message to users when program is being started.
+     */
     void showWelcome() {
         String line = "---------------------------------------------";
         String greet = "Hello! I'm Duke";
@@ -14,10 +28,16 @@ public class Ui {
         System.out.println(line + "\n" + greet + "\n" + question + "\n" + line);
     }
 
+    /**
+     * line in UI.
+     */
     void showLine() {
         System.out.println("---------------------------------------------");
     }
 
+    /**
+     * method used to read the commands that user inputs.
+     */
     String readCommand() {
         Scanner in = new Scanner(System.in);
         String text;
@@ -25,6 +45,9 @@ public class Ui {
         return text;
     }
 
+    /**
+     * methods that helps to show the specific error message when error encountered.
+     */
     String showError(String errorMessage) {
         return errorMessage;
     }
