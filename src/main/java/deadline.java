d8b737044732754f43397df4be532b3d5ed2f153
insafhere <insaf@u.nus.edu>
2022-09-30 00:42:44 +0800

    modified

--- src/main/java/deadline.java
+++ src/main/java/deadline.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
 
+/**
+ * deadline class extend from command class.
+ */
 public class deadline extends Command {
     String fullCommand;
+
+    /**
+     * constructor for deadline.
+     */
     deadline(String fullCommand) {
         this.fullCommand = fullCommand;
     }
 
+    /**
+     * execute method prints new deadline task and adds it into the task list.
+     */
     public void execute(ArrayList<String> type, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
         System.out.println("Got it. I've added this task");
 
         // Modify text according to deadline
         String editedCommand = fullCommand.substring(9, fullCommand.length());
-        int indexOfSlash = editedCommand.indexOf("/", 9);
+        int indexOfSlash = editedCommand.indexOf("/", 0);
         String day = editedCommand.substring(indexOfSlash + 4, editedCommand.length());
         String task = editedCommand.substring(0, indexOfSlash);
         editedCommand = task + "(by: " + day + ")";
@@ -24,6 +34,10 @@ public class deadline extends Command {
         Integer count = storage.getCount();
         storage.updateCount(count+1);
     }
+
+    /**
+     * program does not exit after this class is called.
+     */
     boolean isExit() {
         return false;
     }
