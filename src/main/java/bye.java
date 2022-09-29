5be626e6695eec7199c2657f85f431a065954146
insafhere <insaf@u.nus.edu>
2022-09-30 00:42:16 +0800

    modified

--- src/main/java/bye.java
+++ src/main/java/bye.java
@@ -1,14 +1,29 @@
 import java.util.ArrayList;
 
+/**
+ * Bye Class extend from Command Class.
+ */
 public class bye extends Command {
     String fullCommand;
+
+
+    /**
+     * constructor for bye command.
+     */
     bye(String fullCommand) {
         this.fullCommand = fullCommand;
     }
 
+    /**
+     * execute method prints bye message to user.
+     */
     public void execute(ArrayList<String> type, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    /**
+     * program exit after this class is called.
+     */
     boolean isExit() {
         return true;
     }
