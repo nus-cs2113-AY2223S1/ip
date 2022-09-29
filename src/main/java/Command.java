3f1f07757d68ee71c8897fdd76baaec2807e506c
insafhere <insaf@u.nus.edu>
2022-09-30 00:41:29 +0800

    modified

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 
+/**
+ * Command Class.
+ */
 public class Command {
 
+    /**
+     * execute method prints out if command is unknown/invalid.
+     */
     public void execute(ArrayList<String> types, ArrayList<String> marks, ArrayList<String> tasks, Ui ui, Storage storage) {
         System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
+    /**
+     * program does not exit after this class is called.
+     */
     boolean isExit() {
         return false;
     }
