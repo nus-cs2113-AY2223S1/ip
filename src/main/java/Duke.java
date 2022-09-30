e1c8c4d2915bb772fa8154c5f2a940649ed0f47b
brian-vb <brianzwongz@gmail.com>
2022-09-30 15:24:21 +0800

    Level 2: Add, List Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 public class Duke {
     public static void printWelcome() {
-        String line = ("____________________________________________________________");
-        System.out.println(line + "\n" + "Hello! I'm Duke" + "\n" + "What can I do for you?" + "\n" + line);
+        String line = ("    ____________________________________________________________");
+        System.out.println(line + "\n" + "    Hello! I'm Duke" + "\n" + "    What can I do for you?" + "\n" + line);
     }
     public static void main(String[] args) {
         printWelcome();
         Scanner input = new Scanner(System.in);
         String command;
+        String[] tasks = new String[100];
+        int numberOfTasks = 0;
 
         while (true) {
             command = input.nextLine();
             if(command.equals("bye")) {
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
                 System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
                 break;
+            } else if (command.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int counter = 0; counter < numberOfTasks; counter++) {
+                    System.out.println("    " + (counter + 1) + ". " + tasks[counter]);
+                }
+                System.out.println("    ____________________________________________________________");
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println("    " + command);
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
+                tasks[numberOfTasks++] = command;
+                System.out.println("    added: " + command);
+                System.out.println("    ____________________________________________________________");
             }
         }
     }
