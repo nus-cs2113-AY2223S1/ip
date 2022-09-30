9daf4f7e5276553c3a2b6ee06f9b90d8966b401f
brian-vb <brianzwongz@gmail.com>
2022-09-30 16:30:17 +0800

    Level 3: Mark as Done Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,59 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
+    public static void printLine() {
+        System.out.println("    ____________________________________________________________");
+    }
     public static void printWelcome() {
-        String line = ("    ____________________________________________________________");
-        System.out.println(line + "\n" + "    Hello! I'm Duke" + "\n" + "    What can I do for you?" + "\n" + line);
+        printLine();
+        System.out.println("    Hello! I'm Duke" + "\n" + "    What can I do for you?");
+        printLine();
+    }
+    public static void printGoodbye() {
+        printLine();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printLine();
     }
     public static void main(String[] args) {
         printWelcome();
         Scanner input = new Scanner(System.in);
         String command;
-        String[] tasks = new String[100];
-        int numberOfTasks = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             command = input.nextLine();
             if(command.equals("bye")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
+                printGoodbye();
                 break;
             } else if (command.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                for (int counter = 0; counter < numberOfTasks; counter++) {
-                    System.out.println("    " + (counter + 1) + ". " + tasks[counter]);
+                printLine();
+                System.out.println("    Here are the tasks in your list:");
+                for (int counter = 0; counter < tasks.size(); counter++) {
+                    System.out.println("    " + (counter + 1) + ".[" + tasks.get(counter).getStatusIcon() + "] " + tasks.get(counter).getTask());
                 }
-                System.out.println("    ____________________________________________________________");
+                printLine();
+            } else if (command.startsWith("mark ")) {
+                printLine();
+                System.out.println("    Nice! I've marked this task as done:");
+                command = command.replace("mark ", "");
+                int index = Integer.parseInt(command) - 1;
+                tasks.get(index).setAsDone(true);
+                System.out.println("    [" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).getTask());
+                printLine();
+            } else if (command.startsWith("unmark ")) {
+                printLine();
+                System.out.println("    OK, I've marked this task as not done yet:");
+                command = command.replace("unmark ", "");
+                int index = Integer.parseInt(command) - 1;
+                tasks.get(index).setAsDone(false);
+                System.out.println("    [" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).getTask());
+                printLine();
             } else {
-                System.out.println("    ____________________________________________________________");
-                tasks[numberOfTasks++] = command;
-                System.out.println("    added: " + command);
-                System.out.println("    ____________________________________________________________");
+                printLine();
+                Task t = new Task(command);
+                tasks.add(t);
+                System.out.println("    Added task: " + command);
+                printLine();
             }
         }
     }
