2ec514fb68f171c8ba449f78383f21d88464ac4c
ZHEQI SHEN <>
2022-09-30 14:28:42 +0800

    add find features

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -25,7 +25,8 @@ public class Ui {
     }
 
     public static void printDontKnowMessage(){
-        String dontKnowMessage = "    ____________________________________________________________\n"
+        String dontKnowMessage =
+                "    ____________________________________________________________\n"
                 + "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
                 + "    ____________________________________________________________";
         System.out.println(dontKnowMessage);
