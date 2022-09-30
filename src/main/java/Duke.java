534303f59fe88c79830314c934579491ef0cd73e
brian-vb <brianzwongz@gmail.com>
2022-09-30 15:10:24 +0800

    Level 1: Greet, Echo, Exit Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
+    public static void printWelcome() {
         String line = ("____________________________________________________________");
-        System.out.println(line + "\n" + "Hello! I'm Duke" + "\n" + "What can I do for you?" + "\n" + line
-        + "\n" + "Bye. Hope to see you again soon!" + "\n" + line);
+        System.out.println(line + "\n" + "Hello! I'm Duke" + "\n" + "What can I do for you?" + "\n" + line);
+    }
+    public static void main(String[] args) {
+        printWelcome();
+        Scanner input = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = input.nextLine();
+            if(command.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println("    " + command);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
